ce89692a5d28c93e1410b0148186347adb9ae74f
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-06 15:34:47 +0100

    Add basic documentation and test for flattenhierarchy.

---
+++ Core.Tests/Linq/HierarchyTests.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+using Void.Linq;
+using System.Linq;
+
+namespace Core.Tests.Linq
+{
+    [TestFixture]
+    public class HierarchyTests
+    {
+        private class Hierarchical
+        {
+            public Hierarchical[] Children = new Hierarchical[0];
+        }
+
+        [Test]
+        public void ShouldReturnAllInstancesInGraphWithoutDuplicates()
+        {
+            var root1 = new Hierarchical
+                        {
+                            Children = new[]
+                                       {
+                                           new Hierarchical
+                                           {
+                                               Children = new[]
+                                                          {
+                                                              new Hierarchical(),
+                                                              new Hierarchical()
+                                                          }
+                                           },
+                                           new Hierarchical()
+                                       }
+                        };
+            var root2 = new Hierarchical
+                        {
+                            Children = new[]
+                                       {
+                                           new Hierarchical
+                                           {
+                                               Children = new[]
+                                                          {
+                                                              new Hierarchical(),
+                                                              new Hierarchical()
+                                                          }
+                                           },
+                                           new Hierarchical()
+                                       }
+                        };
+
+            var flattened = Seq.Create(root1, root2).FlattenHierarchy(root => root.Children);
+            Assert.That(flattened.Count(), Is.EqualTo(10));//Ensures no duplicates
+            Assert.That(flattened.Distinct().Count(), Is.EqualTo(10));//Ensures all objects are there.
+
+        }
+    }
+}
--- Core/Linq/Hierarchy.cs
+++ Core/Linq/Hierarchy.cs
@@ -5,6 +5,13 @@ namespace Void.Linq
 {
     public static class Hierarchy
     {
+        /// <summary>
+        /// Flattens a hierarchical structure of <typeparamref name="TSource"/> objects into an <see cref="IEnumerable{TSource}"/>
+        /// </summary>
+        /// <typeparam name="TSource">The type of the objects in the hierarchy.</typeparam>
+        /// <param name="source">The source collection.</param>
+        /// <param name="childrenSelector">A function that given a <typeparamref name="TSource"/> returns all the immediate descendent.</param>
+        /// <returns>An <see cref="IEnumerable{TSource}"/> containing all the <typeparamref name="TSource"/> instances in the <paramref name="source"/>.</returns>
         public static IEnumerable<TSource> FlattenHierarchy<TSource>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TSource>> childrenSelector)
         {
             foreach (var item in source)

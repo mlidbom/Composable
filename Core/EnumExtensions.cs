cec28f94a0cbecb87453e951cd570b8cce4b980c
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-19 17:18:14 +0100

    Document Enum extensions.

--- Core/EnumExtensions.cs
+++ Core/EnumExtensions.cs
@@ -2,19 +2,25 @@ using System;
 
 namespace Void
 {
+    /// <summary/>
     public static class EnumExtensions
     {
+        /// <summary>
+        /// True if <paramref name="value"/> contains the bit flag <paramref name="flag"/>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="flag"></param>
+        /// <returns></returns>
         public static bool HasFlag(this Enum value, Enum flag)
         {
             if (!value.GetType().Equals(flag.GetType()))
             {
                 throw new ArgumentOutOfRangeException();
             }
-            var longValue = Convert.ToInt64(value); ;
+            var longValue = Convert.ToInt64(value);
             var longFlag = Convert.ToInt64(flag);
 
             return (longValue & longFlag) == longFlag;
         }
-
     }
 }

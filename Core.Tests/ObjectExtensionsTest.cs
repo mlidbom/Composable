b9a8b564782ffd107f91be86db8ab5a9ad85c0db
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-19 16:55:50 +0100

    Full testcoverage for ObjectExtensions.

--- Core.Tests/ObjectExtensionsTest.cs
+++ Core.Tests/ObjectExtensionsTest.cs
@@ -1,6 +1,8 @@
+using System;
 using NUnit.Framework;
 using Void.Linq;
 using System.Linq;
+using Void;
 
 namespace Core.Tests
 {
@@ -11,6 +13,28 @@ namespace Core.Tests
         public void RepeatShouldCreateSequenceOfLengthEqualToParameter()
         {
             Assert.That(12.Repeat(10).Count(), Is.EqualTo(10));
+        }
+
+        [Test]
+        public void CallingTransformShouldBeEquivalentToCallingItsFunctionArgumentWithItsImplicitArgument()
+        {
+            int i = 12;
+            Func<int, int> plusone = x => x + 1;
+            var expected = plusone(i);
+            var actual = i.Transform(plusone);
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void CallingDoShouldCallTheSuppliedFunctionUsingTheImpliedArgument()
+        {
+            const int expected = 3;
+            var result = 0;
+            expected.Do(me => result = me);
+
+            Assert.That(result, Is.EqualTo(expected));
+            
         }
     }
 }

35bb98df79b2f2a111f27632fed38f81d3e76bd5
mlidbom <magnus.lidbom@avega.se>
2009-12-19 20:18:22 +0100

    Remove useless stuff....

--- Void.SpaceTime/Time/Impl/SimpleTimePoint.cs
+++ Void.SpaceTime/Time/Impl/SimpleTimePoint.cs
@@ -4,26 +4,15 @@ namespace Void.Time.Impl
 {
     public class SimpleTimePoint : ITimePoint
     {
-        private readonly DateTime _datetimeValue;
-
-        public SimpleTimePoint(DateTime position)
+        public DateTime DateTimeValue { get; private set; }
+        protected internal SimpleTimePoint(DateTime position)
         {
-            _datetimeValue = position;
+            DateTimeValue = position;
         }
 
         public SimpleTimePoint(ITimePoint position)
         {
-            _datetimeValue = position.DateTimeValue;
-        }
-
-        public DateTime DateTimeValue
-        {
-            get { return _datetimeValue; }
-        }
-
-        public ITimePoint TimeCoordinate
-        {
-            get { return this; }
-        }
+            DateTimeValue = position.DateTimeValue;
+        }        
     }
 }
--- Void.SpaceTime/Time/TimePoint.cs
+++ Void.SpaceTime/Time/TimePoint.cs
@@ -1,12 +1,18 @@
+using System;
 using Void.Time.Impl;
 
 namespace Void.Time
 {
     public static class TimePoint
     {
+        public static ITimePoint FromDateTime(DateTime time)
+        {
+            return new SimpleTimePoint(time);
+        }
+
         public static ITimePoint Offset(this ITimePoint me, ITimeMovement movement)
         {
-            return new SimpleTimePoint(me.DateTimeValue +  movement.AsTimeSpan());
+            return FromDateTime(me.DateTimeValue + movement.AsTimeSpan());
         }
     }
 }

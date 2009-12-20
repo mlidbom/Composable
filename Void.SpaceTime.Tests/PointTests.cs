56171cba3c0d54463fef148136f5bda1b09155d6
mlidbom <magnus.lidbom@avega.se>
2009-12-20 15:02:10 +0100

    Add simple test project.

---
+++ Void.SpaceTime.Tests/PointTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Void.SpaceTime.Tests
+{
+    [TestFixture]
+    public class PointTests
+    {
+        [Test]
+        public void JustSomeNonsense()
+        {
+        }
+    }
+}

5806429a77eca73fa111f2a04101996ec570df97
malm <magnus.lidbom@gmail.com>
2010-06-11 17:26:36 +0200

    Updates to various files due to pex changes.

--- Void.Core.PexTests/Void.Core.Designer.cs
+++ Void.Core.PexTests/Void.Core.Designer.cs
@@ -1,2 +1,2 @@
-// <assemblyHash>2c6bd168</assemblyHash>
-// <compilationHash>7b0iY9q+kxX1SgpKhSFQsfTnPDY=r9gKFU6H9SC5yCQ4ym1mjJhzxJw=</compilationHash>
+// <assemblyHash>b702b609</assemblyHash>
+// <compilationHash>y5iY3l77gUOyTU2mXKufZYiEps8=r9gKFU6H9SC5yCQ4ym1mjJhzxJw=</compilationHash>

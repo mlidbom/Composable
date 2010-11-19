e17bf5c62b309a71b77b1192f4a5d1aec3c6deaa
malm <magnus.lidbom@gmail.com>
2010-11-19 19:35:54 +0100

    Improve tostring for ValueObject to use JSon serialization.

---
+++ System/System.Tests/DDD/ValueObjectToString.cs
@@ -0,0 +1,75 @@
+#region usings
+
+using Composable.DDD;
+using NUnit.Framework;
+
+#endregion
+
+namespace Composable.Tests.DDD
+{
+    [TestFixture]
+    public class ValueObjectToString
+    {
+        public class Root : ValueObject<Root>
+        {
+            public string Name { get; set; }
+            public Branch Branch1 { get; set; }
+            public Branch Branch2 { get; set; }
+        }
+
+        public class Branch
+        {
+            public string Name { get; set; }
+            public Leaf Leaf1 { get; set; }
+            public Leaf Leaf2 { get; set; }
+        }
+
+        public class Leaf
+        {
+            public string Name { get; set; }
+        }
+
+        [Test]
+        public void ReturnsHirerarchicalDescriptionOfData()
+        {
+            var description = new Root
+                                  {
+                                      Name = "RootName",
+                                      Branch1 = new Branch
+                                                    {
+                                                        Name = "Branch1Name",
+                                                        Leaf1 = new Leaf {Name = "Leaf1Name"},
+                                                        Leaf2 = new Leaf {Name = "Leaf1Name"},
+                                                    },
+                                      Branch2 = new Branch
+                                                    {
+                                                        Name = "Branch1Name",
+                                                        Leaf1 = new Leaf {Name = "Leaf1Name"},
+                                                        Leaf2 = new Leaf {Name = "Leaf1Name"},
+                                                    }
+                                  }.ToString();
+            Assert.That(description, Is.EqualTo(
+@"{
+  ""Name"": ""RootName"",
+  ""Branch1"": {
+    ""Name"": ""Branch1Name"",
+    ""Leaf1"": {
+      ""Name"": ""Leaf1Name""
+    },
+    ""Leaf2"": {
+      ""Name"": ""Leaf1Name""
+    }
+  },
+  ""Branch2"": {
+    ""Name"": ""Branch1Name"",
+    ""Leaf1"": {
+      ""Name"": ""Leaf1Name""
+    },
+    ""Leaf2"": {
+      ""Name"": ""Leaf1Name""
+    }
+  }
+}"));
+        }
+    }
+}

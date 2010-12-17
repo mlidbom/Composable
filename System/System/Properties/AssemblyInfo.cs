ddc583026ed7b5f903a314f87a2257336ccda95c
malm <magnus.lidbom@gmail.com>
2010-12-17 20:16:57 +0100

    Bump versions to 1.1.0.0

--- System/System/Properties/AssemblyInfo.cs
+++ System/System/Properties/AssemblyInfo.cs
@@ -36,7 +36,7 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 
 
-[assembly: AssemblyVersion("0.5.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 //[assembly: InternalsVisibleTo("Void.Core.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f1089b3ac60497d6472a2ea1a62c4618d2ff2eda20ed073b860a20c6abab8c45b50bde9d3342aee84bedb1d59845774245ca2be436da710a43ba047d24b85d70a227a1d12075c48ec04a33bf022cb9baea035f6a2385dc9b4f9b8acab3b1a29bdd75e06ff4d489a1aedd01477b2a8186df21f2044756acac7f2e29d2fd15deaa")]
 //[assembly: InternalsVisibleTo("Void.Core.PexTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f1089b3ac60497d6472a2ea1a62c4618d2ff2eda20ed073b860a20c6abab8c45b50bde9d3342aee84bedb1d59845774245ca2be436da710a43ba047d24b85d70a227a1d12075c48ec04a33bf022cb9baea035f6a2385dc9b4f9b8acab3b1a29bdd75e06ff4d489a1aedd01477b2a8186df21f2044756acac7f2e29d2fd15deaa")]

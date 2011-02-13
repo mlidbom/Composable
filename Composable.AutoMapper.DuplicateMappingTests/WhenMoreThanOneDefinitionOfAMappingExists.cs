972194cd879645734f6bd712268392c1ed17424c
Magnus Lidbom <magnus.lidbom@gmail.com>
2011-02-14 00:16:45 +0100

    Make Composable.AutoMapper more composable by having users supply the service locator used to find IProvidesMappings implementors.

--- Composable.AutoMapper.DuplicateMappingTests/WhenMoreThanOneDefinitionOfAMappingExists.cs
+++ Composable.AutoMapper.DuplicateMappingTests/WhenMoreThanOneDefinitionOfAMappingExists.cs
@@ -1,4 +1,7 @@
 using System;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using CommonServiceLocator.WindsorAdapter;
 using NUnit.Framework;
 
 namespace Composable.AutoMapper.Tests
@@ -6,6 +9,21 @@ namespace Composable.AutoMapper.Tests
     [TestFixture]
     public class WhenMoreThanOneDefinitionOfAMappingExists
     {
+        [SetUp]
+        public void Setup()
+        {
+            var container = new WindsorContainer();
+            var locator = new WindsorServiceLocator(container);
+            container.Register(AllTypes.FromThisAssembly().BasedOn<IProvidesMappings>().WithService.Base());
+            ComposableMapper.Init(locator);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ComposableMapper.ResetOnlyCallFromTests();
+        }
+
         public class MappingBootstrapper : IProvidesMappings
         {
             public void CreateMappings(SafeConfiguration configuration)

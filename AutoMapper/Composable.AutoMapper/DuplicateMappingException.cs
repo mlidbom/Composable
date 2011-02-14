093b728783ee61481a0a23d20eee0cdd5a160d02
Magnus Lidbom <magnus.lidbom@gmail.com>
2011-02-14 04:27:48 +0100

    Make Composable.AutoMapper independent of Composable.Core.

--- AutoMapper/Composable.AutoMapper/DuplicateMappingException.cs
+++ AutoMapper/Composable.AutoMapper/DuplicateMappingException.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using Composable.System;
 
 namespace Composable.AutoMapper
 {
     public class DuplicateMappingException : Exception
     {
-        public DuplicateMappingException(Type sourceType, Type targetType):base("Mapping from {0} to {1} already exists".FormatWith(sourceType, targetType))
+        public DuplicateMappingException(Type sourceType, Type targetType):base(string.Format("Mapping from {0} to {1} already exists",sourceType, targetType))
         {
             
         }

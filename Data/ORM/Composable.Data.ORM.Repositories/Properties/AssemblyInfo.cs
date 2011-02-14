69c1afae527438ce79f047235fadc81247b15b12
Magnus Lidbom <magnus.lidbom@gmail.com>
2011-02-14 05:02:39 +0100

    Break out repository interfaces only into Composable.Repositories to minimize coupling, need for reference updates....

--- Data/ORM/Composable.Data.ORM.Repositories/Properties/AssemblyInfo.cs
+++ Data/ORM/Composable.Data.ORM.Repositories/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ServiceModel")]
+[assembly: AssemblyTitle("Composable.Data.ORM.Repositories")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ServiceModel")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyProduct("Composable.Data.ORM.Repositories")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("730cf514-c227-4ca8-917a-f01f3c8b7086")]
+[assembly: Guid("0c6c421e-6001-40da-a39d-9fb3c0696d0a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.5.0.0")]

d1f0f0e6fe1e881dbe7d39046f7ef1b63f5da0ef
Magnus Lidbom <magnus.lidbom@gmail.com>
2011-04-20 19:02:40 +0200

    Move more event handling into composable.cqrs

---
+++ CQRS/CQRS/StuffThatDoesNotBelongHere/EventPersistenceService.cs
@@ -0,0 +1,31 @@
+﻿using System.Transactions;
+using Composable.CQRS;
+using Composable.DomainEvents;
+using Manpower.System.Web.Mvc.StuffThatDoesNotBelongHere;
+using Microsoft.Practices.ServiceLocation;
+
+namespace Composable.StuffThatDoesNotBelongHere
+{
+    public class EventPersistenceService : IEventPersistenceService
+    {
+        private readonly IServiceLocator _serviceLocator;
+
+        public EventPersistenceService(IServiceLocator serviceLocator)
+        {
+            _serviceLocator = serviceLocator;
+        }
+
+        public virtual void Persist<TEvent>(TEvent evt) where TEvent : IDomainEvent
+        {
+            using (DomainEvent.Register<IDomainEvent>(x => {}))
+            {
+                using (var transaction = new TransactionScope())
+                {
+                    var handler = _serviceLocator.GetSingleInstance<IEventPersister<TEvent>>();
+                    handler.Persist(evt);
+                    transaction.Complete();
+                }
+            }
+        }
+    }
+}

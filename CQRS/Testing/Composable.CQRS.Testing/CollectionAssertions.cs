7744168f5a274fd9a9c627130051c1bd785b0265
mlidbo <mlidbo@manpower.se>
2011-05-19 14:49:56 +0200

    Move test framework into composable.cqrs and rename appropriately

---
+++ CQRS/Testing/Composable.CQRS.Testing/CollectionAssertions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Linq;
+using Composable.System.Linq;
+using NUnit.Framework;
+
+namespace Composable.CQRS.Testing
+{
+    public static class CollectionAssertions
+    {
+        public static void AssertContainsSingle<T>(this IEnumerable me)
+        {
+            var result = me.Cast<object>().OfType<T>();
+            if(result.None())
+            {
+                Assert.Fail("Expected collection to contain Single instance of {0} but no instance was found", typeof(T));
+            }if(result.Count() > 1)
+            {
+                Assert.Fail("Expected collection to contain Single instance of {0} but {1} instances were found", typeof(T), result.Count());
+            }
+        }
+    }
+}

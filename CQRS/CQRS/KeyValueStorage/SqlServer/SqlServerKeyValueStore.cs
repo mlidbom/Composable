9c14a04eda71a36368f547a4ed1a407b3f824e9c
mlidbo <mlidbo@manpower.se>
2011-05-26 23:49:01 +0200

    Simple Sql server key value store implemented

---
+++ CQRS/CQRS/KeyValueStorage/SqlServer/SqlServerKeyValueStore.cs
@@ -0,0 +1,184 @@
+#region usings
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Composable.NewtonSoft;
+using Composable.System;
+using Newtonsoft.Json;
+using System.Linq;
+
+#endregion
+
+namespace Composable.KeyValueStorage.SqlServer
+{
+    public class SqlServerKeyValueStore : IKeyValueStore
+    {
+        private readonly string _connectionString;
+
+        public SqlServerKeyValueStore(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IKeyValueSession OpenSession()
+        {
+            return new SqlServerKeyValueStoreSession(this);
+        }
+
+        private class SqlServerKeyValueStoreSession : IKeyValueSession
+        {
+            private readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.Objects,
+                ContractResolver = new IncludeMembersWithPrivateSettersResolver()
+            };
+
+            private readonly SqlServerKeyValueStore _store;
+            private readonly Dictionary<Guid, object> _idMap = new Dictionary<Guid, object>();
+            private readonly HashSet<Guid> _persistentValues = new HashSet<Guid>();
+            private readonly SqlConnection _connection;
+            private bool TableVerifiedToExist;
+            private const int UniqueConstraintViolationErrorNumber= 2627;
+            private const int SqlBatchSize = 10;
+
+            public SqlServerKeyValueStoreSession(SqlServerKeyValueStore store)
+            {
+                _store = store;
+                _connection = new SqlConnection(store._connectionString);
+                _connection.Open();
+                EnsureTableExists();
+            }
+
+            private void EnsureTableExists()
+            {
+                if (!TableVerifiedToExist)
+                {
+                    var checkForTableCommand = _connection.CreateCommand();
+                    checkForTableCommand.CommandText = "select count(*) from sys.tables where name = 'Store'";
+                    var exists = (int)checkForTableCommand.ExecuteScalar();
+                    if (exists == 0)
+                    {
+                        var createTableCommand = _connection.CreateCommand();
+                        createTableCommand.CommandText =
+                            @"
+CREATE TABLE [dbo].[Store](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Value] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_Store] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+";
+                        createTableCommand.ExecuteNonQuery();
+                    }
+                    TableVerifiedToExist = true;
+                }
+            }
+
+            public TValue Load<TValue>(Guid key)
+            {
+                object value;
+                if(_idMap.TryGetValue(key, out value))
+                {
+                    return (TValue)value;
+                }
+
+                using (var loadCommand = _connection.CreateCommand())
+                {
+                    loadCommand.CommandText = "SELECT Value FROM Store WHERE Id=@Id";
+                    loadCommand.Parameters.Add(new SqlParameter("Id", key));
+                    value = loadCommand.ExecuteScalar();
+                    value = JsonConvert.DeserializeObject((String)value, JsonSettings);
+                }
+                _persistentValues.Add(key);
+                _idMap.Add(key, value);
+                return (TValue)value;
+            }
+
+            public void Save<TValue>(Guid key, TValue value)
+            {
+                object existing;
+                if (_idMap.TryGetValue(key, out existing))
+                {
+                    throw new AttemptToSaveAlreadyPersistedValueException(key, value);
+                }
+                _idMap.Add(key, value);
+            }
+
+            public void SaveChanges()
+            {
+                InsertValues(_idMap.Where(entry => !_persistentValues.Contains(entry.Key)));
+                UpdateValues(_idMap.Where(entry => _persistentValues.Contains(entry.Key)));
+            }
+
+            private void UpdateValues(IEnumerable<KeyValuePair<Guid, object>> values)
+            {
+                var handled = 0;
+                int eventCount = values.Count();
+                while (handled < eventCount)
+                {
+                    using (var command = _connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.Text;
+                        for(var handledInBatch = 0; handledInBatch < SqlBatchSize && handled < eventCount; handledInBatch++, handled++)
+                        {
+                            var entry = values.ElementAt(handledInBatch);
+
+                            command.CommandText += "UPDATE Store SET Value = @Value{0} WHERE Id = @Id{0}"
+                                .FormatWith(handledInBatch);
+
+                            command.Parameters.Add(new SqlParameter("Id" + handledInBatch, entry.Key));
+                            command.Parameters.Add(new SqlParameter("Value" + handledInBatch,
+                                                                    JsonConvert.SerializeObject(entry.Value, Formatting.None, JsonSettings)));
+                        }
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            private void InsertValues(IEnumerable<KeyValuePair<Guid, object>> values)
+            {
+                var handled = 0;
+                int eventCount = values.Count();
+                while (handled < eventCount)
+                {
+                    using (var command = _connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.Text;
+                        for (var handledInBatch = 0; handledInBatch < SqlBatchSize && handled < eventCount; handledInBatch++, handled++)
+                        {
+                            var entry = values.ElementAt(handledInBatch);
+
+                            command.CommandText += "INSERT Store(Id, Value) VALUES(@Id{0}, @Value{0})"
+                                .FormatWith(handledInBatch);
+
+                            command.Parameters.Add(new SqlParameter("Id" + handledInBatch, entry.Key));
+                            command.Parameters.Add(new SqlParameter("Value" + handledInBatch, JsonConvert.SerializeObject(entry.Value, Formatting.None, JsonSettings)));
+                        }
+                        try
+                        {
+                            command.ExecuteNonQuery();
+                        }catch(SqlException e)
+                        {
+                            if(e.Number == UniqueConstraintViolationErrorNumber)
+                            {
+                                throw new AttemptToSaveAlreadyPersistedValueException(Guid.Empty, "Batched insert cannot extract value...");
+                            }
+                            throw;
+                        }
+                    }
+                }
+
+            }
+
+            public void Dispose()
+            {
+                _connection.Dispose();
+                _idMap.Clear();
+            }
+        }
+    }
+}

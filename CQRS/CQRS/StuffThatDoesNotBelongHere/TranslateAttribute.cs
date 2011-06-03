89cc3e3d1d0dd43f715e0c5d78bb42daf6c7338f
mlidbo <mlidbo@manpower.se>
2011-06-03 20:05:25 +0200

    Move translate attribute into stuffthatdoesnotbelonghere

---
+++ CQRS/CQRS/StuffThatDoesNotBelongHere/TranslateAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Composable.StuffThatDoesNotBelongHere
+{
+    /// <summary>
+    /// Put this attribute on a property of type string to make the <see cref="TranslationInterceptor"/> automatically translate the entity when it is loaded.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class TranslateAttribute : Attribute
+    {
+        /// <summary>
+        /// Key to use for the translation. If null, use the property name.
+        /// </summary>
+        public string Key { get; private set; }
+
+        public TranslateAttribute()
+        {
+        }
+
+        public TranslateAttribute(string key)
+        {
+            this.Key = key;
+        }
+    }
+}

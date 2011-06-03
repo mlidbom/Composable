f15f40a15b56a83d68165edeb0fe3e658ab14dab
mlidbo <mlidbo@manpower.se>
2011-06-03 13:07:27 +0200

    checkpoint

--- CQRS/CQRS/KeyValueStorage/IObjectStore.cs
+++ CQRS/CQRS/KeyValueStorage/IObjectStore.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 
 namespace Composable.KeyValueStorage
 {
-    public interface IObjectStore
+    public interface IObjectStore : IDisposable
     {
         bool TryGet<T>(Guid id, out T value);
         void Add<T>(Guid id, T value);
         bool Remove<T>(Guid id);
-        void Update(Guid key, object value);
+        void Update(IEnumerable<KeyValuePair<Guid, object>> values);
         IEnumerable<KeyValuePair<Guid, T>> GetAll<T>();
     }
 }

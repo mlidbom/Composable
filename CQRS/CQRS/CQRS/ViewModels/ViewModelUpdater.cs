d5624e799440388ea6d6b502203ccddde7d57f3e
mlidbo <mlidbo@manpower.se>
2011-06-04 17:47:05 +0200

    Cleanup MultiEventhandler

--- CQRS/CQRS/CQRS/ViewModels/ViewModelUpdater.cs
+++ CQRS/CQRS/CQRS/ViewModels/ViewModelUpdater.cs
@@ -1,13 +1,14 @@
 using System;
 using Composable.CQRS.EventSourcing;
+using Composable.DomainEvents;
 using Composable.StuffThatDoesNotBelongHere;
 
 namespace Composable.CQRS.ViewModels
 {
-    public abstract class ViewModelUpdater<TImplementor, TViewModel, TEvent> : 
-        MultiEventHandler<TImplementor, TEvent>, 
-        IViewModelUpdater<TViewModel, TEvent> where TImplementor : MultiEventHandler<TImplementor, TEvent> where TEvent : IAggregateRootEvent
+    public class ViewModelUpdater<TImplementor, TViewModel, TEvent> : MultiEventHandler<TImplementor, TEvent> 
+        where TEvent : IAggregateRootEvent 
+        where TImplementor : ViewModelUpdater<TImplementor, TViewModel, TEvent>
     {
-        public abstract void Handle(TEvent message);
+        
     }
 }

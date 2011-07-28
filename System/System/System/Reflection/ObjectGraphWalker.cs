7717faf795d413a93b877bad023632a2c4534685
mlidbo <mlidbo@manpower.se>
2011-07-28 14:34:14 +0200

    Significant improvement in speed in ObjectGraphWalker
       Stop walking primitives, string, datetime, guid
       attempt to reuse valuefetchers for collections.

--- System/System/System/Reflection/ObjectGraphWalker.cs
+++ System/System/System/Reflection/ObjectGraphWalker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,25 +9,50 @@ namespace Composable.System.Reflection
     {
         public static IEnumerable<object> GetGraph(object o)
         {
-            if(o == null)
+            return InternalGetGraph(o);
+        }
+
+        private static IEnumerable<object> InternalGetGraph(object o, Func<object, object>[] potentialGetters = null, Type getterType = null)
+        {
+            if (o == null)
+            {
+                yield break;
+            }
+            
+            yield return o;
+
+            var objectType = o.GetType();
+            if (objectType.IsPrimitive || o is string || o is DateTime || o is Guid)
             {
                 yield break;
             }
 
-            yield return o;
-            if(o is IEnumerable)
+            if (o is IEnumerable)
             {
-                foreach (var value in (o as IEnumerable).Cast<object>().SelectMany(GetGraph))
+                var enumerable = (o as IEnumerable).Cast<object>().ToList();
+                if (enumerable.Any())
+                {
+                    var firstType = enumerable.First().GetType();
+                    var firstGetters = MemberAccessorHelper.GetFieldsAndPropertyGetters(firstType);
+
+                    foreach (var value in enumerable.SelectMany(k => InternalGetGraph(k, firstGetters, firstType)))
+                    {
+                        yield return value;
+                    }
+                }
+            }
+            else
+            {
+                if(potentialGetters == null || objectType != getterType)
+                {
+                    potentialGetters = MemberAccessorHelper.GetFieldsAndPropertyGetters(objectType);
+                }
+
+                foreach (var value in potentialGetters.Select(getter => getter(o)).SelectMany(GetGraph))
                 {
                     yield return value;
                 }
-            }else
-            {
-                foreach (var value in MemberAccessorHelper.GetFieldAndPropertyValues(o).SelectMany(GetGraph))
-                {
-                    yield return value;
-                }
-            }                        
+            }
         }
     }
 }

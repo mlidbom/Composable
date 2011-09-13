473cbde4cd5aef758ea13dce5d4cf2d5be82dcb3
mlidbo <mlidbo@manpower.se>
2011-09-13 19:50:49 +0200

    Build and package cqrs.windsor

--- CQRS/CQRS.Windsor/Properties/AssemblyInfo.cs
+++ CQRS/CQRS.Windsor/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("46eb7bfb-8771-4b9d-bbb6-33a8938bf3f8")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

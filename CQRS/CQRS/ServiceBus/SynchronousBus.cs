2494e2f888de2cdc6131d44bc8fb83f9e623034a
mlidbo <mlidbo@manpower.se>
2011-09-13 20:02:13 +0200

    Get synchronousbus and dualdispatchbus into composable

---
+++ CQRS/CQRS/ServiceBus/SynchronousBus.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Castle.Windsor;
+using Composable.System.Reflection;
+using Composable.System.Transactions;
+using NServiceBus;
+
+namespace Composable.ServiceBus
+{
+    public class SynchronousBus : IServiceBus
+        {
+            private readonly IWindsorContainer _serviceLocator;
+
+            public SynchronousBus(IWindsorContainer serviceLocator)
+            {
+                _serviceLocator = serviceLocator;
+            }
+
+            public void Publish(object message)
+            {
+                ((dynamic)this).Publish((dynamic)message);
+            }
+
+            private void Publish<TMessage>(TMessage message) where TMessage : IMessage
+            {
+                var handlerTypes = message.GetType().GetAllTypesInheritedOrImplemented()
+                    .Where(t => t.Implements(typeof(IMessage)))
+                    .Select(t => typeof(IHandleMessages<>).MakeGenericType(t))
+                    .ToArray();
+
+
+                var handlers = new List<object>();
+                foreach (var handlerType in handlerTypes)
+                {
+                    handlers.AddRange(_serviceLocator.ResolveAll(handlerType).Cast<object>());
+                }
+
+                InTransaction.Execute(() =>
+                {
+                    foreach (dynamic handler in handlers)
+                    {
+                        handler.Handle((dynamic)message);
+                    }
+                });
+            }
+
+            public void SendLocal(object message)
+            {
+                Publish(message);
+            }
+       
+        } 
+    }
---
+++ CQRS/Composable.CQRS.ServiceBus.NServiceBus/DualDispatchBus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Composable.ServiceBus;
+
+namespace Composable.CQRS.ServiceBus.NServiceBus
+{
+    public class DualDispatchBus : IServiceBus
+    {
+        private readonly SynchronousBus _local;
+        private readonly NServiceBusServiceBus _realBus;
+
+        public DualDispatchBus(SynchronousBus local, NServiceBusServiceBus realBus)
+        {
+            _local = local;
+            _realBus = realBus;
+        }
+
+        public void Publish(object message)
+        {
+            _local.Publish(message);
+            _realBus.Publish(message);
+        }
+
+        public void SendLocal(object message)
+        {
+            throw new NotImplementedException("No sane way to implement dual dispatch for sendlocal that I can come up with. Same registrations will be called sync and then via the bus...");
+        }
+    }
+}

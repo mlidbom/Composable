a7f9995ed54abd9d9a494eb30d51e11c8cd65979
mlidbo <mlidbo@manpower.se>
2011-10-12 23:33:37 +0200

    Update to fixed version of nservicebus and add test for the previously failing transactionality.

---
+++ CQRS/Testing/Composable.CQRS.Testing/TestingSupportMessageModule.cs
@@ -0,0 +1,31 @@
+﻿#region usings
+
+using System;
+using System.Transactions;
+using NServiceBus;
+
+#endregion
+
+namespace Composable.CQRS.Testing
+{
+    public class TestingSupportMessageModule : IMessageModule
+    {
+        public static event Action<Transaction> OnHandleBeginMessage = t => {};
+        public void HandleBeginMessage()
+        {
+            OnHandleBeginMessage(Transaction.Current);
+        }
+
+        public static event Action<Transaction> OnHandleEndMessage = t => { };
+        public void HandleEndMessage()
+        {
+            OnHandleEndMessage(Transaction.Current);
+        }
+
+        public static event Action OnHandleError = () => { };
+        public void HandleError()
+        {
+            OnHandleError();
+        }
+    }
+}
---
+++ System/System/System/Transactions/TransactionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Transactions;
+
+namespace Composable.System.Transactions
+{
+    public static class TransactionExtensions
+    {
+         public static bool WaitForTransactionToComplete(this Transaction me, TimeSpan timeout)
+         {             
+             var done = new ManualResetEvent(false);
+             me.TransactionCompleted += (_, __) => done.Set();
+
+             if(me.TransactionInformation.Status != TransactionStatus.Active)
+             {
+                 done.Set();
+             }
+
+             return done.WaitOne(timeout);
+         }
+    }
+}

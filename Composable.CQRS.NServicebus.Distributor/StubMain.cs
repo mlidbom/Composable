c78540a8fd6a82e9f10be0d5d45b553f1649b3a4
mlidbo <mlidbo@manpower.se>
2011-10-18 13:33:26 +0200

    Make sure distributor will get correct assembly redirects with nuget.

---
+++ Composable.CQRS.NServicebus.Distributor/StubMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composable.CQRS.NServicebus.Distributor
+{
+    public class StubMain
+    {
+        public static void Main()
+        {
+            throw new Exception("Not indended to be used.");
+        } 
+    }
+}

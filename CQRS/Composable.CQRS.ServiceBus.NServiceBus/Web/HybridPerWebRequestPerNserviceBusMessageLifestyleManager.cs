993635d2d7ea683471976c8461590927354f4ede
mlidbo <mlidbo@manpower.se>
2012-04-02 17:19:21 +0200

    Builds and compiles after update.

--- CQRS/Composable.CQRS.ServiceBus.NServiceBus/Web/HybridPerWebRequestPerNserviceBusMessageLifestyleManager.cs
+++
@@ -1,6 +0,0 @@
-using Composable.CQRS.ServiceBus.NServiceBus.Web.WindsorNServicebusWeb;
-
-namespace Composable.CQRS.ServiceBus.NServiceBus.Web
-{
-    public class HybridPerWebRequestPerNserviceBusMessageLifestyleManager : HybridPerWebRequestLifestyleManager<PerNserviceBusMessageLifestyleManager> { }
-}

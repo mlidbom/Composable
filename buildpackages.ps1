b1821b7540e5d1e49d892617b05eff767116dcd0
mlidbo <mlidbo@manpower.se>
2012-04-18 16:28:30 +0200

    Various improvements to the package build. Should mean that builds fail if packaging fails etc.

--- buildpackages.ps1
+++ buildpackages.ps1
@@ -5,37 +5,47 @@ Param(
 
 $ErrorActionPreference="Stop"
 
-Set-Alias Build-Pkg .\packages\NuGet.CommandLine.1.5.20905.5\tools\NuGet.exe
-
 $scriptRoot = Split-Path (Resolve-Path $myInvocation.MyCommand.Path) 
-$scriptRoot = $scriptRoot + "\"
+$OutputDirectory = Resolve-Path "$scriptRoot\$OutputDirectory"
+
+Set-Alias Build-Pkg-Internal $scriptRoot\packages\NuGet.CommandLine.1.5.20905.5\tools\NuGet.exe
+
+function Build-Pkg ($ProjectFile)
+{
+	Write-Host -ForegroundColor Cyan "Packaging $ProjectFile into $OutputDirectory"
+	Build-Pkg-Internal pack $ProjectFile -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
+	if($LASTEXITCODE -ne 0)
+	{
+		Write-Error "Bailing out because nuget.exe exited with code $LASTEXITCODE"
+	}
+	Write-Host
+}
 
 function GetAssemblyVersion($assembly)
 {   
-   $assembly = $scriptRoot + $assembly
-   write-host $assembly
+   $ErrorActionPreference="Stop"
+   $assembly = "$scriptRoot\$assembly"	
    $Myasm = [System.Reflection.Assembly]::Loadfile($assembly)   
    $Aname = $Myasm.GetName()
    $Aver =  $Aname.version
    return $Aver
 }
 
-$CoreVersion = GetAssemblyVersion("System\System\Bin\" + $Configuration + "\Composable.Core.dll")
-$WindsorVersion = GetAssemblyVersion("CQRS\CQRS.Windsor\Bin\" + $Configuration + "\Composable.CQRS.Windsor.dll")
-$NServiceBusVersion = GetAssemblyVersion("CQRS\Composable.CQRS.ServiceBus.NServiceBus\Bin\" + $Configuration + "\Composable.CQRS.ServiceBus.NServiceBus.dll")
-$CqrsVersion = GetAssemblyVersion("CQRS\CQRS\Bin\" + $Configuration + "\Composable.CQRS.dll")
-$DomainEventsVersion = GetAssemblyVersion("Composable.DomainEvents\Bin\" + $Configuration + "\Composable.DomainEvents.dll")
-$AutoMapperVersion = GetAssemblyVersion("AutoMapper\Composable.AutoMapper\Bin\" + $Configuration + "\Composable.AutoMapper.dll")
+$CoreVersion = GetAssemblyVersion("System\System\Bin\$Configuration\Composable.Core.dll")
+$WindsorVersion = GetAssemblyVersion("CQRS\CQRS.Windsor\Bin\$Configuration\Composable.CQRS.Windsor.dll")
+$NServiceBusVersion = GetAssemblyVersion("CQRS\Composable.CQRS.ServiceBus.NServiceBus\Bin\$Configuration\Composable.CQRS.ServiceBus.NServiceBus.dll")
+$CqrsVersion = GetAssemblyVersion("CQRS\CQRS\Bin\$Configuration\Composable.CQRS.dll")
+$DomainEventsVersion = GetAssemblyVersion("Composable.DomainEvents\Bin\$Configuration\Composable.DomainEvents.dll")
+$AutoMapperVersion = GetAssemblyVersion("AutoMapper\Composable.AutoMapper\Bin\$Configuration\Composable.AutoMapper.dll")
 
-
-Build-Pkg pack ".\System\System\Composable.Core.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration
-Build-Pkg pack ".\CQRS\CQRS\Composable.CQRS.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
-Build-Pkg pack ".\CQRS\NHibernateRepositories\Composable.CQRS.NHibernate.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
-Build-Pkg pack ".\CQRS\Composable.CQRS.ServiceBus.NServiceBus\Composable.CQRS.ServiceBus.NServiceBus.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
-Build-Pkg pack ".\Composable.DomainEvents\Composable.DomainEvents.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
-Build-Pkg pack ".\AutoMapper\Composable.AutoMapper\Composable.AutoMapper.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
-Build-Pkg pack ".\CQRS\CQRS.Windsor\Composable.CQRS.Windsor.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
-Build-Pkg pack ".\CQRS\Testing\Composable.CQRS.Testing\Composable.CQRS.Testing.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
-Build-Pkg pack ".\CQRS\Composable.CQRS.Population.Client\Composable.CQRS.Population.Client.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
-Build-Pkg pack ".\CQRS\Composable.CQRS.Population.Server\Composable.CQRS.Population.Server.csproj" -OutputDirectory $OutputDirectory -Prop Configuration=$Configuration -Prop CoreVersion=$CoreVersion -Prop CqrsVersion=$CqrsVersion -Prop WindsorVersion=$WindsorVersion -Prop DomainEventsVersion=$DomainEventsVersion -Prop NServiceBusVersion=$NServiceBusVersion -Prop AutoMapperVersion=$AutoMapperVersion
+Build-Pkg "$scriptRoot\System\System\Composable.Core.csproj"
+Build-Pkg "$scriptRoot\CQRS\CQRS\Composable.CQRS.csproj"
+Build-Pkg "$scriptRoot\CQRS\NHibernateRepositories\Composable.CQRS.NHibernate.csproj"
+Build-Pkg "$scriptRoot\CQRS\Composable.CQRS.ServiceBus.NServiceBus\Composable.CQRS.ServiceBus.NServiceBus.csproj"
+Build-Pkg "$scriptRoot\Composable.DomainEvents\Composable.DomainEvents.csproj"
+Build-Pkg "$scriptRoot\AutoMapper\Composable.AutoMapper\Composable.AutoMapper.csproj"
+Build-Pkg "$scriptRoot\CQRS\CQRS.Windsor\Composable.CQRS.Windsor.csproj"
+Build-Pkg "$scriptRoot\CQRS\Testing\Composable.CQRS.Testing\Composable.CQRS.Testing.csproj"
+Build-Pkg "$scriptRoot\CQRS\Composable.CQRS.Population.Client\Composable.CQRS.Population.Client.csproj"
+Build-Pkg "$scriptRoot\CQRS\Composable.CQRS.Population.Server\Composable.CQRS.Population.Server.csproj"
 

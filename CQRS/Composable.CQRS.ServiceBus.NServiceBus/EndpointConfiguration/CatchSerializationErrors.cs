87d1a67b8a83c6add34c8596d04afe1271fb6987
mlidbo <mlidbo@manpower.se>
2012-04-20 16:45:21 +0200

    Move message error handlers to their own files.

---
+++ CQRS/Composable.CQRS.ServiceBus.NServiceBus/EndpointConfiguration/CatchSerializationErrors.cs
@@ -0,0 +1,24 @@
+using System.Runtime.Serialization;
+using Composable.CQRS.EventSourcing;
+using NServiceBus;
+
+namespace Composable.CQRS.ServiceBus.NServiceBus.EndpointConfiguration
+{
+    public class CatchSerializationErrors : IMessageHandler<IMessage>
+    {
+        private readonly IBus _bus;
+
+        public CatchSerializationErrors(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public void Handle(IMessage message)
+        {
+            if (message.GetType() == typeof (IMessage) || message.GetType() == typeof (AggregateRootEvent))
+            {
+                throw new SerializationException("Message failed to serialize correctly");
+            }
+        }
+    }
+}

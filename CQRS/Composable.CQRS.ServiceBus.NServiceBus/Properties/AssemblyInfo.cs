0739333b40c28365884ae25bf4af538018164c38
mlidbo <mlidbo@manpower.se>
2012-04-26 10:25:03 +0200

    Go back to using a single version for all packaged assemblies and improve flexibility of package build script.

--- CQRS/Composable.CQRS.ServiceBus.NServiceBus/Properties/AssemblyInfo.cs
+++ CQRS/Composable.CQRS.ServiceBus.NServiceBus/Properties/AssemblyInfo.cs
@@ -1,24 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Composable.CQRS.ServiceBus.NServiceBus")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Composable.CQRS.ServiceBus.NServiceBus")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e1e7288d-9a40-4f1a-b8ae-6dd5eb10c076")]
-[assembly: AssemblyVersion("1.7.1.0")]
--- CQRS/NHibernateRepositories/Properties/AssemblyInfo.cs
+++ CQRS/NHibernateRepositories/Properties/AssemblyInfo.cs
@@ -1,30 +1,4 @@
-﻿#region usings
+﻿using System.Reflection;
 
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-#endregion
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("NHibernateRepositories")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("NHibernateRepositories")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("3bbe2307-fd5b-42eb-b434-98cbaf0f1295")]
-[assembly: AssemblyVersion("1.7.1.0")]
+[assembly: AssemblyTitle("Composable.CQRS.NHibernate")]
+[assembly: AssemblyProduct("Composable.CQRS.NHibernate")]

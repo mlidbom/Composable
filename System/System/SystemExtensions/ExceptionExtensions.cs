565caecaacc8c75d7b0ca6c6aa4b5b20e55e0f26
mlidbo <mlidbo@manpower.se>
2012-05-24 11:00:35 +0200

    Add some extensions to help out with nested exceptions.

---
+++ System/System/SystemExtensions/ExceptionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Composable.SystemExtensions
+{
+    public static class ExceptionExtensions
+    {
+         public static IEnumerable<Exception> GetAllExceptionsInStack(this Exception e)
+         {
+             do
+             {
+                 yield return e;
+                 e = e.InnerException;
+             } while (e != null);
+         } 
+
+        public static Exception GetRootCauseException(this Exception e)
+        {
+            return e.GetAllExceptionsInStack().Last();
+        }
+    }
+}

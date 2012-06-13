7e6ec9fd7025dbed21a2872074a40b85af33fc92
mlidbo <mlidbo@manpower.se>
2012-06-13 19:10:07 +0200

    Hard-deprecate Composable.Automapper

--- AutoMapper/Composable.AutoMapper.Tests/WithoutConfiguration.cs
+++ AutoMapper/Composable.AutoMapper.Tests/WithoutConfiguration.cs
@@ -1,35 +1,35 @@
-#region usings
+//#region usings
 
-using Composable.DDD;
-using NUnit.Framework;
+//using Composable.DDD;
+//using NUnit.Framework;
 
-#endregion
+//#endregion
 
-namespace Composable.AutoMapper.Tests
-{
-    [TestFixture]
-    public class WithoutConfiguration
-    {
-        public struct StructA
-        {
-            public string Title { get; set; }
-        }
+//namespace Composable.AutoMapper.Tests
+//{
+//    [TestFixture]
+//    public class WithoutConfiguration
+//    {
+//        public struct StructA
+//        {
+//            public string Title { get; set; }
+//        }
 
-        public class ClassA : ValueObject<ClassA>
-        {
-            public string Val1 { get; set; }
-            public string Val2;
-        }
+//        public class ClassA : ValueObject<ClassA>
+//        {
+//            public string Val1 { get; set; }
+//            public string Val2;
+//        }
 
 
-        [Test]
-        public void MappingToSameTypeWorks()
-        {
-            var structA = new StructA { Title = "A" };
-            var classA = new ClassA { Val1 = "1", Val2 = "2" };
+//        [Test]
+//        public void MappingToSameTypeWorks()
+//        {
+//            var structA = new StructA { Title = "A" };
+//            var classA = new ClassA { Val1 = "1", Val2 = "2" };
 
-            Assert.That(structA.MapTo<StructA>(), Is.EqualTo(structA));
-            Assert.That(classA.MapTo<ClassA>(), Is.EqualTo(classA));
-        }
-    }
-}
+//            Assert.That(structA.MapTo<StructA>(), Is.EqualTo(structA));
+//            Assert.That(classA.MapTo<ClassA>(), Is.EqualTo(classA));
+//        }
+//    }
+//}
--- Composable.AutoMapper.DuplicateMappingTests/WhenMoreThanOneDefinitionOfAMappingExists.cs
+++ Composable.AutoMapper.DuplicateMappingTests/WhenMoreThanOneDefinitionOfAMappingExists.cs
@@ -1,53 +1,53 @@
-#region usings
+//#region usings
 
-using Castle.MicroKernel.Registration;
-using Castle.Windsor;
-using CommonServiceLocator.WindsorAdapter;
-using NUnit.Framework;
+//using Castle.MicroKernel.Registration;
+//using Castle.Windsor;
+//using CommonServiceLocator.WindsorAdapter;
+//using NUnit.Framework;
 
-#endregion
+//#endregion
 
-namespace Composable.AutoMapper.Tests
-{
-    [TestFixture]
-    public class WhenMoreThanOneDefinitionOfAMappingExists
-    {
-        [SetUp]
-        public void Setup()
-        {
-            var container = new WindsorContainer();
-            var locator = new WindsorServiceLocator(container);
-            container.Register(AllTypes.FromThisAssembly().BasedOn<IProvidesMappings>().WithService.Base());
-            ComposableMapper.Init(locator);
-        }
+//namespace Composable.AutoMapper.Tests
+//{
+//    [TestFixture]
+//    public class WhenMoreThanOneDefinitionOfAMappingExists
+//    {
+//        [SetUp]
+//        public void Setup()
+//        {
+//            var container = new WindsorContainer();
+//            var locator = new WindsorServiceLocator(container);
+//            container.Register(AllTypes.FromThisAssembly().BasedOn<IProvidesMappings>().WithService.Base());
+//            ComposableMapper.Init(locator);
+//        }
 
-        [TearDown]
-        public void TearDown()
-        {
-            ComposableMapper.ResetOnlyCallFromTests();
-        }
+//        [TearDown]
+//        public void TearDown()
+//        {
+//            ComposableMapper.ResetOnlyCallFromTests();
+//        }
 
-        public class MappingBootstrapper : IProvidesMappings
-        {
-            public void CreateMappings(SafeConfiguration configuration)
-            {
-                configuration.CreateMap<A, B>();
-                configuration.CreateMap<A, B>();
-            }
-        }
+//        public class MappingBootstrapper : IProvidesMappings
+//        {
+//            public void CreateMappings(SafeConfiguration configuration)
+//            {
+//                configuration.CreateMap<A, B>();
+//                configuration.CreateMap<A, B>();
+//            }
+//        }
 
-        [Test]
-        public void DuplicateMappingExceptionIsThrown()
-        {
-            Assert.Throws<DuplicateMappingException>(() => new A().MapTo<B>());
-        }
+//        [Test]
+//        public void DuplicateMappingExceptionIsThrown()
+//        {
+//            Assert.Throws<DuplicateMappingException>(() => new A().MapTo<B>());
+//        }
 
-        public class A
-        {
-        }
+//        public class A
+//        {
+//        }
 
-        public class B
-        {
-        }
-    }
-}
+//        public class B
+//        {
+//        }
+//    }
+//}

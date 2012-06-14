7241e5702aa3286366e76debb9a03b30b47ba6e0
mlidbo <mlidbo@manpower.se>
2012-06-14 13:49:29 +0200

    Make sure Composable.Automapper is actually composable.

--- AutoMapper/Composable.AutoMapper/ComposableMapper.cs
+++ AutoMapper/Composable.AutoMapper/ComposableMapper.cs
@@ -12,133 +12,72 @@ using Microsoft.Practices.ServiceLocation;
 
 namespace Composable.AutoMapper
 {
-    [Obsolete(ComposableAutomapperIsBeingRetiredDueToCausingABadCaseOfStaticitis, true)]
+    [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
     public static class ComposableMapper
-    {
-        private const string ComposableAutomapperIsBeingRetiredDueToCausingABadCaseOfStaticitis = "Composable.AutoMapper is being retired due to causing a bad case of staticitis";
+    {      
+        //todo:remove me
+        private const string ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis = 
+            "This member is being retired due to causing a bad case of staticitis. it will throw an ObsoletException if called! Please migrate to functionality in ComposableMappingEngine instead.";
 
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static void Init(Func<IMappingEngine> engineProvider)
         {
-            lock(LockObject)
-            {
-                if(_initialized)
-                {
-                    throw new Exception("You may only call Init once");
-                }
-                if(engineProvider == null)
-                {
-                    throw new ArgumentNullException("engineProvider");
-                }
-                _initialized = true;
-                _engineProvider = engineProvider;
-            }
+            throw new ObsoleteException();
         }
 
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static void Init(IServiceLocator locator)
         {
-            lock(LockObject)
-            {
-                if(_initialized)
-                {
-                    throw new Exception("You may only call Init once");
-                }
-                if(locator == null)
-                {
-                    throw new ArgumentNullException("locator");
-                }
-                _initialized = true;
-                _locator = locator;
-            }
+            throw new ObsoleteException();
         }
 
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static void ResetOnlyCallFromTests()
         {
-            lock(LockObject)
-            {
-                _initialized = false;
-                _locator = null;
-                _engineProvider = null;
-            }
+            throw new ObsoleteException();
         }
+        
 
-        private static readonly object LockObject = new Object();
-        private static Func<IMappingEngine> _engineProvider;
-        private static IServiceLocator _locator;
-        private static bool _initialized;
-
-        private static IMappingEngine Engine
-        {
-            get
-            {
-                if(!_initialized)
-                {
-                    throw new InvalidOperationException("You must call init before using MapTo");
-                }
-                if(_engineProvider == null)
-                {
-                    lock(LockObject)
-                    {
-                        if(_engineProvider == null)
-                        {
-                            _engineProvider = CreateDefaultProvider();
-                        }
-                    }
-                }
-                return _engineProvider();
-            }
-        }
-
-        private static Func<IMappingEngine> CreateDefaultProvider()
-        {
-            var configuration = new ConfigurationStore(new TypeMapFactory(), MapperRegistry.AllMappers());
-            var safeConfiguration = new SafeConfiguration(configuration);
-
-            foreach(var mappingCreator in _locator.GetAllInstances<IProvidesMappings>().ToArray())
-            {
-                mappingCreator.CreateMappings(safeConfiguration);
-            }
-
-            configuration.AssertConfigurationIsValid();
-
-            var engine = new MappingEngine(configuration);
-
-            return () => engine;
-        }
-
-        [Obsolete(ComposableAutomapperIsBeingRetiredDueToCausingABadCaseOfStaticitis, true)]
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static TTarget MapTo<TTarget>(this object me)
         {
-            return (TTarget)Engine.Map(me, me.GetType(), typeof(TTarget));
+            throw new ObsoleteException();
         }
 
-        [Obsolete(ComposableAutomapperIsBeingRetiredDueToCausingABadCaseOfStaticitis, true)]
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static IEnumerable<TTarget> MapTo<TTarget>(this IEnumerable me)
         {
-            return me.Cast<object>().Select(MapTo<TTarget>);
+            throw new ObsoleteException();
         }
 
-        [Obsolete(ComposableAutomapperIsBeingRetiredDueToCausingABadCaseOfStaticitis, true)]
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static object MapTo(this object me, Type targetType)
         {
-            return Engine.Map(me, me.GetType(), targetType);
+            throw new ObsoleteException();
         }
 
-        [Obsolete(ComposableAutomapperIsBeingRetiredDueToCausingABadCaseOfStaticitis, true)]
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static IEnumerable<object> MapTo(this IEnumerable me, Type targetType)
         {
-            return me.Cast<object>().Select(obj => obj.MapTo(targetType));
+            throw new ObsoleteException();
         }
 
-        [Obsolete(ComposableAutomapperIsBeingRetiredDueToCausingABadCaseOfStaticitis, true)]
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static void MapOnto<TSource, TTarget>(this TSource source, TTarget target)
         {
-            Engine.Map(source, target);
+            throw new ObsoleteException();
         }
 
-        [Obsolete(ComposableAutomapperIsBeingRetiredDueToCausingABadCaseOfStaticitis, true)]
+        [Obsolete(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis, false)]
         public static void MapDynamicOnto<TSource, TTarget>(this TSource source, TTarget target)
         {
-            Engine.DynamicMap(source, target);
+            throw new ObsoleteException();
+        }
+
+        public class ObsoleteException : Exception
+        {
+            public ObsoleteException() : base(ThisMemberIsBeingRetiredDueToCausingABadCaseOfStaticitis)
+            {}
         }
     }
 }

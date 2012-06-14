11cf6b846a76c4dcd5c5abe5451b8a95725273f2
mlidbo <mlidbo@manpower.se>
2012-06-14 13:49:29 +0200

    Make sure Composable.Automapper is actually composable.

--- AutoMapper/Composable.AutoMapper/IProvidesMappings.cs
+++ AutoMapper/Composable.AutoMapper/IProvidesMappings.cs
@@ -1,5 +1,8 @@
-﻿namespace Composable.AutoMapper
+﻿using System;
+
+namespace Composable.AutoMapper
 {
+    [Obsolete("Will be removed soon. Migrate away immediately!")]
     public interface IProvidesMappings
     {
         void CreateMappings(SafeConfiguration configuration);

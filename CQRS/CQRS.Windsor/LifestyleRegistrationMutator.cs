ba3b65dca99d20a17c6bf86b0d71aa597dfd10f0
fgoran <fgoran@manpower.se>
2012-06-20 10:50:10 +0200

    Added LifestyleRegistratonMutator and KeyReplacementHandlerSelector

---
+++ CQRS/CQRS.Windsor/LifestyleRegistrationMutator.cs
@@ -0,0 +1,27 @@
+﻿using Castle.Core;
+using Castle.MicroKernel;
+using Castle.MicroKernel.ModelBuilder;
+
+namespace Composable.CQRS.Windsor
+{
+    public class LifestyleRegistrationMutator : IContributeComponentModelConstruction
+    {
+        private readonly LifestyleType _originalLifestyle;
+        private readonly LifestyleType _newLifestyleType;
+
+        public LifestyleRegistrationMutator(
+            LifestyleType originalLifestyle = LifestyleType.PerWebRequest,
+            LifestyleType newLifestyleType = LifestyleType.Scoped)
+        {
+            _originalLifestyle = originalLifestyle;
+            _newLifestyleType = newLifestyleType;
+        }
+
+        public void ProcessModel(IKernel kernel,
+                                 ComponentModel model)
+        {
+            if (model.LifestyleType == _originalLifestyle)
+                model.LifestyleType = _newLifestyleType;
+        }
+    }
+}

c7790f5cb42f453dbdbb592cf83b96f17c1299a0
Magnus Lidbom <magnus.lidbom@gmail.com>
2012-07-02 16:34:51 +0200

    Add DefaultToKeyHandlerSelector

---
+++ CQRS/CQRS.Windsor/DefaultToKeyHandlerSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Castle.MicroKernel;
+
+namespace Composable.CQRS.Windsor
+{
+    public class DefaultToKeyHandlerSelector : IHandlerSelector
+    {
+        private readonly Type _type;
+        private readonly string _keyToDefaultTo;
+
+        public DefaultToKeyHandlerSelector(Type type, string keyToDefaultTo)
+        {
+            _type = type;
+            _keyToDefaultTo = keyToDefaultTo;
+        }
+
+        public bool HasOpinionAbout(string key, Type service)
+        {
+            return service == _type;
+        }
+
+        public IHandler SelectHandler(string key, Type service, IHandler[] handlers)
+        {
+            var handlerForDefaultKey = handlers.FirstOrDefault(handler => handler.ComponentModel.Name == _keyToDefaultTo);
+            if (handlerForDefaultKey == null)
+                return handlers.FirstOrDefault();
+
+            return handlerForDefaultKey;
+        }
+    }
+}

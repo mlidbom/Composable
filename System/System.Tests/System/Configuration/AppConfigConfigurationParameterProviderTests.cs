0c09c26a22d6f5c29fa053449393bfd2888551db
nwann <nwanng@manpowergroup.se>
2012-09-04 15:30:42 +0200

    Added AppConfig and ConnectionString ParameterProviders

---
+++ System/System.Tests/System/Configuration/AppConfigConfigurationParameterProviderTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using Composable.System.Configuration;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Composable.Tests.System.Configuration
+{
+    [TestFixture]
+    public class AppConfigConfigurationParameterProviderTests
+    {
+        [Test]
+        public void ParameterProvider_should_return_the_value_specified_in_the_configuration_file()
+        {
+            //Arrange
+            var provider = new AppConfigConfigurationParameterProvider();
+
+            //Assert
+            Assert.AreEqual(ConfigurationManager.AppSettings["KeyTest1"], provider.GetString("KeyTest1"));
+        }
+
+        [Test]
+        public void ParameterProvider_should_throw_ConfigurationErrorsException_when_key_does_not_exist()
+        {
+            //Arrange
+            var provider = new AppConfigConfigurationParameterProvider();
+
+            //Assert
+            Assert.Throws<ConfigurationErrorsException>(() => provider.GetString("ErrorTest1"));
+        }
+
+        [Test]
+        public void ParameterProvider_exception_should_contain_parameter_key()
+        {
+            //Arrange
+            var parameterKey = "ErrorTest1";
+            var provider = new AppConfigConfigurationParameterProvider();
+            
+            //Assert
+            var exc=Assert.Throws<ConfigurationErrorsException>(() => provider.GetString(parameterKey));
+            exc.Message.Should().Contain(parameterKey);
+        }
+    }
+}

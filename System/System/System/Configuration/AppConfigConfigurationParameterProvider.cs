fb13db23dd97df237ac9011eb91ab06e82da9bd3
nwann <nwanng@manpowergroup.se>
2012-09-04 15:30:42 +0200

    Added AppConfig and ConnectionString ParameterProviders

---
+++ System/System/System/Configuration/AppConfigConfigurationParameterProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace Composable.System.Configuration
+{
+    public class AppConfigConfigurationParameterProvider : IConfigurationParameterProvider
+    {
+        public string GetString(string parameterName)
+        {
+            var parameter = ConfigurationManager.AppSettings[parameterName];
+            if (parameter==null)
+            {
+                throw new ConfigurationErrorsException(string.Format("ApplicationSettings Parameter {0} does not exists",parameterName));
+            }
+            return parameter;
+        }
+    }
+}
---
+++ System/System/System/Configuration/ConnectionStringConfigurationParameterProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+
+namespace Composable.System.Configuration
+{
+    public class ConnectionStringConfigurationParameterProvider : IConnectionStringProvider
+    {
+        public ConnectionStringSettings GetConnectionString(string parameterName)
+        {
+            var parameter = ConfigurationManager.ConnectionStrings[parameterName];
+            if (parameter==null)
+            {
+                throw new ConfigurationErrorsException(string.Format("ConnectionString with name {0} does not exists",parameterName));
+            }
+            return parameter;
+        }
+    }
+}

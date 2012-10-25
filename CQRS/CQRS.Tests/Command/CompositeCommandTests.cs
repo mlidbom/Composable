3cd098e1d92552abf8b2588e29c7ffb461e7009a
lsandb <leif.sandberg@manpower.se>
2012-10-25 10:53:15 +0200

    Yehaa now subcommands pass correct paths when throwing commandfailedexceptions.

--- CQRS/CQRS.Tests/Command/CompositeCommandTests.cs
+++ CQRS/CQRS.Tests/Command/CompositeCommandTests.cs
@@ -1,57 +1,145 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using CommonServiceLocator.WindsorAdapter;
+using Composable.CQRS;
 using Composable.CQRS.Command;
 using FluentAssertions;
+using Microsoft.Practices.ServiceLocation;
 using NUnit.Framework;
+using Composable.System.Linq;
 
 namespace CQRS.Tests.Command
 {
     [TestFixture]
     public class CompositeCommandTests
     {
+        private WindsorContainer _container;
+        private WindsorServiceLocator _locator;
+        private IServiceLocator Locator { get { return _locator; } }
+
+        [SetUp]
+        public void Setup()
+        {
+            _container = new WindsorContainer();
+            _locator = new WindsorServiceLocator(_container);
+            _container.Register(
+                Component.For<ICommandService>().ImplementedBy<CommandService>(),
+                Component.For<IServiceLocator>().Instance(Locator),
+                Component.For<ICommandHandler<EditCommand>>().ImplementedBy<EditCommandHandler>()
+                );
+
+            EditCommandHandler.ExecuteCalled = false;
+        }
 
         [Test]
         public void ShouldFindTheCommandsInCompositeCommand()
         {
-            var compositeCommand = new EditThisAndEditSomethingElse();
-            var subCommand = new SubCommand(() => compositeCommand);
-            var result = compositeCommand.GetContainedCommands().ToList();
+            var somethingAndEditCommand = new SomethingAndEditCommand
+                                          {
+                                              EditCommand = new EditCommand(),
+                                              SomethingCommand = new SomethingCommand()
+                                          };
+
+            var result = somethingAndEditCommand.GetContainedCommands().ToList();
+
+
             result.Should().HaveCount(2);
-            result.Should().Contain(c => c.Name == "EditSomething" && c.Command is EditSomethingCommand);
+            result.Should().Contain(c => c.Name == "SomethingCommand" && c.Command is SomethingCommand);
         }
 
-        public class EditThisAndEditSomethingElse : CompositeCommand
+        [Test]
+        public void CompositeCommandsThatFailsShouldBuildCorrectMemberPath()
         {
-            public EditSomethingCommand EditSomething { get; private set; }
-            public EditSomethingElseCommand EditSomethingElse { get; private set; }
+            var somethingAndEditCommand = new SomethingAndEditCommand
+                                          {
+                                              EditCommand = new EditCommand(),
+                                              SomethingCommand = new SomethingCommand()
+                                          };
 
-            public EditThisAndEditSomethingElse()
-            {
-                EditSomething = new EditSomethingCommand();
-                EditSomethingElse = new EditSomethingElseCommand();
-            }
+            //Execute command
+            var result = Assert.Throws<CommandFailedException>(() => _container.Resolve<ICommandService>().Execute(somethingAndEditCommand));
+
+            //CommandService should now rethrow a CommandFailedException with correct member path
+            result.Should().BeOfType<CommandFailedException>();
+            result.Message.Should().Be("EditedField is invalid");
+            result.InvalidMembers.Should().HaveCount(1);
+            result.InvalidMembers.First().Should().Be("EditCommand.EditedField");
+        }
+
+        [Test]
+        public void CompositeCommandsThatFailsShouldBuildCorrectMemberPathWhenMultipleFieldAreInvalid()
+        {
+            var somethingAndEditCommand = new SomethingAndEditCommand
+                                          {
+                                              EditCommand = new EditCommand()
+                                                            {
+                                                                BothInvalid = true
+                                                            },
+                                              SomethingCommand = new SomethingCommand()
+                                          };
+
+            //Execute command
+            var result = Assert.Throws<CommandFailedException>(() => _container.Resolve<ICommandService>().Execute(somethingAndEditCommand));
+
+            //CommandService should now rethrow a CommandFailedException with correct member path
+            result.Should().BeOfType<CommandFailedException>();
+            result.Message.Should().Be("EditedField and otherfield is invalid");
+            result.InvalidMembers.Should().HaveCount(2);
+            result.InvalidMembers.First().Should().Be("EditCommand.EditedField");
+            result.InvalidMembers.Second().Should().Be("EditCommand.OtherEditedField");
+        }
+
+        public class SomethingAndEditCommand : CompositeCommand
+        {
+            public EditCommand EditCommand { private get; set; }
+            public SomethingCommand SomethingCommand { private get; set; }
 
             override public IEnumerable<SubCommand> GetContainedCommands()
             {
-                return new List<SubCommand>()
-                   {
-                       new SubCommand(() => EditSomething),
-                       new SubCommand(() => EditSomethingElse)
-                   };
+                return new List<SubCommand>
+                       {
+                           new SubCommand(() => EditCommand),
+                           new SubCommand(() => SomethingCommand)
+                       };
             }
 
-            //TODO: remove. Needed until CVM is cleaned up 
             override public IEnumerable<Composable.CQRS.Command.Command> GetContainedCommandsOld()
             {
-                throw new System.NotImplementedException();
+                //TODO: remove when BatchCommand is deleted
+                throw new NotImplementedException();
             }
         }
 
-        public class EditSomethingElseCommand : Composable.CQRS.Command.Command { }
-
-        public class EditSomethingCommand : Composable.CQRS.Command.Command
+        public class EditCommand : Composable.CQRS.Command.Command
         {
+            public bool BothInvalid { get; set; }
+            public string EditedField { get; set; }
+            public string OtherEditedField { get; set; }
+        }
 
+        public class SomethingCommand : Composable.CQRS.Command.Command {}
+
+        public class EditCommandHandler : ICommandHandler<EditCommand>
+        {
+            public static bool ExecuteCalled { get; set; }
+
+            public void Execute(EditCommand command)
+            {
+                ExecuteCalled = true;
+                if(command.BothInvalid)
+                {
+                    throw new CommandFailedException("EditedField and otherfield is invalid",
+                                                     () => command.EditedField,
+                                                     () => command.OtherEditedField);
+                }
+                else
+                {
+                    throw new CommandFailedException("EditedField is invalid", () => command.EditedField);
+                }
+            }
         }
     }
 }

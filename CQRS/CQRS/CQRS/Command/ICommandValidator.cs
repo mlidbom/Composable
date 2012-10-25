7ee0d033abf7427d3a9aa9c18f3edebd788be46a
Magnus Lidbom <magnus.lidbom@gmail.com>
2012-10-25 16:54:48 +0200

    Add some UsedImplicitly attributes to improve resharper code analysis

--- CQRS/CQRS/CQRS/Command/ICommandValidator.cs
+++ CQRS/CQRS/CQRS/Command/ICommandValidator.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using JetBrains.Annotations;
 
 namespace Composable.CQRS.Command
 {
+    [UsedImplicitly]
     public interface ICommandValidator<in TCommand>
     {
         IEnumerable<IValidationFailure> Validate(TCommand command);

e9533934dd273dd27ea1b7ac183518cbe8c9f81d
Magnus Lidbom <magnus.lidbom@gmail.com>
2012-11-23 15:21:13 +0100

    Stop using a common assemblyinfo for version settings and bump changed..

--- CQRS/NHibernateRepositories/Properties/AssemblyInfo.cs
+++ CQRS/NHibernateRepositories/Properties/AssemblyInfo.cs
@@ -2,3 +2,5 @@
 
 [assembly: AssemblyTitle("Composable.CQRS.NHibernate")]
 [assembly: AssemblyProduct("Composable.CQRS.NHibernate")]
+
+[assembly: AssemblyVersion("1.7.17.0")]
--- Composable.DomainEvents/Properties/AssemblyInfo.cs
+++ Composable.DomainEvents/Properties/AssemblyInfo.cs
@@ -2,3 +2,5 @@
 
 [assembly: AssemblyTitle("Composable.DomainEvents")]
 [assembly: AssemblyProduct("Composable.DomainEvents")]
+
+[assembly: AssemblyVersion("1.7.17.0")]

04f2edc07fded47b26d62a69924ab9fb8066f55b
Magnus Lidbom <magnus.lidbom@gmail.com>
2013-02-13 15:37:24 +0100

    Remove obsolete population projects

--- CQRS/Composable.CQRS.Population.Server/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Composable.CQRS.Population.Server")]
-[assembly: AssemblyProduct("Composable.CQRS.Population.Server")]
-
-[assembly: AssemblyVersion("1.8.4.0")]

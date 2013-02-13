e295f0d6b8fc75f12e933ef66a1171e6a3ee5514
Magnus Lidbom <magnus.lidbom@gmail.com>
2013-02-13 15:53:10 +0100

    Implement base functionality for repopulating viewmodels from events.

---
+++ CQRS/CQRS/KeyValueStorage/Population/ViewModelBatchPopulator.cs
@@ -0,0 +1,49 @@
+﻿using Castle.Windsor;
+using System;
+using System.Threading.Tasks;
+using Composable.System.Linq;
+using JetBrains.Annotations;
+
+namespace Composable.KeyValueStorage.Population
+{
+    [UsedImplicitly]
+    public class ViewModelBatchPopulator : IViewModelBatchPopulator
+    {
+
+        private readonly IWindsorContainer _container;
+        private readonly IPopulationBatchLogger _logger;
+
+        public ViewModelBatchPopulator(IWindsorContainer container, IPopulationBatchLogger logger)
+        {
+            _container = container;
+            _logger = logger;
+        }       
+
+        public void PopulateEntities(params Guid[] aggregateRootIds)
+        {
+           _logger.Initialize(aggregateRootIds.Length);
+
+            Parallel.ForEach(
+                aggregateRootIds,
+                entityId =>
+                {
+                    try
+                    {
+                        using(var unitOfWork = _container.BeginTransactionalUnitOfWorkScope())
+                        {
+                            _container.ResolveAll<IViewModelPopulator>()
+                                      .ForEach(populator => populator.Populate(entityId));
+                            
+                            unitOfWork.Commit();                            
+                        }
+                        _logger.LogAggregateHandled(entityId);
+                    }
+                    catch(Exception e)
+                    {
+                        _logger.LogError(e, entityId);
+                    }
+                });
+        }      
+
+    }
+}

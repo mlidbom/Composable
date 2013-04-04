96039d3ec984d9fcf667e9c3e63dd7214115acc2
mlidbo <magnus.lidbom@gmail.com>
2013-04-04 15:15:02 +0200

    Use interface rather than class to mark commands.

--- CQRS/CQRS/CQRS/Command/CommandFailed.cs
+++ CQRS/CQRS/CQRS/Command/CommandFailed.cs
@@ -4,6 +4,15 @@ namespace Composable.CQRS.Command
 {
     public abstract class CommandFailed : Event
     {
+        [Obsolete("This is only for serialization", true)]
+        public CommandFailed()
+        {}
+
+        public CommandFailed( Guid commandId, string message)
+        {
+            
+        }
+
         public Guid CommandId { get; set; }
         public string Message { get; set; }
     }

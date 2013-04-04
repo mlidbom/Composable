75aa124fe28414169ed5360d7cd495dbac53a670
Niklas Arbin <niklas.arbin@ffcg.se>
2013-04-04 22:11:41 +0200

    command success and failed are now messages not events

--- CQRS/CQRS/CQRS/Command/CommandFailed.cs
+++ CQRS/CQRS/CQRS/Command/CommandFailed.cs
@@ -2,7 +2,7 @@
 
 namespace Composable.CQRS.Command
 {
-    public abstract class CommandFailed : Event
+    public abstract class CommandFailed : ICommandMessage
     {
         public Guid CommandId { get; set; }
         public string Message { get; set; }

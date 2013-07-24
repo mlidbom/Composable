bbceb61d0c8b0e0f788f9ef3313c5f525f328ff4
mlidbo <magnus.lidbom@gmail.com>
2013-07-24 22:37:05 +0200

    extract interface IDocumentUpdatedNotifier

--- CQRS/CQRS/KeyValueStorage/IObservableObjectStore.cs
+++ CQRS/CQRS/KeyValueStorage/IObservableObjectStore.cs
@@ -1,9 +1,6 @@
-using System;
-using Composable.KeyValueStorage.SqlServer;
-
 namespace Composable.KeyValueStorage
 {
-    public interface IObservableObjectStore : IObservable<IDocumentUpdated>, IObjectStore
+    public interface IObservableObjectStore : IObjectStore, IDocumentUpdatedNotifier
     {
         
     }

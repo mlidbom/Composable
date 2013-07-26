34bd826824fb378f51d57fa502b8967533cd4822
mlidbo <magnus.lidbom@gmail.com>
2013-07-26 17:23:56 +0200

    Bump domain events.

--- Composable.DomainEvents/Properties/AssemblyInfo.cs
+++ Composable.DomainEvents/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Composable.DomainEvents")]
 [assembly: AssemblyProduct("Composable.DomainEvents")]
 
-[assembly: AssemblyVersion("1.8.4.0")]
+[assembly: AssemblyVersion("1.8.5.0")]

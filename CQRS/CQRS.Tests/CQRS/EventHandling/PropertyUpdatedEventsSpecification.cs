962e238c8c605a7d347c8974660cdf1b91f2bc4c
mlidbo <magnus.lidbom@gmail.com>
2013-07-28 16:09:18 +0200

    A decent example of how to work with domain events and inheritance completed.

--- CQRS/CQRS.Tests/CQRS/EventHandling/PropertyUpdatedEventsSpecification.cs
+++ CQRS/CQRS.Tests/CQRS/EventHandling/PropertyUpdatedEventsSpecification.cs
@@ -6,6 +6,9 @@ using Composable.DDD;
 using Composable.KeyValueStorage;
 using Composable.NewtonSoft;
 using Composable.System.Linq;
+using CQRS.Tests.CQRS.EventHandling.CVManagement;
+using CQRS.Tests.CQRS.EventHandling.CVManagement.GlobalEvents;
+using CQRS.Tests.CQRS.EventHandling.CVManagement.InternalEvents.InternalImplementations;
 using FluentAssertions;
 using Moq;
 using Newtonsoft.Json;
@@ -13,55 +16,62 @@ using NUnit.Framework;
 
 namespace CQRS.Tests.CQRS.EventHandling
 {
-    public static class CVPropertyUpdated
+    namespace CVManagement
     {
-        public interface IEmail : PropertyUpdatedEventsSpecification.ICVEvent
+        namespace GlobalEvents
         {
-            string Email { get; set; }
-        }
+            #region Generic events intended to be inherited. None of these should ever be raised. Only inheritors should be raised.
+            
+            //Every single CV event should inherit this one. Directly or indirectly.
+            public interface ICVEvent : IAggregateRootEvent {}
 
-        public interface IPassword : PropertyUpdatedEventsSpecification.ICVEvent
-        {
-            string Password { get; set; }
-        }
+            //Note how this event inherits IAggregateRootCreatedEvent. This allows SingleAggregateQueryModelUpdater and others to automatically know that it is a creation event.
+            //Any event that signifies that a CV has been created should inherit this event
+            //That way SingleAggregateQueryModelUpdater will not try to read the model from the DB and it can automatically instantiate the model with the correct aggregate root id.
+            public interface ICVCreated : ICVEvent, IAggregateRootCreatedEvent {}
 
-        public interface ISkills : PropertyUpdatedEventsSpecification.ICVEvent
-        {
-            List<string> AddedSkills { get; set; }
-            List<string> RemovedSkills { get; set; }
-        }
-    }
+            //Note how this event inherits IAggregateRootDeletedEvent. This allows SingleAggregateQueryModelUpdater and others to automatically know that it is a deletion event.
+            //That way SingleAggregateQueryModelUpdater will know that it can go right ahead and delete the model.
+            public interface ICVDeleted : ICVEvent, IAggregateRootDeletedEvent {}
 
-    public class PropertyUpdatedEventsSpecification : NSpec.NUnit.nspec
-    {
-        public interface ICVEvent : IAggregateRootEvent {}
+            //Should be inherited by any event that is triggered by the candidate editing his/her CV
+            public interface ICVUpdatedByOwner : ICVEvent {}
 
-        //Property updated events. There should be one per property or collection.
-        //For collections they should handle collections. Not Items.
+            //Should be inherited by any event that is triggered by a recruiter acting on the CV
+            public interface ICVUpdatedByRecruiter : ICVEvent
+            {
+                Guid RecruiterId { get; set; }
+            }
 
-        public interface ICVUpdatedByRecruiter : ICVEvent
-        {
-            Guid RecruiterId { get; set; }
-        }
+            //Property updated events. There should be one per property or collection.            
+            //All other events MUST inherit each PropertyUpdated event that applies. They should NOT add properties that are part of the aggregate in any way but by inheriting
+            //a PropertyUpdated event.
+            namespace PropertyUpdated
+            {
+                public interface ICVEmailPropertyUpdated : ICVEvent
+                {
+                    string Email { get; set; }
+                }
 
-        public interface ICVSkillsEditedByRecruiter : ICVUpdatedByRecruiter, CVPropertyUpdated.ISkills { }
-        
-        public interface ICVCreated : ICVEvent, IAggregateRootCreatedEvent {}
+                public interface ICVPasswordPropertyUpdated : ICVEvent
+                {
+                    string Password { get; set; }
+                }
 
-        public interface ICVRegistered : ICVCreated, CVPropertyUpdated.IEmail, CVPropertyUpdated.IPassword {}
+                //For collections PropertyUpdated events should handle List<T>. Not Items. 
+                //They should have both Added* and Removed* properties.
+                public interface ICVSkillsPropertyUpdated : ICVEvent
+                {
+                    List<string> AddedSkills { get; set; }
+                    List<string> RemovedSkills { get; set; }
+                }
+            }
 
-        public interface ICVSkillsEditedByCandidate : CVPropertyUpdated.ISkills {}        
+            #endregion
 
-        public class CVRegisteredEvent : AggregateRootEvent, ICVRegistered
-        {
-            public string Email { get; set; }
-            public string Password { get; set; }
-        }
-
-        public class CVSkillsEdited : AggregateRootEvent, ICVSkillsEditedByCandidate
-        {
-            public List<string> AddedSkills { get; set; }
-            public List<string> RemovedSkills { get; set; }
+            public interface ICVRegistered : ICVCreated, PropertyUpdated.ICVEmailPropertyUpdated, PropertyUpdated.ICVPasswordPropertyUpdated {}
+            public interface ICVSkillsEditedByCandidate : PropertyUpdated.ICVSkillsPropertyUpdated, ICVUpdatedByOwner {}
+            public interface ICVSkillsEditedByRecruiter : PropertyUpdated.ICVSkillsPropertyUpdated, ICVUpdatedByRecruiter {}         
         }
 
         public class UserQueryModel : ValueObject<UserQueryModel>, IHasPersistentIdentity<Guid>
@@ -74,27 +84,49 @@ namespace CQRS.Tests.CQRS.EventHandling
 
         public class UserQueryModelUpdater : SingleAggregateQueryModelUpdater<UserQueryModelUpdater, UserQueryModel, ICVEvent, IDocumentDbSession>
         {
-            public UserQueryModelUpdater(IDocumentDbSession session) : base(session)
+            public UserQueryModelUpdater(IDocumentDbSession session)
+                : base(session)
             {
                 RegisterHandlers()
-                    .For<CVPropertyUpdated.IEmail>(e => Model.Email = e.Email)
-                    .For<CVPropertyUpdated.IPassword>(e => Model.Password = e.Password)
-                    .For<CVPropertyUpdated.ISkills>(e =>
-                                                   {
-                                                       Model.Skills.RemoveWhere(skill => e.RemovedSkills.Contains(skill));
-                                                       Model.Skills.AddRange(e.AddedSkills);
-                                                   });
+                    .For<CVManagement.GlobalEvents.PropertyUpdated.ICVEmailPropertyUpdated>(e => Model.Email = e.Email)
+                    .For<CVManagement.GlobalEvents.PropertyUpdated.ICVPasswordPropertyUpdated>(e => Model.Password = e.Password)
+                    .For<CVManagement.GlobalEvents.PropertyUpdated.ICVSkillsPropertyUpdated>(e =>
+                    {
+                        Model.Skills.RemoveRange(e.RemovedSkills);
+                        Model.Skills.AddRange(e.AddedSkills);
+                    });
             }
         }
 
+        namespace InternalEvents
+        {
+            namespace InternalImplementations
+            {
+                public class CVRegisteredEvent : AggregateRootEvent, ICVRegistered
+                {
+                    public string Email { get; set; }
+                    public string Password { get; set; }
+                }
+
+                public class CVSkillsEdited : AggregateRootEvent, ICVSkillsEditedByCandidate
+                {
+                    public List<string> AddedSkills { get; set; }
+                    public List<string> RemovedSkills { get; set; }
+                }
+            }
+        }
+    }
+
+    public class PropertyUpdatedEventsSpecification : NSpec.NUnit.nspec
+    {
         [Test]
         public void TestSerilizationOfCollectionPropertyUpdatedEvents()
         {
             var skillsEdited = new CVSkillsEdited()
-                                          {
-                                              AddedSkills = new List<string>() { "AddedSkill1", "AddedSkill2", "AddedSkill3" },
-                                              RemovedSkills = new List<string>() {}
-                                          };
+                               {
+                                   AddedSkills = new List<string>() {"AddedSkill1", "AddedSkill2", "AddedSkill3"},
+                                   RemovedSkills = new List<string>() {}
+                               };
 
             Console.WriteLine(JsonConvert.SerializeObject(skillsEdited, JsonSettings.JsonSerializerSettings));
         }
@@ -105,7 +137,6 @@ namespace CQRS.Tests.CQRS.EventHandling
             UserQueryModelUpdater userQueryModelUpdater = null;
             Mock<IDocumentDbSession> documentDbSessionMock = null;
             Guid cvId = Guid.Parse("00000000-0000-0000-0000-000000000001");
-
 
             before = () =>
                      {

db7cd33cd487294089bca5c5e7fbce68de480eba
mlidbo <magnus.lidbom@gmail.com>
2013-08-16 17:21:48 +0200

    Add extension method for testing that all components can be resolved.

--- CQRS/CQRS.Windsor/Testing/TestingWindsorExtensions.cs
+++ CQRS/CQRS.Windsor/Testing/TestingWindsorExtensions.cs
@@ -1,7 +1,12 @@
-﻿using Castle.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Core;
 using Castle.MicroKernel.Lifestyle;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
+using Composable.System;
+using Composable.System.Linq;
 using Composable.SystemExtensions.Threading;
 
 namespace Composable.CQRS.Windsor.Testing
@@ -35,6 +40,78 @@ namespace Composable.CQRS.Windsor.Testing
             {
                 configurer.ConfigureWiringForTesting();
                 container.Release(configurer);
+            }
+        }
+
+        public static void AssertCanResolveAllComponents(this IWindsorContainer container, IEnumerable<Type> ignoredServices = null, IEnumerable<string> ignoredComponents = null)
+        {
+            var errorsOccured = false;
+            ignoredServices = (ignoredServices ?? new Type[] { }).ToList();
+            ignoredComponents = (ignoredComponents ?? new string[] { }).ToList();
+            using (container.BeginScope())
+            {
+                foreach (var handler in container.Kernel.GetAssignableHandlers(typeof(object)).Distinct())
+                {
+                    if (ignoredComponents.Any(ignored => handler.ComponentModel.ComponentName.Name == ignored))
+                    {
+                        Console.WriteLine(@"Ignoring component: {0}", ignoredComponents.Single(ignored => handler.ComponentModel.ComponentName.Name == ignored));
+                        continue;
+                    }
+
+                    Console.WriteLine("Resolving services for component: {0}", handler.ComponentModel.Name);
+
+                    foreach (var service in handler.ComponentModel.Services)
+                    {
+                        if (ignoredServices.Any(ignored => ignored == service))
+                        {
+                            Console.WriteLine(@"    Ignoring service: {0}", ignoredServices.Single(ignored => ignored == service));
+                            continue;
+                        }
+
+                        Console.WriteLine(@"    Resolving all {0} ", service.FullName);
+                        try
+                        {
+                            var resolved = container.ResolveAll(service).Cast<Object>().Select(s => s.GetType().FullName).OrderBy(s => s);
+                            resolved.ForEach((name, index) => Console.WriteLine(@"	Resolved {0} {1}", index + 1, name));
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(@"############################## {0} ##########################", @"Failed to resolve component");
+                            Console.WriteLine(@"##############################Component Name: {0} ##########################", handler.ComponentModel.Name);
+                            Console.WriteLine(@"##############################Service Type: {0} ##########################", service.FullName);
+                            Console.WriteLine(CreateStackTrace(e));
+                            Console.WriteLine();
+                            errorsOccured = true;
+                        }
+                    }
+                }
+            }
+            if (errorsOccured)
+            {
+                throw new Exception("There were errors resolving components. Please see the printed call stacks above this one.");
+            }
+        }
+
+        private static string CreateStackTrace(Exception exception)
+        {
+            return GetNestedExceptionsList(exception)
+                .Reverse()
+                .Select(currentException => string.Format("Exception:{1}{0}Message:{2}{0}{0}{3}",
+                    Environment.NewLine,
+                    currentException.GetType().FullName,
+                    currentException.Message,
+                    currentException.StackTrace))
+                .Join(string.Format("{0}   ---End of inner stack trace---{0}{0}", Environment.NewLine));
+        }
+
+        private static IEnumerable<Exception> GetNestedExceptionsList(Exception exception)
+        {
+            yield return exception;
+            while (exception.InnerException != null)
+            {
+                yield return exception.InnerException;
+                exception = exception.InnerException;
             }
         }
     }

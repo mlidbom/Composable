962e37ad1c08c433fbd1cc7488ec585773180e08
mlidbo <magnus.lidbom@gmail.com>
2013-08-16 16:07:11 +0200

    Register SingleThreadUseGuard as the ISingleContextUseGuard when creating wiring for tests.

--- CQRS/CQRS.Windsor/Properties/AssemblyInfo.cs
+++ CQRS/CQRS.Windsor/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Composable.CQRS.Windsor")]
 [assembly: AssemblyProduct("Composable.CQRS.Windsor")]
 
-[assembly: AssemblyVersion("1.8.9.0")]
+[assembly: AssemblyVersion("1.8.10.0")]

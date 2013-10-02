046e4adc8a314951d0a27b77ef0c95557c1b2c54
mlidbo <magnus.lidbom@gmail.com>
2013-10-02 14:54:59 +0200

    Ensure that replies work correctly on the dual dispatch bus.

--- CQRS/Composable.CQRS.ServiceBus.NServiceBus/DualDispatchBus.cs
+++ CQRS/Composable.CQRS.ServiceBus.NServiceBus/DualDispatchBus.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Composable.ServiceBus;
+using Composable.SystemExtensions.Threading;
 using NServiceBus;
 
 namespace Composable.CQRS.ServiceBus.NServiceBus
@@ -10,15 +11,24 @@ namespace Composable.CQRS.ServiceBus.NServiceBus
         private readonly NServiceBusServiceBus _realBus;
 
         private bool _dispatchingOnSynchronousBus = false;
+        private readonly SingleThreadUseGuard _usageGuard;
 
         public DualDispatchBus(SynchronousBus local, NServiceBusServiceBus realBus)
         {
+            _usageGuard = new SingleThreadUseGuard();
             _local = local;
             _realBus = realBus;
         }
 
         private void DispatchOnSynchronousBus(Action action)
         {
+            _usageGuard.AssertNoContextChangeOccurred(this);
+            if(_dispatchingOnSynchronousBus)
+            {
+                action();
+                return;
+            }
+
             _dispatchingOnSynchronousBus = true;
             using (new DisposeAction(() => _dispatchingOnSynchronousBus = false))
             {
@@ -28,6 +38,7 @@ namespace Composable.CQRS.ServiceBus.NServiceBus
 
         public void Publish(object message)
         {
+            _usageGuard.AssertNoContextChangeOccurred(this);
             DispatchOnSynchronousBus(() => _local.Publish(message));
             _realBus.Publish(message);
         }
@@ -39,6 +50,7 @@ namespace Composable.CQRS.ServiceBus.NServiceBus
 
         public void Send(object message)
         {
+            _usageGuard.AssertNoContextChangeOccurred(this);
             //There can only be one handler in a send scenario and we let the synchronous bus get the first pick.
             if(_local.Handles((IMessage)message))
             {
@@ -52,6 +64,7 @@ namespace Composable.CQRS.ServiceBus.NServiceBus
 
         public void Reply(object message)
         {
+            _usageGuard.AssertNoContextChangeOccurred(this);
             //I cannot come up with a way to decide whether to dispatch to the real bus or the sync bus except for just giving it to the sync bus if it wants it...
             if(_dispatchingOnSynchronousBus)
             {

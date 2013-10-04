c6623091baf1f7269e60d6d75834f25d64edbe6c
mlidbo <magnus.lidbom@gmail.com>
2013-10-04 14:59:46 +0200

    Add ThreadCultureScope class and bump.

---
+++ System/System/System/Globalization/ThreadCultureScope.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Composable.System.Globalization
+{
+    public class ThreadCultureScope : IDisposable
+    {
+        private readonly CultureInfo _originalCulture;
+        private readonly CultureInfo _originalUICulture;
+
+        public ThreadCultureScope(string languageCode) : this(new CultureInfo(languageCode))
+        {
+            
+        }
+
+        private ThreadCultureScope(CultureInfo cultureInfo)
+        {
+            _originalCulture = Thread.CurrentThread.CurrentCulture;
+            _originalUICulture = Thread.CurrentThread.CurrentUICulture;
+
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        }
+
+        public void Dispose()
+        {
+            Thread.CurrentThread.CurrentCulture = _originalCulture;
+            Thread.CurrentThread.CurrentUICulture = _originalUICulture;
+        }
+    }
+}

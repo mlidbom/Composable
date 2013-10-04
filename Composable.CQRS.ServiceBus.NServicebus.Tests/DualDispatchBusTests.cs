242563b956b9a96024eb05c2a4298a7ef66dd2d4
mlidbo <magnus.lidbom@gmail.com>
2013-10-04 13:35:22 +0200

    Write a test that verifies that the dual dispatch bus handles retries correctly when running into reentrancy and bump.

--- Composable.CQRS.ServiceBus.NServicebus.Tests/DualDispatchBusTests.cs
+++ Composable.CQRS.ServiceBus.NServicebus.Tests/DualDispatchBusTests.cs
@@ -18,38 +18,54 @@ namespace Composable.CQRS.ServiceBus.NServicebus.Tests
         [Test]
         public void WhenReplyingWithinAHandlerCalledByTheSynchronousBusTheReplyGoesToTheSynchronousBus()
         {
-            var container = new WindsorContainer();
-            container.Register(                
-                Component.For<IBus>().Instance(new Mock<IBus>(MockBehavior.Strict).Object),
-                Component.For<ISingleContextUseGuard>().ImplementedBy<SingleThreadUseGuard>(),
-                Component.For<SynchronousBus>(),
-                Component.For<NServiceBusServiceBus>(),
-                Component.For<IServiceBus>().ImplementedBy<DualDispatchBus>().LifestyleScoped(),
-                Component.For<IHandleMessages<ACommand>>().ImplementedBy<ACommandHandler>(),
-                Component.For<ACommandReplyHandler,IHandleMessages<ACommandReply>>().ImplementedBy<ACommandReplyHandler>()
-                );
-
-            container.Register(Component.For<IWindsorContainer>().Instance(container));
+            var container = WireUpDualDispatchBusWithAllLocalMessageHandlersRegisteredAndAStrictMockForTheRealIBus();
 
             using(container.BeginScope())
             {
-                container.Resolve<IServiceBus>().Send(new ACommand());
+                container.Resolve<IServiceBus>().Send(new ReplyCommand());
                 container.Resolve<ACommandReplyHandler>().ReplyReceived.Should().Be(true);
-
             }
         }
 
-        public class ACommandReplyHandler : IHandleMessages<ACommandReply>
+        [Test]
+        public void WhenReplyingWithinAHandlerCalledByTheSynchronousBusInFashionCausingReentrancyTheReplyGoesToTheSynchronousBus()
         {
-            public void Handle(ACommandReply message)
+            var container = WireUpDualDispatchBusWithAllLocalMessageHandlersRegisteredAndAStrictMockForTheRealIBus();
+
+            using (container.BeginScope())
+            {
+                container.Resolve<IServiceBus>().Send(new SendAMessageAndThenReplyCommand());
+                container.Resolve<ACommandReplyHandler>().ReplyReceived.Should().Be(true);                
+            }
+        }
+
+        public class ACommandReplyHandler : IHandleMessages<ReplyMessage>
+        {
+            public bool ReplyReceived { get; set; }
+
+            public void Handle(ReplyMessage message)
             {
                 ReplyReceived = true;
             }
-
-            public bool ReplyReceived { get; set; }
         }
 
-        public class ACommandHandler : IHandleMessages<ACommand>
+        public class SendAMessageAndThenReplyCommandCommandHandler : IHandleMessages<SendAMessageAndThenReplyCommand>
+        {
+            private readonly IServiceBus _bus;
+
+            public SendAMessageAndThenReplyCommandCommandHandler(IServiceBus bus)
+            {
+                _bus = bus;
+            }
+
+            public void Handle(SendAMessageAndThenReplyCommand message)
+            {
+                _bus.Send(new Message());
+                _bus.Reply(new ReplyCommand());
+            }
+        }
+
+        public class ACommandHandler : IHandleMessages<ReplyCommand>
         {
             private readonly IServiceBus _bus;
 
@@ -58,14 +74,36 @@ namespace Composable.CQRS.ServiceBus.NServicebus.Tests
                 _bus = bus;
             }
 
-            public void Handle(ACommand message)
+            public void Handle(ReplyCommand message)
             {
-                _bus.Reply(new ACommandReply());
+                _bus.Reply(new ReplyMessage());
             }
         }
 
-        public class ACommandReply : IMessage { }
+        public class Message : IMessage {}
 
-        public class ACommand : IMessage  { }
+        public class ReplyMessage : IMessage { }
+
+        public class ReplyCommand : IMessage { }
+
+        public class SendAMessageAndThenReplyCommand : IMessage { }
+
+        private static WindsorContainer WireUpDualDispatchBusWithAllLocalMessageHandlersRegisteredAndAStrictMockForTheRealIBus()
+        {
+            var container = new WindsorContainer();
+            container.Register(
+                Component.For<IBus>().Instance(new Mock<IBus>(MockBehavior.Strict).Object),
+                Component.For<ISingleContextUseGuard>().ImplementedBy<SingleThreadUseGuard>(),
+                Component.For<SynchronousBus>(),
+                Component.For<NServiceBusServiceBus>(),
+                Component.For<IServiceBus>().ImplementedBy<DualDispatchBus>().LifestyleScoped(),
+                Component.For<IHandleMessages<ReplyCommand>>().ImplementedBy<ACommandHandler>(),
+                Component.For<ACommandReplyHandler, IHandleMessages<ReplyMessage>>().ImplementedBy<ACommandReplyHandler>(),
+                Component.For<IHandleMessages<SendAMessageAndThenReplyCommand>>().ImplementedBy<SendAMessageAndThenReplyCommandCommandHandler>()
+                );
+
+            container.Register(Component.For<IWindsorContainer>().Instance(container));
+            return container;
+        }
     }   
 }

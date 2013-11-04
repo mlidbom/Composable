86fa608e8d05f0ce69fb3f9a589baf27ec65d36d
mlidbo <magnus.lidbom@gmail.com>
2013-11-04 20:23:09 +0100

    Enable users of the DualDispatchBus and the SynchronousBus to have some control over how messages are dispatched through the new interfaces: ISynchronousBusSubscriberFilter and IDualDispatchBusRouter

--- CQRS/CQRS/ServiceBus/SynchronousBus.cs
+++ CQRS/CQRS/ServiceBus/SynchronousBus.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using Castle.Windsor;
+using Composable.CQRS;
+using Composable.CQRS.EventSourcing;
 using Composable.KeyValueStorage.Population;
+using Composable.System;
 using Composable.System.Linq;
 using Composable.System.Reflection;
 using Composable.System.Transactions;
@@ -14,20 +17,46 @@ namespace Composable.ServiceBus
     public class SynchronousBus : IServiceBus
     {
         protected readonly IWindsorContainer ServiceLocator;
+        private ISynchronousBusSubscriberFilter _subscriberFilter;
 
-        public SynchronousBus(IWindsorContainer serviceLocator)
+        public SynchronousBus(IWindsorContainer serviceLocator, ISynchronousBusSubscriberFilter subscriberFilter = null)
         {
+            _subscriberFilter = subscriberFilter ?? PublishToAllSubscribersSubscriberFilter.Instance;
             ServiceLocator = serviceLocator;
         }
 
         public virtual void Publish(object message)
         {
-            ((dynamic)this).SyncSendLocal((dynamic)message);
+            ((dynamic)this).PublishLocal((dynamic)message);
         }
 
         public virtual bool Handles<TMessage>(TMessage message) where TMessage : IMessage
         {
             return GetHandlerTypes(message).Any();
+        }
+
+        protected virtual void PublishLocal<TMessage>(TMessage message) where TMessage : IMessage
+        {
+
+            var handlerTypes = GetHandlerTypes(message);
+
+            var handlers = new List<object>();
+            foreach (var handlerType in handlerTypes)
+            {
+                handlers.AddRange(ServiceLocator.ResolveAll(handlerType).Cast<object>());
+            }
+
+            using (var transactionalScope = ServiceLocator.BeginTransactionalUnitOfWorkScope())
+            {
+                foreach (dynamic handler in handlers)
+                {
+                    if(_subscriberFilter.PublishMessageToHandler(message, handler))
+                    {
+                        handler.Handle((dynamic)message);
+                    }
+                }
+                transactionalScope.Commit();
+            }
         }
 
         protected virtual void SyncSendLocal<TMessage>(TMessage message) where TMessage : IMessage
@@ -41,12 +70,19 @@ namespace Composable.ServiceBus
                 handlers.AddRange(ServiceLocator.ResolveAll(handlerType).Cast<object>());
             }
 
+            if(handlers.Count() > 1)
+            {
+                throw new MultipleMessageHandlersRegisteredException(message, handlers);
+            }
+
+            if(handlers.None())
+            {
+                throw new NoHandlerException(message.GetType());
+            }
+
             using(var transactionalScope = ServiceLocator.BeginTransactionalUnitOfWorkScope())
             {
-                foreach(dynamic handler in handlers)
-                {
-                    handler.Handle((dynamic)message);
-                }
+                ((dynamic)handlers.Single()).Handle((dynamic)message);
                 transactionalScope.Commit();
             }
         }
@@ -74,5 +110,45 @@ namespace Composable.ServiceBus
         {
             ((dynamic)this).SyncSendLocal((dynamic)message);
         }
+    }
+
+    public class NoHandlerException : Exception
+    {
+        public NoHandlerException(Type messageType):base("No handler registered for message type: {0}".FormatWith(messageType.FullName))
+        {
+            
+        }
+    }
+
+    public class MultipleMessageHandlersRegisteredException : Exception
+    {
+        public MultipleMessageHandlersRegisteredException(object message, List<object> handlers):base(CreateMessage(message,handlers))
+        {
+        }
+
+        private static string CreateMessage(object message, List<object> handlers)
+        {
+            var exceptionMessage = "There are multiple handlers registered for the message type:{0}".FormatWith(message.GetType());
+            handlers.Select(handler => handler.GetType())
+                .ForEach(handlerType => exceptionMessage += "{0}{1}".FormatWith(Environment.NewLine, handlerType.FullName)                
+                );
+            return exceptionMessage;
+
+        }
+    }
+
+    public class PublishToAllSubscribersSubscriberFilter : ISynchronousBusSubscriberFilter
+    {
+        public static readonly ISynchronousBusSubscriberFilter Instance = new PublishToAllSubscribersSubscriberFilter();
+
+        public bool PublishMessageToHandler(object message, object handler)
+        {
+            return true;
+        }
+    }
+
+    public interface ISynchronousBusSubscriberFilter
+    {
+        bool PublishMessageToHandler(object message, object handler);
     }
 }
---
+++ CQRS/Composable.CQRS.ServiceBus.NServiceBus/IDualDispatchBusRouter.cs
@@ -0,0 +1,9 @@
+﻿using Composable.ServiceBus;
+
+namespace Composable.CQRS.ServiceBus.NServiceBus
+{
+    public interface IDualDispatchBusRouter
+    {
+        bool SendToSynchronousBus(object message, SynchronousBus syncBus);
+    }
+}

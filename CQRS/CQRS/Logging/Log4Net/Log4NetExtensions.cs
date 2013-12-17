f54d188a4c6d4fefdc17ab7dd11d240201dda5ac
mlidbo <magnus.lidbom@gmail.com>
2013-12-17 12:25:51 +0100

    Add simple extension to object that lets any class log using log4net with a logger specific to that class by calling this.Log().SomeLoggingMethod...

---
+++ CQRS/CQRS/Logging/Log4Net/Log4NetExtensions.cs
@@ -0,0 +1,18 @@
+﻿using log4net;
+
+namespace Composable.Logging.Log4Net
+{
+    public static class Log4NetExtensions
+    {
+        public static ILog Log<T>(this T me)
+        {
+            return LogHolder<T>.Logger;
+        }
+
+        private static class LogHolder<T>
+        {
+            // ReSharper disable once StaticFieldInGenericType
+            public static readonly ILog Logger = LogManager.GetLogger(typeof(T));
+        }
+    }
+}

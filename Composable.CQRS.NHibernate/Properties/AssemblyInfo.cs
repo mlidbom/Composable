58782c19d9525fa98c3820cd6cba9a3ccd277bfc
mlidbo <magnus.lidbom@gmail.com>
2014-02-14 17:27:50 +0100

    Update NHibernate

--- Composable.CQRS.NHibernate/Properties/AssemblyInfo.cs
+++ Composable.CQRS.NHibernate/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Composable.CQRS.NHibernate")]
 [assembly: AssemblyProduct("Composable.CQRS.NHibernate")]
 
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]

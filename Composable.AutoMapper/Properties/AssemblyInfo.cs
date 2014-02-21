c0b415463d8604f072b427fea83117a7b89877d9
mlidbo <magnus.lidbom@gmail.com>
2014-02-21 13:34:17 +0100

    More bumping

--- Composable.AutoMapper/Properties/AssemblyInfo.cs
+++ Composable.AutoMapper/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Composable.AutoMapper")]
 [assembly: AssemblyProduct("Composable.AutoMapper")]
 
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.4.0")]

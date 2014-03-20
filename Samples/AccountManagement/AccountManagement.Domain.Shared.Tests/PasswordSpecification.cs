91f5a47cec02ece6dc6e65e2f6f871908f4041f1
mlidbo <magnus.lidbom@gmail.com>
2014-03-20 18:42:48 +0100

    Complete the test for the password class.

--- Samples/AccountManagement/AccountManagement.Domain.Shared.Tests/PasswordSpecification.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Shared.Tests/PasswordSpecification.cs
@@ -1,23 +1,34 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 
 namespace AccountManagement.Domain.Shared.Tests
 {
     public class PasswordSpecification : NSpec.NUnit.nspec
     {
-        public void when_creating_a_password_from_the_string()
+        public void when_creating_a_new_password()
         {
-            Password password = new Password("a");
-
-            context["password"] =
+            context["from_the_string 'password'"] =
                 () =>
                 {
-                    before =
-                        () => { password = new Password("password"); };
+                    var password = new Password("a");
+                    before = () => { password = new Password("password"); };
+
                     it["HashedPassword is not null"] = () => password.HashedPassword.Should().NotBeNull();
                     it["HashedPassword is not an empty array"] = () => password.HashedPassword.Should().NotBeEmpty();
                     it["Salt is not null"] = () => password.Salt.Should().NotBeNull();
                     it["Salt is not empty"] = () => password.Salt.Should().NotBeEmpty();
                     it["IsCorrectPassword('password') ==  true"] = () => password.IsCorrectPassword("password").Should().BeTrue();
+                    it["IsCorrectPassword('Password') !=  true"] = () => password.IsCorrectPassword("Password").Should().BeFalse();
+                    it["IsCorrectPassword('password ') !=  true"] = () => password.IsCorrectPassword("Password").Should().BeFalse();
+                    it["IsCorrectPassword(' password') !=  true"] = () => password.IsCorrectPassword("Password").Should().BeFalse();
+                    var otherPassword = new Password(Guid.NewGuid().ToString());
+                    context["when comparing to another password created from the string 'otherPassword'"] = 
+                        () =>
+                        {
+                            before = () => otherPassword = new Password("otherPassword");
+                            it["the Salt members are different"] = () => password.Salt.Should().NotEqual(otherPassword.Salt);
+                            it["the HashedPassword members are different"] = () => password.HashedPassword.Should().NotEqual(otherPassword.HashedPassword);
+                        };
                 };
         }
     }

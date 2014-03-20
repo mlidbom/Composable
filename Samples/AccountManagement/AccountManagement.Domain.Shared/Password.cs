1bfe7c6c5dbec95d1c4ac69241aee140b5cda22a
mlidbo <magnus.lidbom@gmail.com>
2014-03-20 17:52:26 +0100

    Making value objects structs often makes sense...

--- Samples/AccountManagement/AccountManagement.Domain.Shared/Password.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Shared/Password.cs
@@ -1,7 +1,20 @@
 ﻿namespace AccountManagement.Domain.Shared
 {
-    public class Password
+    /// <summary>
+    /// If this was a real application this class would do various hashing etc of the password...
+    /// </summary>
+    public struct Password
     {
-        
+        private string Value { get; set; }
+
+        public bool Matches(string password)
+        {
+            return Value == password;
+        }
+
+        public Password(string password) : this()
+        {
+            Value = password;
+        }
     }
 }

88f5c7a14ed780e08288e920eaff3887dd73b03b
mlidbo <magnus.lidbom@gmail.com>
2014-03-20 18:30:54 +0100

    Setup test project and write first failing test for password class.

--- Samples/AccountManagement/AccountManagement.Domain.Shared/Password.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Shared/Password.cs
@@ -1,20 +1,47 @@
-﻿namespace AccountManagement.Domain.Shared
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AccountManagement.Domain.Shared
 {
     /// <summary>
-    /// If this was a real application this class would do various hashing etc of the password...
+    /// Note how all the business logic of a secure password is encapsulated.
     /// </summary>
     public struct Password
     {
-        private string Value { get; set; }
-
-        public bool Matches(string password)
+        public byte[] HashedPassword { get; private set; }
+        public byte[] Salt { get; private set; }
+        
+        public bool IsCorrectPassword(string password)
         {
-            return Value == password;
+            return HashedPassword == HashPassword(Salt, password);
         }
 
         public Password(string password) : this()
         {
-            Value = password;
+            if(string.IsNullOrWhiteSpace(password))
+            {
+                throw new InvalidPasswordException();
+            }
+            Salt = Guid.NewGuid().ToByteArray();
+            HashedPassword = HashPassword(salt: Salt, password: password);
         }
+
+        private static byte[] HashPassword(byte[] salt, string password)
+        {
+            var encodedPassword = Encoding.Unicode.GetBytes(password);
+            var saltedPassword = new byte[salt.Length + encodedPassword.Length];
+            Array.Copy(salt, 0, saltedPassword, 0, salt.Length);
+            Array.Copy(encodedPassword, 0, saltedPassword, salt.Length, encodedPassword.Length);
+            using (var algorithm = SHA256.Create())
+            {
+                return algorithm.ComputeHash(saltedPassword);
+            }
+        }
+    }
+
+    public class InvalidPasswordException : Exception
+    {
+        
     }
 }

8eb5124c8f0d2d89d247741655dcae1712d3a12a
mlidbo <magnus.lidbom@gmail.com>
2014-03-20 21:15:33 +0100

    Implement and test the email class.

--- Samples/AccountManagement/AccountManagement.Domain.Shared/Email.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Shared/Email.cs
@@ -1,10 +1,53 @@
-﻿namespace AccountManagement.Domain.Shared
+﻿using System;
+using System.Text.RegularExpressions;
+using Composable.System;
+
+namespace AccountManagement.Domain.Shared
 {
-    public struct Email 
+    public struct Email
     {
-        public Email(string email)
+        private string Value { get; set; }
+
+        override public string ToString()
         {
-            throw new System.NotImplementedException();
+            return Value;
+        }
+
+        private Email(string emailAddress): this()
+        {
+            Validate(emailAddress);
+            Value = emailAddress;
+        }
+
+        public static Email Parse(string emailAddress)
+        {
+            return new Email(emailAddress);
+        }
+
+        private static void Validate(string emailAddress)
+        {
+            if (string.IsNullOrWhiteSpace(emailAddress))
+                throw new InvalidEmailException("Email address is null empty or whitespace");
+
+            var regex = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$");
+            var isMatch = regex.IsMatch(emailAddress);
+
+            if (!isMatch)
+                throw new InvalidEmailException("Email address {0} does not match pattern for email address.".FormatWith(emailAddress));
+
+            if (emailAddress.Contains(".."))
+                throw new InvalidEmailException("Double dot ('..') in email adress {0} is not allowed.".FormatWith(emailAddress));
+
+            if (emailAddress.Contains("@.") || emailAddress.Contains(".@"))
+                throw new InvalidEmailException("Dot with @ in email adress {0} is not allowed.".FormatWith(emailAddress));
+        }
+    }
+
+    public class InvalidEmailException : ArgumentException
+    {
+        public InvalidEmailException(string message):base(message)
+        {
+            
         }
     }
 }

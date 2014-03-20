c9066a153f6367282b9ab0a4c2a2724f85816c21
mlidbo <magnus.lidbom@gmail.com>
2014-03-20 21:15:33 +0100

    Implement and test the email class.

---
+++ Samples/AccountManagement/AccountManagement.Domain.Shared.Tests/EmailSpecification.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+namespace AccountManagement.Domain.Shared.Tests
+{
+    public class EmailSpecification : NSpec.NUnit.nspec
+    {
+        public void creating_new_email()
+        {
+            context["throws InvalidEmailException for all of these strings"] =
+                () =>
+                {
+                    it["'null'"] = () => expect<InvalidEmailException>(() => Email.Parse(null));
+                    it["''"] = expect<InvalidEmailException>(() => Email.Parse(null));
+                    it["' '"] = expect<InvalidEmailException>(() => Email.Parse(" "));
+                    it["'test.test.com' (No domain)"] = expect<InvalidEmailException>(() => Email.Parse("test.test.com"));
+                    it["'test.test@test..com' (Repeated ..)"] = expect<InvalidEmailException>(() => Email.Parse("test.test@test..com"));
+                    it["'test.test@test...com' (Repeated ...)"] = expect<InvalidEmailException>(() => Email.Parse("test.test@test...com"));
+                    it["'test.test@.test.dk' (@.)"] = expect<InvalidEmailException>(() => Email.Parse("test.test@.test.dk"));
+                    it["'test.test@..test.dk' (Repeated .. and @.)"] = expect<InvalidEmailException>(() => Email.Parse("test.test@..test.dk"));
+                    it["'test.test..@test.dk' (Repeated .. and .@)"] = expect<InvalidEmailException>(() => Email.Parse("test.test..@test.dk"));
+                    it["'test.test.@test.dk' (.@)"] = expect<InvalidEmailException>(() => Email.Parse("test.test.@test.dk"));
+                };
+            context["with string: 'test@test.dk'"] =
+                () =>
+                {
+                    Email email = Email.Parse("t@t.to");
+                    before = () => email = Email.Parse("test@test.dk");
+                    it["ToString returns 'test@test.dk'"] = () => email.ToString().Should().Be("test@test.dk");
+                };
+        }
+    }
+}

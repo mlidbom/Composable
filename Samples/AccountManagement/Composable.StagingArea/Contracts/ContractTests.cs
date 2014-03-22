45402ef78c374fa97731c4c1772a0f6e5f3ea304
mlidbo <magnus.lidbom@gmail.com>
2014-03-22 17:47:01 +0100

    Add composable staging project with a first stab at simple contract validation code..

---
+++ Samples/AccountManagement/Composable.StagingArea/Contracts/ContractTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Composable.Contracts;
+using NUnit.Framework;
+
+namespace Composable.StagingArea
+{
+    [TestFixture]
+    public class ContractTests
+    {
+        [Test]
+        public void ArgumentNotNullThrowsArgumentNullExceptionIfAnyArgumentIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => Contract.ArgumentNotNull(null));
+            Assert.Throws<ArgumentNullException>(() => Contract.ArgumentNotNull("", null));
+            Assert.Throws<ArgumentNullException>(() => Contract.ArgumentNotNull("", null, ""));
+        }
+
+        [Test]
+        public void NotEmptyThrowsArgumentExceptionForEmptyGuid()
+        {
+            Assert.Throws<ArgumentException>(() => Contract.Argument(Guid.Empty).NotEmpty());
+        }
+    }
+}
---
+++ Samples/AccountManagement/Composable.StagingArea/Contracts/StringEmptyArgumentException.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Composable.Contracts
+{
+    public class StringEmptyArgumentException : ArgumentException
+    {
+        
+    }
+}

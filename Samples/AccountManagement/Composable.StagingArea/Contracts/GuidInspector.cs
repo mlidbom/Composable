687eacee863ab02a8ca0083a81f2475818bc7e83
mlidbo <magnus.lidbom@gmail.com>
2014-03-22 21:22:11 +0100

    Add the ability to test for default values for structs

---
+++ Samples/AccountManagement/Composable.StagingArea/Contracts/GuidInspector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Composable.Contracts
+{
+    public static class GuidInspector
+    {
+        public static Inspected<Guid> NotEmpty(this Inspected<Guid> me)
+        {
+            return me.Inspect(
+                inspected => inspected != Guid.Empty,
+                badValue => new GuidIsEmptyException(badValue.Name));
+        }
+    }
+}
---
+++ Samples/AccountManagement/Composable.StagingArea/Contracts/ObjectNullOrDefaultInspector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Composable.Contracts.Tests;
+
+namespace Composable.Contracts
+{
+    public static class ObjectNullOrDefaultInspector
+    {
+        public static Inspected<TArgument> NotNull<TArgument>(this Inspected<TArgument> me)
+            where TArgument : class
+        {
+            return me.Inspect(
+                inspected => !ReferenceEquals(inspected, null),
+                badValue => new ObjectIsNullException(badValue.Name));
+        }
+
+        public static Inspected<TArgument> NotNullOrDefault<TArgument>(this Inspected<TArgument> me)
+        {
+            me.Inspect(
+                inspected => !ReferenceEquals(inspected, null),
+                badValue => new ObjectIsNullException(badValue.Name));
+
+            return me.Inspect(
+                inspected =>
+                {
+                    if(!inspected.GetType().IsValueType)
+                    {
+                        return true;
+                    }
+                    var valueTypeDefault = Activator.CreateInstance(inspected.GetType());
+                    return !Equals(inspected, valueTypeDefault);
+                },
+                badValue => new ObjectIsDefaultException(badValue.Name));
+        }
+    }
+}

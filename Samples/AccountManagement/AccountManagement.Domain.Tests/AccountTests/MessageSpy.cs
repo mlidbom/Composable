0373e86e4b003aba26b716db289d708f46cc6fa6
mlidbo <magnus.lidbom@gmail.com>
2014-03-23 19:42:44 +0100

    Write tests for registration scenarios.

---
+++ Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/MessageSpy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NServiceBus;
+
+namespace AccountManagement.Domain.Tests.AccountTests
+{
+    public class MessageSpy : IHandleMessages<IMessage>
+    {
+        public MessageSpy()
+        {
+            ReceivedMessages = new List<IMessage>();
+        }
+
+        public void Handle(IMessage message)
+        {
+            ReceivedMessages.Add(message);
+        }
+
+        public IList<IMessage> ReceivedMessages { get; private set; }
+    }
+}
---
+++ Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/RegisterAccountFailureTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using AccountManagement.Domain.Services;
+using AccountManagement.Domain.Shared;
+using Castle.MicroKernel.Lifestyle;
+using Composable.Contracts;
+using NUnit.Framework;
+
+namespace AccountManagement.Domain.Tests.AccountTests
+{
+    [TestFixture]
+    public class RegisterAccountFailureTests
+    {
+        private IDisposable _scope;
+        private IAccountManagementEventStoreSession _repository;
+        private readonly Password _validPassword = new Password("Password1");
+        private readonly Email _validEmail = Email.Parse("test.test@test.se");
+        private readonly Guid _validAccountId = Guid.NewGuid();
+
+        [SetUp]
+        public void SetupWiringAndCreateRepositoryAndScope()
+        {
+            var container = DomainTestWiringHelper.SetupContainerForTesting();
+            _scope = container.BeginScope();
+            _repository = container.Resolve<IAccountManagementEventStoreSession>();
+        }
+
+        [TearDown]
+        public void CleanupScope()
+        {
+            _scope.Dispose();
+        }
+
+        [Test]
+        public void WhenPasswordIsNullObjectIsNullExceptionIsThrown()
+        {
+            Assert.Throws<ObjectIsNullException>(() => Account.Register(null, _validPassword, _validAccountId, _repository));
+        }
+
+        [Test]
+        public void WhenEmailIsNullObjectIsNullExceptionIsThrown()
+        {
+            Assert.Throws<ObjectIsNullException>(() => Account.Register(_validEmail, null, _validAccountId, _repository));
+        }
+
+        [Test]
+        public void WhenAccountIdIsEmptyObjectIsDefaultExceptionIsThrown()
+        {
+            Assert.Throws<ObjectIsDefaultException>(() => Account.Register(_validEmail, _validPassword, Guid.Empty, _repository));
+        }
+
+        [Test]
+        public void WhenRepositoryIsNullObjectIsNullExceptionIsThrown()
+        {
+            Assert.Throws<ObjectIsDefaultException>(() => Account.Register(_validEmail, _validPassword, Guid.Empty, _repository));
+        }
+    }
+}

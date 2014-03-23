4b7de562f8a7ccab8d954984dd88d2e4a84ef3d8
mlidbo <magnus.lidbom@gmail.com>
2014-03-23 01:14:13 +0100

    Improve exception message.

--- Samples/AccountManagement/Composable.StagingArea/Contracts/InvalidArgumentAccessorLambda.cs
+++ Samples/AccountManagement/Composable.StagingArea/Contracts/InvalidArgumentAccessorLambda.cs
@@ -4,6 +4,6 @@ namespace Composable.Contracts
 {
     public class InvalidArgumentAccessorLambda : Exception
     {
-        public InvalidArgumentAccessorLambda() : base("The lambda passed must be exactly of this form: () => parameterName") { }
+        public InvalidArgumentAccessorLambda() : base("The lambda passed must be exactly of this form: '() => parameterName' where parameterName really is the name of a parameter.") { }
     }
 }

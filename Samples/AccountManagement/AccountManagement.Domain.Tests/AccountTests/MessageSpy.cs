4c331f96770a270879355a77346a0d6f79c6ac90
mlidbo <magnus.lidbom@gmail.com>
2014-03-23 22:16:14 +0100

    Make sure to implement ISynchronousBusMessageSpy so that things will not fail if send is used during tests.

--- Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/MessageSpy.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/MessageSpy.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Composable.ServiceBus;
 using NServiceBus;
 
 namespace AccountManagement.Domain.Tests.AccountTests
 {
-    public class MessageSpy : IHandleMessages<IMessage>
+    public class MessageSpy : IHandleMessages<IMessage>, 
+        ISynchronousBusMessageSpy//Keeps the bus from getting angry when more than one listener exists when invoking "Send". A hack that will be changed.
     {
         public MessageSpy()
         {

9380f6b325fa4a74098a0e148aa3f5bbf15ebc48
mlidbo <magnus.lidbom@gmail.com>
2014-03-23 18:53:49 +0100

    Add documentation comments.

--- Samples/AccountManagement/AccountManagement.Domain/Account.cs
+++ Samples/AccountManagement/AccountManagement.Domain/Account.cs
@@ -2,36 +2,59 @@
 using AccountManagement.Domain.Events;
 using AccountManagement.Domain.Events.Implementation;
 using AccountManagement.Domain.Events.PropertyUpdated;
+using AccountManagement.Domain.Services;
 using AccountManagement.Domain.Shared;
 using Composable.Contracts;
 using Composable.CQRS.EventSourcing;
 
 namespace AccountManagement.Domain
 {
-    internal class Account : AggregateRoot<Account, IAccountEvent>
+    ///Completely encapsulates all the business logic for an account.  Should make it impossible for clients to use the class incorrectly.
+    public class Account : AggregateRoot<Account, IAccountEvent>
     {
         public Email Email { get; private set; }
         public Password Password { get; private set; }
 
         public Account()
         {
+            //Maintain correct state as events are raised or read from the store. 
+            //Use property updated events whenever possible. Changes to public state should be represented by property updated events.
             RegisterEventAppliers()
                 .For<IAccountEmailPropertyUpdatedEvent>(e => Email = e.Email)
                 .For<IAccountPasswordPropertyUpdateEvent>(e => Password = e.Password);
         }
 
-        public void Register(Email email, Password password, Guid accountId)
+        //Ensure that the state of the instance is sane. If not throw an exception.
+        //Called after every call to RaiseEvent.
+        override protected void AssertInvariantsAreMet()
         {
-            Contract.Arguments(() => email, () => password, () => accountId).NotNullOrDefault();
-
-            RaiseEvent(new UserRegisteredAccountEvent(accountId:accountId, email: email, password: password ));
+            Contract.Invariant(() => Email, () => Password, () => Id).NotNullOrDefault();
         }
 
-        public void ChangePassword(Password password)
+        /// <summary><para>Usen when a user manually creates an account themselves.</para>
+        /// <para>Note how this design with a named static creation method: </para>
+        /// <para> * makes it clearear what the caller intends.</para>
+        /// <para> * makes it impossible to use the class incorrectly, such as forgetting to save the new instance in the event store.</para>
+        /// <para> * reduces code duplication since multiple callers are not burdened with saving the instance.</para>
+        /// </summary>
+        public static Account Register(Email email, Password password, Guid accountId, IAccountManagementEventStoreSession repository)
         {
-            Contract.Arguments(() => password).NotNullOrDefault();
+            //Ensure that it is impossible to call with invalid arguments. 
+            //Since these types all ensure that it is impossible to create a non-default value that is invalid we only have to disallow default values.
+            Contract.Arguments(() => email, () => password, () => accountId).NotNullOrDefault();
 
-            RaiseEvent(new UserChangedAccountPassword(password));
+            var created = new Account();            
+            created.RaiseEvent(new UserRegisteredAccountEvent(accountId: accountId, email: email, password: password));
+            repository.Save(created);
+            return created;
+        }
+        
+        public void ChangePassword(string oldPassword, Password newPassword)
+        {
+            Contract.Arguments(() => newPassword).NotNullOrDefault();
+            Contract.Arguments(() => oldPassword).NotNullEmptyOrWhiteSpace();            
+
+            RaiseEvent(new UserChangedAccountPassword(newPassword));
         }
 
         public void ChangeEmail(Email email)

9ef0798e56e0659f5222b803092f59c31d83cb07
mlidbo <magnus.lidbom@gmail.com>
2014-03-24 18:20:44 +0100

    Extract test base class to keep test code short and to the point. All it does is set up the container and a scope..

--- Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/ChangePasswordFailureScenariosTests.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/ChangePasswordFailureScenariosTests.cs
@@ -1,7 +1,33 @@
-﻿namespace AccountManagement.Domain.Tests.AccountTests
+﻿using AccountManagement.Domain.Shared;
+using AccountManagement.TestHelpers.Fixtures;
+using Composable.Contracts;
+using NUnit.Framework;
+
+namespace AccountManagement.Domain.Tests.AccountTests
 {
-    public class ChangePasswordFailureScenariosTests
+    [TestFixture]
+    public class ChangePasswordFailureScenariosTests : DomainTestBase
     {
-         
+        private ValidAccountRegisteredFixture _fixture;
+        private Account _account;
+
+        [SetUp]
+        public void RegisterAccount()
+        {
+            _fixture = new ValidAccountRegisteredFixture();
+            _account = _fixture.Setup(Container);
+        }
+
+        [Test]
+        public void WhenNewPasswordIsNullObjectNullExceptionIsThrown()
+        {
+            Assert.Throws<ObjectIsNullException>(() => _account.ChangePassword(_fixture.PasswordAsString, null));
+        }
+
+        [Test]
+        public void WhenOldPasswordIsIncorrectWrongPasswordExceptionIsThrown()
+        {
+            Assert.Throws<WrongPasswordException>(() => _account.ChangePassword("wrong", new Password("SomeComplexPassword1!")));
+        }
     }
 }
--- Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/RegisterAccountFailureScenariosTests.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/RegisterAccountFailureScenariosTests.cs
@@ -1,69 +1,55 @@
 ﻿using System;
 using AccountManagement.Domain.Services;
-using AccountManagement.TestHelpers;
 using AccountManagement.TestHelpers.Fixtures;
-using Castle.MicroKernel.Lifestyle;
-using Castle.Windsor;
 using Composable.Contracts;
 using NUnit.Framework;
 
 namespace AccountManagement.Domain.Tests.AccountTests
 {
     [TestFixture]
-    public class RegisterAccountFailureScenariosTests   
+    public class RegisterAccountFailureScenariosTests : DomainTestBase
     {
-        private IDisposable _scope;
         private IAccountManagementEventStoreSession _repository;
-        private WindsorContainer _container;
         private IDuplicateAccountChecker _duplicateAccountChecker;
-        private ValidAccountRegisteredFixture _validAccountRegisteredFixture;
+        private ValidAccountRegisteredFixture _accountFixture;
 
         [SetUp]
         public void SetupWiringAndCreateRepositoryAndScope()
         {
-            _container = DomainTestWiringHelper.SetupContainerForTesting();
-            _scope = _container.BeginScope();
-            _repository = _container.Resolve<IAccountManagementEventStoreSession>();
-            _duplicateAccountChecker = _container.Resolve<IDuplicateAccountChecker>();
-            _validAccountRegisteredFixture = new ValidAccountRegisteredFixture();
-            //_fixture.Execute(_container);
-        }
-
-        [TearDown]
-        public void CleanupScope()
-        {
-            _scope.Dispose();
+            _repository = Container.Resolve<IAccountManagementEventStoreSession>();
+            _duplicateAccountChecker = Container.Resolve<IDuplicateAccountChecker>();
+            _accountFixture = new ValidAccountRegisteredFixture();
         }
 
         [Test]
         public void WhenEmailIsAlreadyRegisteredADuplicateAccountExceptionIsThrown()
         {
-            _validAccountRegisteredFixture.Setup(_container);
-            Assert.Throws<DuplicateAccountException>(() => Account.Register(_validAccountRegisteredFixture.Email, _validAccountRegisteredFixture.Password, _validAccountRegisteredFixture.AccountId, _repository, _duplicateAccountChecker));
+            _accountFixture.Setup(Container);
+            Assert.Throws<DuplicateAccountException>(() => Account.Register(_accountFixture.Email, _accountFixture.Password, _accountFixture.AccountId, _repository, _duplicateAccountChecker));
         }
 
         [Test]
         public void WhenPasswordIsNullObjectIsNullExceptionIsThrown()
         {
-            Assert.Throws<ObjectIsNullException>(() => Account.Register(_validAccountRegisteredFixture.Email, null, _validAccountRegisteredFixture.AccountId, _repository, _duplicateAccountChecker));
+            Assert.Throws<ObjectIsNullException>(() => Account.Register(_accountFixture.Email, null, _accountFixture.AccountId, _repository, _duplicateAccountChecker));
         }
 
         [Test]
         public void WhenEmailIsNullObjectIsNullExceptionIsThrown()
         {
-            Assert.Throws<ObjectIsNullException>(() => Account.Register(null, _validAccountRegisteredFixture.Password, _validAccountRegisteredFixture.AccountId, _repository, _duplicateAccountChecker));
+            Assert.Throws<ObjectIsNullException>(() => Account.Register(null, _accountFixture.Password, _accountFixture.AccountId, _repository, _duplicateAccountChecker));
         }
 
         [Test]
         public void WhenAccountIdIsEmptyObjectIsDefaultExceptionIsThrown()
         {
-            Assert.Throws<ObjectIsDefaultException>(() => Account.Register(_validAccountRegisteredFixture.Email, _validAccountRegisteredFixture.Password, Guid.Empty, _repository, _duplicateAccountChecker));
+            Assert.Throws<ObjectIsDefaultException>(() => Account.Register(_accountFixture.Email, _accountFixture.Password, Guid.Empty, _repository, _duplicateAccountChecker));
         }
 
         [Test]
         public void WhenRepositoryIsNullObjectIsNullExceptionIsThrown()
         {
-            Assert.Throws<ObjectIsDefaultException>(() => Account.Register(_validAccountRegisteredFixture.Email, _validAccountRegisteredFixture.Password, Guid.Empty, _repository, _duplicateAccountChecker));
+            Assert.Throws<ObjectIsDefaultException>(() => Account.Register(_accountFixture.Email, _accountFixture.Password, Guid.Empty, _repository, _duplicateAccountChecker));
         }
     }
 }

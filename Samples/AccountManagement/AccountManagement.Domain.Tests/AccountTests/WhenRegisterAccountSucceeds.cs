fc59b5b84641fe37f72e0a875d9df47eefb2011e
mlidbo <magnus.lidbom@gmail.com>
2014-03-24 18:20:44 +0100

    Extract test base class to keep test code short and to the point. All it does is set up the container and a scope..

--- Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/WhenRegisterAccountSucceeds.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Tests/AccountTests/WhenRegisterAccountSucceeds.cs
@@ -1,50 +1,40 @@
 ﻿using System.Linq;
 using AccountManagement.Domain.Events;
-using AccountManagement.Domain.Shared;
-using AccountManagement.TestHelpers;
 using AccountManagement.TestHelpers.Fixtures;
-using Castle.MicroKernel.Lifestyle;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace AccountManagement.Domain.Tests.AccountTests
 {
     [TestFixture]
-    public class RegisterAccountSuccessScenarioTests
+    public class RegisterAccountSuccessScenarioTests : DomainTestBase
     {
-        private readonly Email _registrationEmail = Email.Parse("test.test@test.se");
-        private MessageSpy _messageSpy;
         private Account _registeredAccount;
-        private ValidAccountRegisteredFixture _validAccountRegisteredFixture;
+        private ValidAccountRegisteredFixture _accountFixture;
 
         [SetUp]
         public void RegisterAccount()
         {
-            _validAccountRegisteredFixture = new ValidAccountRegisteredFixture();
-            var container = DomainTestWiringHelper.SetupContainerForTesting();
-            using(container.BeginScope())
-            {
-              _messageSpy = container.Resolve<MessageSpy>();
-              _registeredAccount = _validAccountRegisteredFixture.Setup(container);
-            }
+            _accountFixture = new ValidAccountRegisteredFixture();
+            _registeredAccount = _accountFixture.Setup(Container);
         }
 
         [Test]
         public void AnIUserRegisteredAccountEventIsPublished()
         {
-            _messageSpy.ReceivedMessages.OfType<IUserRegisteredAccountEvent>().ToList().Should().HaveCount(1);
+            MessageSpy.ReceivedMessages.OfType<IUserRegisteredAccountEvent>().ToList().Should().HaveCount(1);
         }
 
         [Test]
         public void AccountEmailIsTheOneUsedForRegistration()
         {
-            Assert.That(_registrationEmail, Is.EqualTo(_validAccountRegisteredFixture.Email));
+            Assert.That(_registeredAccount.Email, Is.EqualTo(_accountFixture.Email));
         }
 
         [Test]
         public void AccountPasswordIsTheOnUsedForRegistration()
         {
-            Assert.True(_registeredAccount.Password.IsCorrectPassword(_validAccountRegisteredFixture.PasswordAsString));
+            Assert.True(_registeredAccount.Password.IsCorrectPassword(_accountFixture.PasswordAsString));
         }
     }
 }
---
+++ Samples/AccountManagement/AccountManagement.Domain.Tests/DomainTestBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AccountManagement.TestHelpers;
+using Castle.MicroKernel.Lifestyle;
+using Castle.Windsor;
+using NUnit.Framework;
+
+namespace AccountManagement.Domain.Tests
+{
+    [TestFixture]
+    public abstract class DomainTestBase
+    {
+        protected WindsorContainer Container { get; set; }
+        protected IDisposable Scope { get; set; }
+        protected MessageSpy MessageSpy { get { return Container.Resolve<MessageSpy>(); } }
+
+        [SetUp]
+        public void SetupContainerAndBeginScope()
+        {
+            Container = DomainTestWiringHelper.SetupContainerForTesting();
+            Scope = Container.BeginScope();
+        }
+
+        [TearDown]
+        public void DisposeScopeAndContainer()
+        {
+            Scope.Dispose();
+            Container.Dispose();
+        }
+    }
+}

6f80181d687ff4430a97996effeaa44b7190af20
mlidbo <magnus.lidbom@gmail.com>
2014-03-25 18:40:44 +0100

    Get DisplayAccountDetails working correctly including test.

--- Samples/AccountManagement/AccountManagement.Web/Views/Account/DisplayAccountDetailsViewModel.cs
+++ Samples/AccountManagement/AccountManagement.Web/Views/Account/DisplayAccountDetailsViewModel.cs
@@ -1,9 +1,11 @@
-﻿using Composable.DDD;
+﻿using AccountManagement.Domain.Shared;
+using Composable.DDD;
 
 namespace AccountManagement.Web.Views.Account
 {
     public class DisplayAccountDetailsViewModel : ValueObject<DisplayAccountDetailsViewModel>
     {
         public readonly string PageTitle =  DisplayAccountDetailsViewModelResources.Title;
+        public string Email { get; set; }
     }
 }

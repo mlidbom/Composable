255d2c203ab3816f3275ab14810d1977e1154d2f
mlidbo <magnus.lidbom@gmail.com>
2014-03-25 18:00:13 +0100

    Lets use display account details as the first test because displayregisteraccountview has no dependencies and almost zero logic..

--- Samples/AccountManagement/AccountManagement.Web/Views/RegisterAccount/DisplayAccountRegistrationViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Composable.DDD;
-
-namespace AccountManagement.Web.Views.RegisterAccount
-{
-    public class DisplayAccountRegistrationViewModel : ValueObject<DisplayAccountRegistrationViewModel>
-    {
-        public readonly string PageTitle =  DisplayAccountRegistrationViewModelResources.Title;
-    }
-}

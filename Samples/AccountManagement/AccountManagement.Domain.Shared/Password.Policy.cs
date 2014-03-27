d7dfa9e3b9215cc1910267812e7890154cb05ee5
mlidbo <magnus.lidbom@gmail.com>
2014-03-27 20:52:35 +0100

    Rename Failures.ContainsWhitespace Failures.BorderedByWhitespace

--- Samples/AccountManagement/AccountManagement.Domain.Shared/Password.Policy.cs
+++ Samples/AccountManagement/AccountManagement.Domain.Shared/Password.Policy.cs
@@ -10,7 +10,7 @@ namespace AccountManagement.Domain.Shared
     {
         public static class Policy
         {
-            ///<summary><para>returns a list of the ways in which a specific password fails to fulfill the policy. If the list is empty the password is matches the policy.</para> </summary>
+            ///<summary><para>returns a list of the ways in which a specific password fails to fulfill the policy. If the list is empty the password is compliant with the policy.</para> </summary>
             public static IEnumerable<Failures> GetPolicyFailures(string password)
             {
                 if(password == null)
@@ -21,7 +21,7 @@ namespace AccountManagement.Domain.Shared
                 var failures = new List<Failures>();
                 //Create a simple extension to keep the code short an expressive and DRY. If AddIf is unclear just hover your pointer over the method and the documentation comment should clear everything up.
                 failures.AddIf(password.Length < 4, Failures.ShorterThanFourCharacters);
-                failures.AddIf(password.Trim() != password, Failures.ContainsWhitespace);
+                failures.AddIf(password.Trim() != password, Failures.BorderedByWhitespace);
                 failures.AddIf(password.ToLower() == password, Failures.MissingUppercaseCharacter);
                 failures.AddIf(password.ToUpper() == password, Failures.MissingLowerCaseCharacter);
                 return failures;
@@ -41,11 +41,11 @@ namespace AccountManagement.Domain.Shared
             [Flags]
             public enum Failures
             {
-                Null = 1,
+                Null = 1,//Make sure all values are powers of 2 so that the flags can be combined freely.
                 MissingUppercaseCharacter = 2,
                 MissingLowerCaseCharacter = 4,
                 ShorterThanFourCharacters = 8,
-                ContainsWhitespace = 16
+                BorderedByWhitespace = 16
             }
         }
     }

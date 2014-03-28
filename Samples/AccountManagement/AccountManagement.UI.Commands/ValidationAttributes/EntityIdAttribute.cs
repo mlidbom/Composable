d15a26a1d121946adace5cd5abc824991b37557a
mlidbo <magnus.lidbom@gmail.com>
2014-03-28 21:10:51 +0100

    Improve documentation comments.

--- Samples/AccountManagement/AccountManagement.UI.Commands/ValidationAttributes/EntityIdAttribute.cs
+++ Samples/AccountManagement/AccountManagement.UI.Commands/ValidationAttributes/EntityIdAttribute.cs
@@ -9,7 +9,7 @@ namespace AccountManagement.UI.Commands.ValidationAttributes
         {
             if(value == null)
             {
-                return true; //We validate that values are correct emails. Not that they are present. That is for the Required attribute.
+                return true; //We validate that values are correct entity ids. Not that they are present. That is the job of the Required attribute.
             }
             return (Guid)value != Guid.Empty;
         }

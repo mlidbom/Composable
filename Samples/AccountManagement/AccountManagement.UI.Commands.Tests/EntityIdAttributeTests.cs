03e1538f5973add2722324386ba2c70b30db6527
mlidbo <magnus.lidbom@gmail.com>
2014-03-31 19:42:07 +0200

    Fix test coverage.

---
+++ Samples/AccountManagement/AccountManagement.UI.Commands.Tests/EntityIdAttributeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AccountManagement.UI.Commands.ValidationAttributes;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AccountManagement.UI.Commands.Tests
+{
+    [TestFixture]
+    public class EntityIdAttributeTests
+    {
+        [Test]
+        public void IsValidIfIdIsNull()
+        {
+            new EntityIdAttribute().IsValid(null).Should().Be(true);
+        }
+
+        [Test]
+        public void IsNotValidIfIdIsEmpty()
+        {
+            new EntityIdAttribute().IsValid(Guid.Empty).Should().Be(false);
+        }
+    }
+}

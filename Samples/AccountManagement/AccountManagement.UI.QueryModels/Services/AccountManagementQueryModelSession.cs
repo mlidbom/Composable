bf516c71c4725a232e8b1bd21bb9b9a8c2dad2b3
mlidbo <magnus.lidbom@gmail.com>
2014-04-02 16:33:58 +0200

    Checkpoint

--- Samples/AccountManagement/AccountManagement.UI.QueryModels/Services/AccountManagementQueryModelSession.cs
+++ Samples/AccountManagement/AccountManagement.UI.QueryModels/Services/AccountManagementQueryModelSession.cs
@@ -1,8 +1,10 @@
 ﻿using Composable.KeyValueStorage;
 using Composable.SystemExtensions.Threading;
+using JetBrains.Annotations;
 
 namespace AccountManagement.UI.QueryModels.Services
 {
+    [UsedImplicitly]
     internal class AccountManagementQueryModelSession : DocumentDbSession, IAccountManagementQueryModelSession
     {
         public AccountManagementQueryModelSession(IDocumentDb backingStore, ISingleContextUseGuard usageGuard, IDocumentDbSessionInterceptor interceptor)

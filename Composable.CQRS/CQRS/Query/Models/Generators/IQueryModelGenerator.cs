80f5d9f8cefb39433201528063c3bac9dbc7c83b
mlidbo <magnus.lidbom@gmail.com>
2014-04-02 18:51:00 +0200

    Whip the query model generating code into decent shape.

---
+++ Composable.CQRS/CQRS/Query/Models/Generators/IQueryModelGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Composable.CQRS.Query.Models.Generators
+{
+    public interface IQueryModelGenerator { }
+
+
+    public interface IQueryModelGenerator<out TDocument> : IQueryModelGenerator
+    {
+        TDocument TryGenerate(Guid id);
+    }    
+}
---
+++ Composable.CQRS/CQRS/Query/Models/Generators/SingleAggregateQueryModelGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using Composable.CQRS.EventHandling;
+using Composable.CQRS.EventSourcing;
+using Composable.System.Linq;
+
+namespace Composable.CQRS.Query.Models.Generators
+{
+    public abstract class SingleAggregateQueryModelGenerator<TImplementer, TViewModel, TEvent, TSession> : IQueryModelGenerator<TViewModel>
+        where TImplementer : SingleAggregateQueryModelGenerator<TImplementer, TViewModel, TEvent, TSession>
+        where TSession : IEventStoreReader
+        where TEvent : IAggregateRootEvent
+        where TViewModel : class, ISingleAggregateQueryModel, new()
+    {
+        private readonly CallMatchingHandlersInRegistrationOrderEventDispatcher<TEvent> _eventDispatcher = new CallMatchingHandlersInRegistrationOrderEventDispatcher<TEvent>();
+        protected readonly TSession Session;
+        protected TViewModel Model { get; set; }
+
+        protected SingleAggregateQueryModelGenerator(TSession session)
+        {
+            Session = session;
+            _eventDispatcher.RegisterHandlers()
+                .ForGenericEvent<IAggregateRootCreatedEvent>(e => Model.SetId(e.AggregateRootId))
+                .ForGenericEvent<IAggregateRootDeletedEvent>(e => Model = null);
+        }
+
+        ///<summary>Registers handlers for the incoming events. All matching handlers will be called in the order they were registered.</summary>
+        protected CallMatchingHandlersInRegistrationOrderEventDispatcher<TEvent>.RegistrationBuilder RegisterHandlers()
+        {
+            return _eventDispatcher.RegisterHandlers();
+        }
+
+        public TViewModel TryGenerate(Guid id)
+        {
+            var history = Session.GetHistory(id).Cast<TEvent>().ToList();
+            if(history.None())
+            {
+                return null;
+            }
+            var queryModel = new TViewModel();
+            Model = queryModel;
+            history.ForEach(_eventDispatcher.Dispatch);
+            var result = Model;//Yes it does make sense. Look at the registered handler for IAggregateRootDeletedEvent
+            Model = null;
+            return result;
+        }
+    }
+}

91beb9f0364c46a76c0ae79a080fa8fb779fac9d
mlidbo <magnus.lidbom@gmail.com>
2014-04-02 14:46:36 +0200

    Bump packages on the develop branch.

--- Composable.CQRS.NHibernate/Properties/AssemblyInfo.cs
+++ Composable.CQRS.NHibernate/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Composable.CQRS.NHibernate")]
 [assembly: AssemblyProduct("Composable.CQRS.NHibernate")]
 
-[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
--- Composable.CQRS/Properties/AssemblyInfo.cs
+++ Composable.CQRS/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Composable.CQRS")]
 [assembly: AssemblyProduct("Composable.CQRS")]
 
-[assembly: AssemblyVersion("2.2.1.0")]
+[assembly: AssemblyVersion("2.3.0.0")]

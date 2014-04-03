f1a9cff48b9d680600a550653693d1d8ab2e19a6
mlidbo <magnus.lidbom@gmail.com>
2014-04-03 10:25:17 +0200

    Some cleanup

--- Samples/AccountManagement/AccountManagement.UI.QueryModels.Generators/ContainerInstallers/AccountManagementQueryModelGeneratingQueryModelSessionInstaller.cs
+++ Samples/AccountManagement/AccountManagement.UI.QueryModels.Generators/ContainerInstallers/AccountManagementQueryModelGeneratingQueryModelSessionInstaller.cs
@@ -1,5 +1,4 @@
-﻿using AccountManagement.UI.QueryModels.DocumentDB.Readers.Services;
-using AccountManagement.UI.QueryModels.EventStore.Generators.Services;
+﻿using AccountManagement.UI.QueryModels.EventStore.Generators.Services;
 using AccountManagement.UI.QueryModels.Services;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;

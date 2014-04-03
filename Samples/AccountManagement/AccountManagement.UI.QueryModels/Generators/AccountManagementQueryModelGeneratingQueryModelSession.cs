309b4d12c1254b7b5b53033f01fd4b24e2bb3461
mlidbo <magnus.lidbom@gmail.com>
2014-04-03 22:35:45 +0200

    Properly expose checking if an account exists through IAccountManagementQueryModelsReader

--- Samples/AccountManagement/AccountManagement.UI.QueryModels/Generators/AccountManagementQueryModelGeneratingQueryModelSession.cs
+++ Samples/AccountManagement/AccountManagement.UI.QueryModels/Generators/AccountManagementQueryModelGeneratingQueryModelSession.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using AccountManagement.Domain.Shared;
+using AccountManagement.UI.QueryModels.DocumentDb;
 using AccountManagement.UI.QueryModels.Services;
 using Composable.CQRS.Query.Models.Generators;
 using Composable.KeyValueStorage;
@@ -9,15 +11,33 @@ namespace AccountManagement.UI.QueryModels.Generators
 {
     public class AccountManagementQueryModelGeneratingQueryModelSession : QueryModelGeneratingDocumentDbReader,  IAccountManagementQueryModelsReader
     {
+        private readonly IAccountManagementDocumentDbReader _documentDbReader;
+
         public AccountManagementQueryModelGeneratingQueryModelSession(
             ISingleContextUseGuard usageGuard, 
             IDocumentDbSessionInterceptor interceptor,
-            IEnumerable<IAccountManagementQueryModelGenerator> documentGenerators) 
-            : base(usageGuard, interceptor, documentGenerators) {}
+            IEnumerable<IAccountManagementQueryModelGenerator> documentGenerators,
+            IAccountManagementDocumentDbReader documentDbReader) 
+            : base(usageGuard, interceptor, documentGenerators)
+        {
+            _documentDbReader = documentDbReader;
+        }
 
         public AccountQueryModel GetAccount(Guid accountId)
         {
             return Get<AccountQueryModel>(accountId);
+        }
+
+        public bool TryGetAccountByEmail(Email accountEmail, out AccountQueryModel account)
+        {
+            EmailToAccountMapQueryModel accountMap;
+            if(_documentDbReader.TryGet(accountEmail.ToString(), out accountMap))
+            {
+                account = GetAccount(accountMap.AccountId);
+                return true;
+            }
+            account = null;
+            return false;
         }
     }
 }

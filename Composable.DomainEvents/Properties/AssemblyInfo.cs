3cfc11df272c8624a7d2ec45820e17af88dc4b01
mlidbo <magnus.lidbom@gmail.com>
2014-04-03 14:18:37 +0200

    Update to latest composable.

--- Composable.DomainEvents/Properties/AssemblyInfo.cs
+++ Composable.DomainEvents/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("Composable.DomainEvents")]
 [assembly: AssemblyProduct("Composable.DomainEvents")]
 
-[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyVersion("2.3.1.0")]

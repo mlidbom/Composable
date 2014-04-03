ef0962596c594ee1a85d447afe74115f18a9b5d0
mlidbo <magnus.lidbom@gmail.com>
2014-04-03 17:48:50 +0200

    Move to using the generator session fully.

---
+++ Samples/AccountManagement/AccountManagement.UI.QueryModels.DocumentDB.Readers/Services/IAccountManagementDocumentDBReader.cs
@@ -0,0 +1,9 @@
+﻿using Composable.KeyValueStorage;
+
+namespace AccountManagement.UI.QueryModels.DocumentDB.Readers.Services
+{
+    public interface IAccountManagementDocumentDbReader : IDocumentDbReader
+    {
+         
+    }
+}

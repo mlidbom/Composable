c68f4e66bcfff42a3c1024d9cf02378da2e60f82
mlidbo <magnus.lidbom@gmail.com>
2014-04-12 13:22:53 +0200

    Make sure that adding,removing and notifying observers is thread safe. Create some classes for creating observables easily.

---
+++ Composable.System/System/Reactive/SimpleObservable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Composable.System.Linq;
+
+namespace Composable.System.Reactive
+{
+    public class SimpleObservable<TEvent> : IObservable<TEvent>
+    {
+        private readonly HashSet<IObserver<TEvent>> _observerCollection = new HashSet<IObserver<TEvent>>();
+
+        public void OnNext(TEvent @event)
+        {
+            _observerCollection.ForEach(objserver => objserver.OnNext(@event));
+        }
+
+        public IDisposable Subscribe(IObserver<TEvent> observer)
+        {
+            _observerCollection.Add(observer);
+            return new DisposeAction(() => _observerCollection.Remove(observer));
+        }
+    }
+}
---
+++ Composable.System/System/Reactive/ThreadSafeObservable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Composable.System.Reactive
+{
+    public class ThreadSafeObservable<TEvent> : IObservable<TEvent>
+    {
+        private readonly ThreadSafeObserverCollection<TEvent> _observerCollection = new ThreadSafeObserverCollection<TEvent>();
+
+        public void OnNext(TEvent @event)
+        {
+            _observerCollection.Notify(@event);
+        }
+
+        public IDisposable Subscribe(IObserver<TEvent> observer)
+        {
+            _observerCollection.Add(observer);
+            return new DisposeAction(() => _observerCollection.Remove(observer));
+        }
+    }
+}

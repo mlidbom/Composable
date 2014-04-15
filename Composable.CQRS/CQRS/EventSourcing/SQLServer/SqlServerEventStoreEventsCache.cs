bd635e7f72452535ff99efabaf9e0041d8faa4e0
mlidbo <magnus.lidbom@gmail.com>
2014-04-15 10:58:37 +0200

    Fix bug mixing up events from different stores for aggregates with the same id.

---
+++ Composable.CQRS/CQRS/EventSourcing/SQLServer/SqlServerEventStoreEventsCache.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using Composable.System;
+
+namespace Composable.CQRS.EventSourcing.SQLServer
+{
+    public class SqlServerEventStoreEventsCache
+    {
+        private static readonly ConcurrentDictionary<string, SqlServerEventStoreEventsCache> ConnectionStringToCacheMap = new ConcurrentDictionary<string, SqlServerEventStoreEventsCache>(); 
+        
+        private MemoryCache _internalCache = new MemoryCache("EventStore");
+
+        public SqlServerEventStoreEventsCache()
+        {
+            
+        }
+
+        public static SqlServerEventStoreEventsCache ForConnectionString(string connectionString)
+        {
+            return ConnectionStringToCacheMap.GetOrAdd(connectionString, new SqlServerEventStoreEventsCache());
+        }
+
+        private readonly CacheItemPolicy Policy = new CacheItemPolicy()
+                                                         {
+                                                             //todo: this way of doing cache expiration is unlikely to be acceptable in the long run....
+                                                             SlidingExpiration = 20.Minutes()
+                                                         };
+
+        public List<IAggregateRootEvent> Get(Guid id)
+        {
+            var cached = _internalCache.Get(id.ToString());
+            if(cached == null)
+            {
+                return new List<IAggregateRootEvent>();
+            }
+            //Make sure each caller gets their own copy.
+            return ((List<IAggregateRootEvent>)cached).ToList();
+
+        }
+
+        public void Store(Guid id, IEnumerable<IAggregateRootEvent> events)
+        {
+             
+            _internalCache.Set(key: id.ToString(), policy: Policy, value: events.ToList());
+        }
+        public void Clear()
+        {
+            _internalCache.Dispose();
+            _internalCache = new MemoryCache("name");
+        }
+
+        public void Remove(Guid id)
+        {
+            _internalCache.Remove(key: id.ToString());
+        }
+    }
+}

35c0ec4cb8a5559350debc622d0de64eebb341b9
mlidbo <magnus.lidbom@gmail.com>
2014-05-04 13:37:31 +0200

    Standardise the account tests to use scenarios.

---
+++ Samples/AccountManagement/AccountManagement.TestHelpers/Scenarios/ChangeEmailScenario.cs
@@ -0,0 +1,29 @@
+﻿using AccountManagement.Domain;
+using AccountManagement.Domain.Shared;
+using Castle.Windsor;
+using Composable.CQRS.Windsor;
+
+namespace AccountManagement.TestHelpers.Scenarios
+{
+    public class ChangeAccountEmailScenario
+    {
+        private readonly IWindsorContainer _container;
+
+        public Email NewEmail = TestData.Email.CreateValidEmail();
+        public readonly Email OldEmail;
+        public readonly Account Account;
+
+        public ChangeAccountEmailScenario(IWindsorContainer container)
+        {
+            _container = container;
+            var registerAccountScenario = new RegisterAccountScenario(container);
+            Account = registerAccountScenario.Execute();
+            OldEmail = Account.Email;
+        }
+
+        public void Execute()
+        {            
+            _container.ExecuteUnitOfWork(() => Account.ChangeEmail(NewEmail));
+        }
+    }
+}

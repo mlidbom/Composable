81a959ce017f1b6048e4f7d8d5745fbd2afb56d2
mlidbo <magnus.lidbom@gmail.com>
2014-08-17 17:26:01 +0200

    Get rid of the obsolete querying code.

--- Composable.CQRS/CQRS/Query/IQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Composable.CQRS.Query
-{
-    [Obsolete("Will be removed soon. Please switch to using another solution for queries.")]
-    public interface IQuery<TQuery, TQueryResult> where TQuery : IQuery<TQuery, TQueryResult>
-    {
-    }
-}

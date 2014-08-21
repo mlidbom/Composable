2289ae92526e8ebdf43b53d7e9c70143ab8b0e42
mlidbo <magnus.lidbom@gmail.com>
2014-08-21 17:33:57 +0200

    Use IMessageMutator instead of IMessageHandler to do message inspection.

---
+++ Composable.CQRS.ServiceBus.NServiceBus/EndpointConfiguration/CatchSerializationErrorsMessageInspector.cs
@@ -0,0 +1,21 @@
+using System.Runtime.Serialization;
+using Composable.CQRS.EventSourcing;
+using JetBrains.Annotations;
+using NServiceBus;
+using NServiceBus.MessageMutator;
+
+namespace Composable.CQRS.ServiceBus.NServiceBus.EndpointConfiguration
+{
+    public class CatchSerializationErrorsMessageInspector : IMutateIncomingMessages
+    {
+        //todo: Try to check via the bus that the message actually implements all the interfaces that it should instead of this simplistic check for errors.
+        public object MutateIncoming(object message)
+        {
+            if (message.GetType() == typeof(IMessage) || message.GetType() == typeof(AggregateRootEvent))
+            {
+                throw new SerializationException("Message failed to serialize correctly");
+            }
+            return message;
+        }
+    }
+}

19b99ab57c035a41e74a75a368e1ade3ca389c99
Richie <zhangyang@shinetechchina.com>
2015-05-19 11:29:21 +0200

    move the invoke logic to separate class

---
+++ Composable.CQRS/ServiceBus/MessageHandlersExtentions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Composable.System.Linq;
+
+namespace Composable.ServiceBus
+{
+    internal static class MessageHandlersExtentions
+    {
+        internal static void Invoke(this MessageHandlerResolver.MessageHandlers messageHandlers)
+        {
+            foreach (var handlerInstance in messageHandlers.HandlerInstances)
+            {
+                MessageHandlerInvoker.GetMethodsToInvoke(handlerInstance.GetType(), messageHandlers.HandlerInterfaceType)
+                    .Where(holder => holder.HandledMessageType.IsInstanceOfType(messageHandlers.Message))
+                    .Select(holder => holder.HandlerMethod)
+                    .ForEach(handlerMethod => handlerMethod(handlerInstance, messageHandlers.Message));
+            }
+        }
+    }
+}

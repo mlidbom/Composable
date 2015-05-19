10ed0423908e2799c8d711726a64265d047a7b89
Richie <zhangyang@shinetechchina.com>
2015-05-19 09:47:59 +0200

    some tests and improve

--- Composable.CQRS.Tests/ServiceBus/SynchronousBusTests.cs
+++ Composable.CQRS.Tests/ServiceBus/SynchronousBusTests.cs
@@ -1,37 +1,30 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Castle.MicroKernel.Lifestyle;
 using Castle.MicroKernel.Registration;
-using Castle.MicroKernel.Registration.Interceptor;
 using Castle.Windsor;
 using Composable.ServiceBus;
 using Composable.SystemExtensions.Threading;
 using FluentAssertions;
 using NServiceBus;
-using NSpec;
 using NUnit.Framework;
 
 namespace CQRS.Tests.ServiceBus
 {
     [TestFixture]
-    public class WhenHandleBasedMessageTests
+    public class SynchronousBusTests
     {
         private IWindsorContainer _container;
         private IDisposable _scope;
 
         [SetUp]
-        public void SetupConatinerAndBeginScope()
+        public void SetupContainerAndBeginScope()
         {
             _container = new WindsorContainer();
             _container.Register(
                 Component.For<IServiceBus>().ImplementedBy<SynchronousBus>().LifestyleScoped(),
                 Component.For<IWindsorContainer>().Instance(_container).LifestyleScoped(),
-                //Component.For<ISynchronousBusSubscriberFilter>().Instance(PublishToAllSubscribersSubscriberFilter.Instance).LifestyleScoped(),
                 Component.For<ISingleContextUseGuard>().ImplementedBy<SingleThreadUseGuard>(),
-                Component.For<CandidateUpdater, IHandleMessages<INamePropertyUpdatedMessage>, IHandleMessages<IAgePropertyUpdatedMessage>, IHandleMessages<AnotherMessage>>()
+                Component.For<CandidateUpdater, IHandleMessages<INamePropertyUpdatedMessage>, IHandleInProcessMessages<IAgePropertyUpdatedMessage>, IHandleRemoteMessages<IPhonePropertyUpdatedMessage>>()
                     .ImplementedBy<CandidateUpdater>().LifestyleScoped(),
                 Component.For<CandidateMessageSpy>().Instance(CandidateMessageSpy.Instance).LifestyleSingleton()
                 );
@@ -50,9 +43,10 @@ namespace CQRS.Tests.ServiceBus
         public void When_publish_a_message_should_invoke_multiple_interface_handler()
         {
             //Arrange
-            var name = "Candidate1";
-            var age = 30;
-            var editCandidateMessage = new CandidateEditedMessage(name: name, age: age);
+            const string name = "Candidate1";
+            const int age = 30;
+            const string phone = "phoneNumber";
+            var editCandidateMessage = new CandidateEditedMessage(name: name, age: age, phone: phone);
 
             //Act
             _container.Resolve<IServiceBus>().Publish(editCandidateMessage);
@@ -68,35 +62,19 @@ namespace CQRS.Tests.ServiceBus
         public void When_send_a_message_should_invoke_multiple_interface_handler()
         {
             //Arrange
-            var name = "Candidate2";
-            var age = 32;
-            var editCandidateMessage = new CandidateEditedMessage(name: name, age: age);
+            const string name = "Candidate2";
+            const int age = 32;
+            const string phone = "phoneNumber";
+            var editCandidateMessage = new CandidateEditedMessage(name: name, age: age, phone: phone);
 
             //Act
-            _container.Resolve<IServiceBus>().Send(editCandidateMessage);
+            Action send= () => _container.Resolve<IServiceBus>().Send(editCandidateMessage);
 
             //Assert
-            var candidate = _container.Resolve<CandidateMessageSpy>();
-            candidate.Name.Should().Be(name);
-            candidate.Age.Should().Be(age);
-            candidate.Email.Should().Be(null);
+            send.ShouldThrow<MultipleMessageHandlersRegisteredException>("multiple handlers registered for message");
         }
 
-        [Test]
-        public void Should_only_handle_message_type_relevant()
-        {
-            //Arrange
-            var anotherMessage = new AnotherMessage("xx@xx.com");
-
-            //Act
-            _container.Resolve<IServiceBus>().Publish(anotherMessage);
-
-            //Assert
-            var candidate = _container.Resolve<CandidateMessageSpy>();
-            candidate.Email.Should().Be("xx@xx.com");
-            candidate.Name.Should().Be(null);
-            candidate.Age.Should().Be(0);
-        }
+      
 
         internal interface INamePropertyUpdatedMessage:IMessage
         {
@@ -108,9 +86,15 @@ namespace CQRS.Tests.ServiceBus
             int Age { get; }
         }
 
+        internal interface IPhonePropertyUpdatedMessage:IMessage
+        {
+            string Phone { get; }
+        }
+
         internal interface ICandidateEditedMessage
             :INamePropertyUpdatedMessage,
-            IAgePropertyUpdatedMessage
+            IAgePropertyUpdatedMessage,
+            IPhonePropertyUpdatedMessage
             
         {
 
@@ -118,26 +102,22 @@ namespace CQRS.Tests.ServiceBus
 
         internal class CandidateEditedMessage : ICandidateEditedMessage
         {
-            public CandidateEditedMessage(string name,int age )
+            public CandidateEditedMessage(string name,int age,string phone)
             {
                 Name = name;
                 Age = age;
+                Phone = phone;
             }
             public string Name { get; private set; }
             public int Age { get; private set; }
+            public string Phone { get; private set; }
         }
 
-        internal class AnotherMessage : IMessage
-        {
-            public string Email { get; private set; }
 
-            public AnotherMessage(string email)
-            {
-                Email = email;
-            }
-        }
-
-        internal class CandidateUpdater : IHandleMessages<INamePropertyUpdatedMessage>, IHandleMessages<IAgePropertyUpdatedMessage>, IHandleMessages<AnotherMessage>
+        internal class CandidateUpdater : 
+            IHandleMessages<INamePropertyUpdatedMessage>,
+            IHandleInProcessMessages<IAgePropertyUpdatedMessage>, 
+            IHandleRemoteMessages<IPhonePropertyUpdatedMessage>
         {
             private readonly CandidateMessageSpy _candidateMessage;
 
@@ -156,10 +136,11 @@ namespace CQRS.Tests.ServiceBus
                 _candidateMessage.Age = message.Age;
             }
 
-            public void Handle(AnotherMessage message)
+            public void Handle(IPhonePropertyUpdatedMessage message)
             {
-                _candidateMessage.Email = message.Email;
+                _candidateMessage.Phone = message.Phone;
             }
+            
         }
 
         internal class CandidateMessageSpy
@@ -167,6 +148,7 @@ namespace CQRS.Tests.ServiceBus
             public string Name { get; set; }
             public int Age { get; set; }
             public string Email { get; set; }
+            public string Phone { get; set; }
 
             private static CandidateMessageSpy _messageSpy;
 

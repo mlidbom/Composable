860f89c34ddf70e0dcf76ac5e19be2e604d3967e
mlidbo <magnus.lidbom@gmail.com>
2015-05-20 19:04:49 +0200

    Break out the class that creates the invocation methods.

--- Composable.CQRS/ServiceBus/MessageHandlerInvoker.cs
+++ Composable.CQRS/ServiceBus/MessageHandlerInvoker.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System.Linq;
 using Castle.Windsor;
 using Composable.System.Linq;
 
@@ -10,8 +6,6 @@ namespace Composable.ServiceBus
 {
     internal class MessageHandlerInvoker
     {
-        private static readonly ConcurrentDictionary<HandlerId, List<MethodToInvokeOnMessageHandlerByMessageType>> HandlerToMessageHandlersMap = new ConcurrentDictionary<HandlerId, List<MethodToInvokeOnMessageHandlerByMessageType>>();
-
         private readonly IWindsorContainer _container;
         private readonly MyMessageHandlerResolver _handlerResolver;
 
@@ -43,27 +37,12 @@ namespace Composable.ServiceBus
             {                
                 if(isSend)
                 {
-                    if(handlers.Length == 0)
-                    {
-                        throw new NoHandlerException(message.GetType());
-                    }
-                    if(handlers.Length > 1)
-                    {
-                        var realHandlers = handlers.Select(handler => handler.Instance)
-                            .Where(handler => !(handler is ISynchronousBusMessageSpy))
-                            .ToList();
-                        if(realHandlers.Count > 1)
-                        {
-                            throw new MultipleMessageHandlersRegisteredException(message, realHandlers);
-                        }
-                    }
+                    AssertThatThereIsExactlyOneRegisteredHandler(handlers, message);
                 }
 
                 foreach(var messageHandlerReference in handlers)
                 {
-                    GetMethodsToInvoke(messageHandlerReference.Instance.GetType(), messageHandlerReference.HandlerInterfaceType)
-                        .Where(holder => holder.HandledMessageType.IsInstanceOfType(message))
-                        .Select(holder => holder.HandlerMethod)
+                    IndividualHandlerInvocationMethodFactory.GetMethodsToInvoke(messageHandlerReference.Instance.GetType(), messageHandlerReference.HandlerInterfaceType, message)
                         .ForEach(handlerMethod => handlerMethod(messageHandlerReference.Instance, message));
                 }
 
@@ -74,89 +53,22 @@ namespace Composable.ServiceBus
             }
         }
 
-        private class HandlerId
+        private void AssertThatThereIsExactlyOneRegisteredHandler(MyMessageHandlerResolver.MessageHandlerReference[] handlers, object message)
         {
-            public Type InstanceType { get; private set; }
-            public Type HandlerInterfaceType { get; private set; }
-
-            public HandlerId(Type instanceType,Type handlerInterfaceType)
+            if (handlers.Length == 0)
             {
-                InstanceType = instanceType;
-                HandlerInterfaceType = handlerInterfaceType;
+                throw new NoHandlerException(message.GetType());
             }
-
-            public override bool Equals(object obj)
+            if (handlers.Length > 1)
             {
-                if (obj == null || GetType() != obj.GetType())
-                    return false;
-
-                var key = obj as HandlerId;
-                return key.InstanceType == InstanceType && key.HandlerInterfaceType == HandlerInterfaceType;
-            }
-
-            public override int GetHashCode()
-            {
-                return InstanceType.GetHashCode() + HandlerInterfaceType.GetHashCode();
-            }
-        }
-
-        //Creates a list of handlers, one per handler type. Each handler is a mapping between message type and what method to invoke when handling this specific type of message.
-        private static IEnumerable<MethodToInvokeOnMessageHandlerByMessageType> GetMethodsToInvoke(Type handlerInstanceType, Type handlerInterfaceType)
-        {
-            List<MethodToInvokeOnMessageHandlerByMessageType> messageHandleHolders;
-
-            if (!HandlerToMessageHandlersMap.TryGetValue(new HandlerId(handlerInstanceType, handlerInterfaceType), out messageHandleHolders))
-            {
-                var holders = new List<MethodToInvokeOnMessageHandlerByMessageType>();
-
-                    var handledMessageTypes = handlerInstanceType.GetInterfaces()
-                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerInterfaceType)
-                    .Select(i => i.GetGenericArguments().First())
+                var realHandlers = handlers.Select(handler => handler.Instance)
+                    .Where(handler => !(handler is ISynchronousBusMessageSpy))
                     .ToList();
-
-                    handledMessageTypes.ForEach(messageType =>
-                    {
-                        var action = TryGetMethodToInvokeWhenHandlingTypeOfMessage(handlerInstanceType, messageType, handlerInterfaceType);
-                        if (action != null)
-                        {
-                            holders.Add(new MethodToInvokeOnMessageHandlerByMessageType(messageType, action));
-                        }
-
-                    });
-
-
-                    HandlerToMessageHandlersMap[new HandlerId(handlerInstanceType, handlerInterfaceType)] = holders;
+                if (realHandlers.Count > 1)
+                {
+                    throw new MultipleMessageHandlersRegisteredException(message, realHandlers);
+                }
             }
-
-            return HandlerToMessageHandlersMap[new HandlerId(handlerInstanceType, handlerInterfaceType)];
-        }
-
-        //Returns an action that can be used to invoke this handler for a specific type of message.
-        private static Action<object, object> TryGetMethodToInvokeWhenHandlingTypeOfMessage(Type handlerInstanceType, Type messageType, Type handlerInterfaceType)
-        {
-            var messageHandlerInterfaceType = handlerInterfaceType.MakeGenericType(messageType);
-            
-            var methodInfo = handlerInstanceType.GetInterfaceMap(messageHandlerInterfaceType).TargetMethods.First();
-            var messageHandlerParameter = Expression.Parameter(typeof(object));
-            var parameter = Expression.Parameter(typeof(object));
-
-            var convertMessageHandler = Expression.Convert(messageHandlerParameter, handlerInstanceType);
-            var convertParameter = Expression.Convert(parameter, methodInfo.GetParameters().First().ParameterType);
-            var execute = Expression.Call(convertMessageHandler, methodInfo, convertParameter);
-            return Expression.Lambda<Action<object, object>>(execute, messageHandlerParameter, parameter).Compile();
-        }
-    }
-
-    ///<summary>Used to hold a single implementation of a message handler</summary>
-    internal class MethodToInvokeOnMessageHandlerByMessageType
-    {
-        public Type HandledMessageType { get; private set; }
-        public Action<object, object> HandlerMethod { get; private set; }
-
-        public MethodToInvokeOnMessageHandlerByMessageType(Type handledMessageType, Action<object, object> handlerMethod)
-        {
-            HandledMessageType = handledMessageType;
-            HandlerMethod = handlerMethod;
-        }
+        }        
     }
 }

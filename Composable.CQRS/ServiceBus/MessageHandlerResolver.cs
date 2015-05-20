9aea4476aa218aa9f97d01fdc17d9994d324a5af
Richie <zhangyang@shinetechchina.com>
2015-05-20 04:35:01 +0200

    fix filter for ResolveMessageHandlers

--- Composable.CQRS/ServiceBus/MessageHandlerResolver.cs
+++ Composable.CQRS/ServiceBus/MessageHandlerResolver.cs
@@ -22,18 +22,17 @@ namespace Composable.ServiceBus
                 .ToArray();
         }
 
-        //override public MessageHandlers ResolveMessageHandlers<TMessage>(TMessage message)
-        //{
-        //    //var handlers = base.ResolveMessageHandlers(message).HandlerInstances
-        //    //    // ReSharper disable once UseIsOperator.2
-        //    //        .Where(h=>!typeof(IHandleRemoteMessages<>).IsInstanceOfType(h))
-        //    //        .ToList()
-        //    //        ;
+        override public MessageHandlers ResolveMessageHandlers<TMessage>(TMessage message)
+        {
+            var remoteMessageHandlerType = typeof(IHandleRemoteMessages<>).MakeGenericType(message.GetType());
+            var handlers = base.ResolveMessageHandlers(message).HandlerInstances
+                // ReSharper disable once UseIsOperator.2
+                    .Where(h => !remoteMessageHandlerType.IsInstanceOfType(h))
+                    .ToList()
+                    ;
 
-        //    //return new MessageHandlers(message, InterfaceType, handlers);
-
-        //    return base.ResolveMessageHandlers(message);
-        //}
+            return new MessageHandlers(message, InterfaceType, handlers);
+        }
     }
 
     internal class InProcessMessageHandlerResolver : MessageHandlerResolver

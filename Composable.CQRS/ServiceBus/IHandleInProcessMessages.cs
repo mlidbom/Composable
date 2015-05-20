694146d4261448d1cb29ab3a8270ad2f203374d3
rnilsa <richard.nilsark@ffcg.se>
2015-05-20 13:54:09 +0800

    Comments

--- Composable.CQRS/ServiceBus/IHandleInProcessMessages.cs
+++ Composable.CQRS/ServiceBus/IHandleInProcessMessages.cs
@@ -3,7 +3,7 @@ using NServiceBus;
 
 namespace Composable.ServiceBus
 {
-    /// <summary> Should be implemented by message handlers that should only listen for messages dispatched by  <see cref="SynchronousBus"/>.</summary>
+    /// <summary> Defines a message handler that should only listen for messages dispatched by <see cref="SynchronousBus"/>.</summary>
     public interface IHandleInProcessMessages<T> where T : IMessage
     {
         void Handle(T message);

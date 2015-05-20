da1449a8e550069abf65cd1f7ae9a9ff239058e5
mlidbo <magnus.lidbom@gmail.com>
2015-05-20 20:22:31 +0200

    Simplify test.

--- Composable.CQRS.Tests/ServiceBus/WhenHandlingAMessageThatInheritsOtherMessages.cs
+++ Composable.CQRS.Tests/ServiceBus/WhenHandlingAMessageThatInheritsOtherMessages.cs
@@ -3,7 +3,6 @@ using Composable.ServiceBus;
 using FluentAssertions;
 using NServiceBus;
 using NUnit.Framework;
-using System;
 
 namespace CQRS.Tests.ServiceBus
 {
@@ -15,129 +14,54 @@ namespace CQRS.Tests.ServiceBus
         {
             Container.Register(
                 Component.For<CandidateUpdater, IHandleMessages<INamePropertyUpdatedMessage>, IHandleInProcessMessages<IAgePropertyUpdatedMessage>>()
-                    .ImplementedBy<CandidateUpdater>().LifestyleScoped(),
-                Component.For<CandidateMessageSpy>().Instance(CandidateMessageSpy.Instance).LifestyleSingleton()
+                    .ImplementedBy<CandidateUpdater>()
+                    .LifestyleScoped()
                 );
         }
 
-        [TearDown]
-        public void ResetSpy()
+        [Test]
+        public void When_publishing_a_message_all_matching_handlers_should_be_invoked()
         {
-            Container.Resolve<CandidateMessageSpy>().Reset();
+            SynchronousBus.Publish(new CandidateEditedMessage());
+
+            var candidateUpdater = Container.Resolve<CandidateUpdater>();
+            candidateUpdater.NamePropertyUpdatedEventReceived.Should().Be(true);
+            candidateUpdater.AgePropertyUpdatedEventReceived.Should().Be(true);
         }
 
         [Test]
-        public void When_publishing_a_message_Then_multiple_message_handlers_should_be_invoked()
+        public void Sending_a_message_throws_MultipleHandlersRegisteredException()
         {
-            //Arrange
-            const string name = "Candidate1";
-            const int age = 30;
-            const string phone = "phoneNumber";
-            var editCandidateMessage = new CandidateEditedMessage(name: name, age: age, phone: phone);
-
-            //Act
-            SynchronousBus.Publish(editCandidateMessage);
-
-            //Assert
-            var candidate = Container.Resolve<CandidateMessageSpy>();
-            candidate.Name.Should().Be(name);
-            candidate.Age.Should().Be(age);
+            SynchronousBus.Invoking(bus => bus.Send(new CandidateEditedMessage()))
+                .ShouldThrow<MultipleMessageHandlersRegisteredException>("multiple handlers registered for message");
         }
 
-        [Test]
-        public void When_sending_a_message_Then_should_throw_exception()
-        {
-            //Arrange
-            const string name = "Candidate2";
-            const int age = 32;
-            const string phone = "phoneNumber";
-            var editCandidateMessage = new CandidateEditedMessage(name: name, age: age, phone: phone);
+        public interface INamePropertyUpdatedMessage : IMessage {}
 
-            //Act
-            Action send = () => SynchronousBus.Send(editCandidateMessage);
+        public interface IAgePropertyUpdatedMessage : IMessage { }
 
-            //Assert
-            send.ShouldThrow<MultipleMessageHandlersRegisteredException>("multiple handlers registered for message");
-        }
+        public interface ICandidateEditedMessage :
+            INamePropertyUpdatedMessage,
+            IAgePropertyUpdatedMessage {}
 
-        internal interface INamePropertyUpdatedMessage : IMessage
-        {
-            string Name { get; }
-        }
-
-        internal interface IAgePropertyUpdatedMessage : IMessage
-        {
-            int Age { get; }
-        }
-
-        internal interface ICandidateEditedMessage
-            : INamePropertyUpdatedMessage,
-            IAgePropertyUpdatedMessage
-            
-        {
-
-        }
-
-        internal class CandidateEditedMessage : ICandidateEditedMessage
-        {
-            public CandidateEditedMessage(string name,int age,string phone)
-            {
-                Name = name;
-                Age = age;
-                Phone = phone;
-            }
-            public string Name { get; private set; }
-            public int Age { get; private set; }
-            public string Phone { get; private set; }
-        }
+        public class CandidateEditedMessage : ICandidateEditedMessage { }
 
 
-        private class CandidateUpdater : 
+        public class CandidateUpdater :
             IHandleMessages<INamePropertyUpdatedMessage>,
             IHandleInProcessMessages<IAgePropertyUpdatedMessage>
         {
-            private readonly CandidateMessageSpy _candidateMessage;
-
-            public CandidateUpdater(CandidateMessageSpy candidateMessage)
-            {
-                _candidateMessage = candidateMessage;
-            }
+            public bool NamePropertyUpdatedEventReceived;
+            public bool AgePropertyUpdatedEventReceived;
 
             public void Handle(INamePropertyUpdatedMessage message)
             {
-                _candidateMessage.Name = message.Name;
+                NamePropertyUpdatedEventReceived = true;
             }
 
             public void Handle(IAgePropertyUpdatedMessage message)
             {
-                _candidateMessage.Age = message.Age;
-            }
-        }
-
-        private class CandidateMessageSpy : ISynchronousBusMessageSpy
-        {
-            public string Name { get; set; }
-            public int Age { get; set; }
-            public string Phone { get; set; }
-
-            private static CandidateMessageSpy _messageSpy;
-
-            private CandidateMessageSpy()
-            {
-                
-            }
-            public static CandidateMessageSpy Instance
-            {
-                get
-                {
-                    if(_messageSpy==null) _messageSpy=new CandidateMessageSpy();
-                    return _messageSpy;
-                }
-            }
-
-            public void Reset()
-            {
-                _messageSpy = new CandidateMessageSpy();
+                AgePropertyUpdatedEventReceived = true;
             }
         }
     }

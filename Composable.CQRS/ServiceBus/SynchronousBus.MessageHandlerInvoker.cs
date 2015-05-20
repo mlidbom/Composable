1ead6d6262b7601237f11db271eda32949718ff1
mlidbo <magnus.lidbom@gmail.com>
2015-05-20 23:49:33 +0200

    Move MessageHandlerId to its own file.

--- Composable.CQRS/ServiceBus/SynchronousBus.MessageHandlerInvoker.cs
+++ Composable.CQRS/ServiceBus/SynchronousBus.MessageHandlerInvoker.cs
@@ -46,38 +46,6 @@ namespace Composable.ServiceBus
                             new MessageHandlerMethod(implementingClass: implementingClass, messageType: messageType, genericInterfaceImplemented: genericInterfaceImplemented))
                     .ToList();
             }
-
-            private class MessageHandlerId
-            {
-                private Type ImplementingClass { get; set; }
-                private Type GenericInterfaceImplemented { get; set; }
-
-                public MessageHandlerId(Type implementingClass, Type genericInterfaceImplemented)
-                {
-                    ImplementingClass = implementingClass;
-                    GenericInterfaceImplemented = genericInterfaceImplemented;
-                }
-
-                override public bool Equals(object other)
-                {
-                    if(other == null || GetType() != other.GetType())
-                    {
-                        return false;
-                    }
-
-                    return Equals((MessageHandlerId)other);
-                }
-
-                private bool Equals(MessageHandlerId other)
-                {
-                    return other.ImplementingClass == ImplementingClass && other.GenericInterfaceImplemented == GenericInterfaceImplemented;
-                }
-
-                override public int GetHashCode()
-                {
-                    return ImplementingClass.GetHashCode() + GenericInterfaceImplemented.GetHashCode();
-                }
-            }
         }
     }
 }

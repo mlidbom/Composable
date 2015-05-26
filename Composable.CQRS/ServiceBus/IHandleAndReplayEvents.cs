2f58d06d4182e5e2c6baae1e424070e9a7ef9ef6
Richie <zhangyang@shinetechchina.com>
2015-05-26 16:49:30 +0200

    add IHandleAndReplayEvents hierarchy

---
+++ Composable.CQRS/ServiceBus/IHandleAndReplayEvents.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace Composable.ServiceBus
+{
+    // ReSharper disable once PossibleInterfaceMemberAmbiguity
+    public  interface IHandleAndReplayEvents<TEvent>:IReplayEvents<TEvent>,IHandleMessages<TEvent> where TEvent:IMessage
+    {
+       
+    }
+   
+}
---
+++ Composable.CQRS/ServiceBus/IReplayEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace Composable.ServiceBus
+{
+    public interface IReplayEvents<in TEvent> where TEvent:IMessage
+    {
+        void Handle(TEvent @event);
+    }
+}

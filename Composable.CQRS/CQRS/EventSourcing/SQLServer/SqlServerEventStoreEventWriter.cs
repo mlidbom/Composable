525d57898b300fd47bd7cc9fb127b8837434750e
mlidbo <magnus.lidbom@gmail.com>
2015-08-06 20:28:38 +0200

    Extract: SqlServerEventStoreEventWriter

---
+++ Composable.CQRS/CQRS/EventSourcing/SQLServer/SqlServerEventStoreEventWriter.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Composable.CQRS.EventSourcing.SQLServer
+{
+    internal class SqlServerEventStoreEventWriter
+    {
+        private readonly SqlServerEventStoreConnectionManager _connectionMananger;
+        private readonly SqlServerEvestStoreEventSerializer _eventSerializer;
+
+        public SqlServerEventStoreEventWriter(SqlServerEventStoreConnectionManager connectionMananger, SqlServerEvestStoreEventSerializer eventSerializer)
+        {
+            _connectionMananger = connectionMananger;
+            _eventSerializer = eventSerializer;
+        }
+
+        public void Insert(IEnumerable<IAggregateRootEvent> events)
+        {
+            using(var connection = _connectionMananger.OpenConnection())
+            {
+                foreach(var @event in events)
+                {
+                    using(var command = connection.CreateCommand())
+                    {
+                        command.CommandType = CommandType.Text;
+
+                        command.CommandText +=
+                            $@"
+INSERT {EventTable.Name} With(READCOMMITTED, ROWLOCK) 
+(       {EventTable.Columns.AggregateId},  {EventTable.Columns.AggregateVersion},  {EventTable.Columns.EventType},  {EventTable.Columns.EventId},  {EventTable.Columns.TimeStamp},  {EventTable.Columns.Event}) 
+VALUES(@{EventTable.Columns.AggregateId}, @{EventTable.Columns.AggregateVersion}, @{EventTable.Columns.EventType}, @{EventTable.Columns.EventId}, @{EventTable.Columns.TimeStamp}, @{EventTable.Columns.Event})";
+
+                        command.Parameters.Add(new SqlParameter(EventTable.Columns.AggregateId, @event.AggregateRootId));
+                        command.Parameters.Add(new SqlParameter(EventTable.Columns.AggregateVersion, @event.AggregateRootVersion));
+                        command.Parameters.Add(new SqlParameter(EventTable.Columns.EventType, @event.GetType().FullName));
+                        command.Parameters.Add(new SqlParameter(EventTable.Columns.EventId, @event.EventId));
+                        command.Parameters.Add(new SqlParameter(EventTable.Columns.TimeStamp, @event.TimeStamp));
+
+                        command.Parameters.Add(new SqlParameter(EventTable.Columns.Event, _eventSerializer.Serialize(@event)));
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}

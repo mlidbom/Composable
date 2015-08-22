f66dcfd6c7aa04fea16a92612b90c9dfd71a46cb
mlidbo <magnus.lidbom@gmail.com>
2015-08-22 21:38:56 +0200

    Add failing test to expose transaction isolation bug in EventStoreSession

--- Composable.CQRS.Tests/CQRS/EventSourcing/Sql/SqlServerEventStoreSessionTests.cs
+++ Composable.CQRS.Tests/CQRS/EventSourcing/Sql/SqlServerEventStoreSessionTests.cs
@@ -1,4 +1,9 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Transactions;
 using Castle.Windsor;
 using Composable.CQRS.EventSourcing;
 using Composable.CQRS.EventSourcing.SQLServer;
@@ -23,6 +28,87 @@ namespace CQRS.Tests.CQRS.EventSourcing.Sql
         protected override IEventStore CreateStore()
         {
             return new SqlServerEventStore(ConnectionString);
+        }
+
+        [Test]
+        public void Serializes_access_to_an_aggregate_so_that_concurrent_transactions_succeed_even_if_history_has_been_read_outside_of_modifying_transactions()
+        {
+            var store = CreateStore();
+
+            var user = new User();
+            user.Register("email@email.se", "password", Guid.NewGuid());
+            using (var session = OpenSession(store))
+            {
+                session.Save(user);
+                session.SaveChanges();
+            }
+
+            Action updateEmail = () =>
+            {
+                using (var session = OpenSession(store))
+                {
+                    var prereadHistory = ((IEventStoreReader)session).GetHistory(user.Id);
+                    using (var transaction = new TransactionScope())
+                    {
+                        var userToUpdate = session.Get<User>(user.Id);
+                        userToUpdate.ChangeEmail($"newemail_{userToUpdate.Version}@somewhere.not");
+                        Thread.Sleep(100);
+                        session.SaveChanges();
+                        transaction.Complete();
+                    }//Sql duplicate key (AggregateId, Version) Exception would be thrown here if history was not serialized 
+                }
+            };
+
+            var task1 = Task.Factory.StartNew(updateEmail);
+            var task2 = Task.Factory.StartNew(updateEmail);
+            var task3 = Task.Factory.StartNew(updateEmail);
+
+            Task.WaitAll(task1, task2, task3);
+
+            using (var session = OpenSession(store))
+            {
+                var userHistory = ((IEventStoreReader)session).GetHistory(user.Id).ToArray();//Reading the aggregate will throw an exception if the history is invalid.
+            }
+        }
+
+        [Test]
+        public void Serializes_access_to_an_aggregate_so_that_concurrent_transactions_succeed()
+        {
+            var store = CreateStore();
+
+            var user = new User();
+            user.Register("email@email.se", "password", Guid.NewGuid());
+            using (var session = OpenSession(store))
+            {
+                session.Save(user);
+                session.SaveChanges();
+            }
+
+            Action updateEmail = () =>
+            {
+                using (var session = OpenSession(store))
+                {
+                    using (var transaction = new TransactionScope())
+                    {
+                        var userToUpdate = session.Get<User>(user.Id);
+                        userToUpdate.ChangeEmail($"newemail_{userToUpdate.Version}@somewhere.not");
+                        Thread.Sleep(100);
+                        session.SaveChanges();
+                        transaction.Complete();
+                    }//Sql duplicate key (AggregateId, Version) Exception would be thrown here if history was not serialized 
+                }
+            };
+
+            var task1 = Task.Factory.StartNew(updateEmail);
+            var task2 = Task.Factory.StartNew(updateEmail);
+            var task3 = Task.Factory.StartNew(updateEmail);
+
+            Task.WaitAll(task1, task2, task3);
+
+            using (var session = OpenSession(store))
+            {
+                var userHistory = ((IEventStoreReader)session).GetHistory(user.Id).ToArray();//Reading the aggregate will throw an exception if the history is invalid.
+            }
         }
     }
 }

76e0ec302af27425f6def2d4a7e18bf92d4b1946
mlidbo <magnus.lidbom@gmail.com>
2015-11-09 01:58:21 +0100

    Reformatting and some refactoring.

--- Composable.CQRS/CQRS/EventSourcing/SQLServer/EventTableSchemaManager.cs
+++ Composable.CQRS/CQRS/EventSourcing/SQLServer/EventTableSchemaManager.cs
@@ -5,81 +5,80 @@ namespace Composable.CQRS.EventSourcing.SQLServer
         override public string Name { get; } = EventTable.Name;
 
         override public string CreateTableSql => $@"
-CREATE TABLE [dbo].[{Name}](
-    {EventTable.Columns.InsertionOrder} [bigint] IDENTITY(1,1) NOT NULL,
-    {EventTable.Columns.InsertAfter} [bigint] null,
-    {EventTable.Columns.InsertBefore} [bigint] null,
-    {EventTable.Columns.Replaces} [bigint] null,
-    {EventTable.Columns.ManualReadOrder} decimal(38,19) null,
-	{EventTable.Columns.AggregateId} [uniqueidentifier] NOT NULL,
-	{EventTable.Columns.AggregateVersion} [int] NOT NULL,
-	{EventTable.Columns.TimeStamp} [datetime] NOT NULL,
-    {EventTable.Columns.SqlInsertDateTime} [datetime2] default SYSUTCDATETIME(),
-    {EventTable.Columns.EventType} [int] NOT NULL,
-	{EventTable.Columns.EventId} [uniqueidentifier] NOT NULL,
-	{EventTable.Columns.Event} [nvarchar](max) NOT NULL,
-	{EventTable.Columns.EffectiveReadOrder} as case 
-		when {EventTable.Columns.ManualReadOrder} is not null then {EventTable.Columns.ManualReadOrder}
-        when {EventTable.Columns.InsertAfter} is null and {EventTable.Columns.InsertBefore} is null and {EventTable.Columns.Replaces} is null then cast({EventTable.Columns.InsertionOrder} as decimal(38,19))
-		else null
-	end
+CREATE TABLE dbo.{Name}(
+    {EventTable.Columns.InsertionOrder} bigint IDENTITY(1,1) NOT NULL,
+    {EventTable.Columns.InsertAfter} bigint null,
+    {EventTable.Columns.InsertBefore} bigint null,
+    {EventTable.Columns.Replaces} bigint null,
+    {EventTable.Columns.ManualReadOrder} {EventTable.ReadOrderType} null,
+    {EventTable.Columns.AggregateId} uniqueidentifier NOT NULL,
+    {EventTable.Columns.AggregateVersion} int NOT NULL,
+    {EventTable.Columns.TimeStamp} datetime NOT NULL,
+    {EventTable.Columns.SqlInsertDateTime} datetime2 default SYSUTCDATETIME(),
+    {EventTable.Columns.EventType} int NOT NULL,
+    {EventTable.Columns.EventId} uniqueidentifier NOT NULL,
+    {EventTable.Columns.Event} nvarchar(max) NOT NULL,
+    {EventTable.Columns.EffectiveReadOrder} as case 
+        when {EventTable.Columns.ManualReadOrder} is not null then {EventTable.Columns.ManualReadOrder}
+        when {EventTable.Columns.InsertAfter} is null and {EventTable.Columns.InsertBefore} is null and {EventTable.Columns.Replaces} is null then cast({EventTable.Columns.InsertionOrder} as {EventTable.ReadOrderType})
+        else null
+    end
 
-    CONSTRAINT [IX_Uniq2_{EventTable.Columns.EventId}] UNIQUE
+    CONSTRAINT PK_{Name} PRIMARY KEY CLUSTERED 
     (
-	    {EventTable.Columns.EventId}
-    ),
-
-    CONSTRAINT [IX_Uniq_{EventTable.Columns.InsertionOrder}] UNIQUE
-    (
-	    {EventTable.Columns.InsertionOrder}
-    ),
-    CONSTRAINT CK_Only_one_reordering_column_specified
-    CHECK 
-    (
-	    ({EventTable.Columns.InsertAfter} is null and {EventTable.Columns.InsertBefore} is null)
-	    or
-	    ({EventTable.Columns.InsertAfter} is null and {EventTable.Columns.Replaces} is null)
-	    or
-	    ({EventTable.Columns.InsertBefore} is null and {EventTable.Columns.Replaces} is null) 
-    ),
-
-    CONSTRAINT [PK_{Name}] PRIMARY KEY CLUSTERED 
-    (
-	    {EventTable.Columns.AggregateId} ASC,
-	    {EventTable.Columns.AggregateVersion} ASC
+        {EventTable.Columns.AggregateId} ASC,
+        {EventTable.Columns.AggregateVersion} ASC
     )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = OFF) ON [PRIMARY],
 
-    CONSTRAINT FK_Events_{EventTable.Columns.EventType} FOREIGN KEY ({EventTable.Columns.EventType}) 
+    CONSTRAINT IX_{Name}_Unique_{EventTable.Columns.EventId} UNIQUE
+    (
+        {EventTable.Columns.EventId}
+    ),
+
+    CONSTRAINT IX_{Name}_Unique_{EventTable.Columns.InsertionOrder} UNIQUE
+    (
+        {EventTable.Columns.InsertionOrder}
+    ),
+
+    CONSTRAINT CK_{Name}_Only_one_reordering_column_allowed_for_use
+    CHECK 
+    (
+        ({EventTable.Columns.InsertAfter} is null and {EventTable.Columns.InsertBefore} is null)
+        or
+        ({EventTable.Columns.InsertAfter} is null and {EventTable.Columns.Replaces} is null)
+        or
+        ({EventTable.Columns.InsertBefore} is null and {EventTable.Columns.Replaces} is null) 
+    ),
+
+    CONSTRAINT FK_{Name}_{EventTable.Columns.EventType} FOREIGN KEY ({EventTable.Columns.EventType}) 
         REFERENCES {EventTypeTable.Name} ({EventTypeTable.Columns.Id}),
 
-    CONSTRAINT FK_{EventTable.Columns.Replaces} FOREIGN KEY ( {EventTable.Columns.Replaces} ) 
-        REFERENCES Event ({EventTable.Columns.InsertionOrder}),
+    CONSTRAINT FK_{Name}_{EventTable.Columns.Replaces} FOREIGN KEY ( {EventTable.Columns.Replaces} ) 
+        REFERENCES {Name} ({EventTable.Columns.InsertionOrder}),
 
-    CONSTRAINT FK_{EventTable.Columns.InsertBefore} FOREIGN KEY ( {EventTable.Columns.InsertBefore} )
-        REFERENCES Event ({EventTable.Columns.InsertionOrder}),
+    CONSTRAINT FK_{Name}_{EventTable.Columns.InsertBefore} FOREIGN KEY ( {EventTable.Columns.InsertBefore} )
+        REFERENCES {Name} ({EventTable.Columns.InsertionOrder}),
 
-    CONSTRAINT FK_{EventTable.Columns.InsertAfter} FOREIGN KEY ( {EventTable.Columns.InsertAfter} ) 
-        REFERENCES Event ({EventTable.Columns.InsertionOrder})
+    CONSTRAINT FK_{Name}_{EventTable.Columns.InsertAfter} FOREIGN KEY ( {EventTable.Columns.InsertAfter} ) 
+        REFERENCES {Name} ({EventTable.Columns.InsertionOrder})
  
-) ON [PRIMARY]
+    ) ON [PRIMARY]
 
-CREATE UNIQUE NONCLUSTERED INDEX [{EventTable.Columns.InsertionOrder}] ON [dbo].[{Name}]
-(
-	[{EventTable.Columns.InsertionOrder}] ASC,
-    [{EventTable.Columns.EventType}] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+    CREATE NONCLUSTERED INDEX IX_{Name}_{EventTable.Columns.EffectiveReadOrder} ON dbo.{Name}
+        ({EventTable.Columns.EffectiveReadOrder} ASC)
+        INCLUDE ({EventTable.Columns.EventType}, {EventTable.Columns.InsertionOrder})
 
-CREATE NONCLUSTERED INDEX [IX_{EventTable.Columns.Replaces}]	ON [dbo].[{Name}] 
-	([{EventTable.Columns.Replaces}])
-	INCLUDE ([{EventTable.Columns.InsertionOrder}])
+    CREATE NONCLUSTERED INDEX IX_{Name}_{EventTable.Columns.Replaces}	ON dbo.{Name}
+        ({EventTable.Columns.Replaces})
+        INCLUDE ({EventTable.Columns.InsertionOrder})
 
-CREATE NONCLUSTERED INDEX [IX_{EventTable.Columns.InsertAfter}]	ON [dbo].[{Name}] 
-	([{EventTable.Columns.InsertAfter}])
-	INCLUDE ([{EventTable.Columns.InsertionOrder}])
+    CREATE NONCLUSTERED INDEX IX_{Name}_{EventTable.Columns.InsertAfter}	ON dbo.{Name}
+        ({EventTable.Columns.InsertAfter})
+        INCLUDE ({EventTable.Columns.InsertionOrder})
 
-CREATE NONCLUSTERED INDEX [IX_{EventTable.Columns.InsertBefore}]	ON [dbo].[{Name}] 
-	([{EventTable.Columns.InsertBefore}])
-	INCLUDE ([{EventTable.Columns.InsertionOrder}])
+    CREATE NONCLUSTERED INDEX IX_{Name}_{EventTable.Columns.InsertBefore}	ON dbo.{Name} 
+        ({EventTable.Columns.InsertBefore})
+        INCLUDE ({EventTable.Columns.InsertionOrder})
 
 ";
 
@@ -93,10 +92,10 @@ declare @{EventTable.Columns.InsertBefore} bigint
 declare @{EventTable.Columns.InsertAfter} bigint
 declare @{EventTable.Columns.Replaces} bigint
 declare @EventsToReorder bigint
-declare @BeforeReadOrder decimal(38,19)
-declare @AfterReadOrder decimal(38,19)
-declare @AvailableSpaceBetwenReadOrders decimal(38,19)
-declare @Increment decimal(38,19)
+declare @BeforeReadOrder {EventTable.ReadOrderType}
+declare @AfterReadOrder {EventTable.ReadOrderType}
+declare @AvailableSpaceBetwenReadOrders {EventTable.ReadOrderType}
+declare @Increment {EventTable.ReadOrderType}
 declare @Done bit 
 set @Done = 0
 
@@ -136,7 +135,7 @@ begin
 		begin 
 		   select @EventsToReorder = count(*) from {Name} where {EventTable.Columns.InsertAfter} = @{EventTable.Columns.InsertAfter}
 		   select @BeforeReadOrder = abs({EventTable.Columns.EffectiveReadOrder}) from {Name} where {EventTable.Columns.InsertionOrder} = @{EventTable.Columns.InsertAfter}
-		   select TOP 1 @AfterReadOrder = {EventTable.Columns.EffectiveReadOrder} from {Name} where {EventTable.Columns.EffectiveReadOrder} > @BeforeReadOrder and ({EventTable.Columns.InsertAfter} is null or {EventTable.Columns.InsertAfter} != @{EventTable.Columns.InsertAfter}) order by {EventTable.Columns.EffectiveReadOrder}
+		   select top 1 @AfterReadOrder = {EventTable.Columns.EffectiveReadOrder} from {Name} where {EventTable.Columns.EffectiveReadOrder} > @BeforeReadOrder and ({EventTable.Columns.InsertAfter} is null or {EventTable.Columns.InsertAfter} != @{EventTable.Columns.InsertAfter}) order by {EventTable.Columns.EffectiveReadOrder}
 
 		   set @AvailableSpaceBetwenReadOrders = @AfterReadOrder - @BeforeReadOrder
 		   set @Increment = @AvailableSpaceBetwenReadOrders / (@EventsToReorder + 1)
@@ -157,9 +156,9 @@ begin
 		   select @AfterReadOrder = abs({EventTable.Columns.EffectiveReadOrder}) from {Name} where {EventTable.Columns.InsertionOrder} = @{EventTable.Columns.InsertBefore}
 
 
-		   select TOP 1 @BeforeReadOrder = {EventTable.Columns.EffectiveReadOrder} from {Name} where {EventTable.Columns.EffectiveReadOrder} < @AfterReadOrder and ({EventTable.Columns.InsertBefore} is null or {EventTable.Columns.InsertBefore} != @{EventTable.Columns.InsertBefore}) order by {EventTable.Columns.EffectiveReadOrder} DESC
+		   select top 1 @BeforeReadOrder = {EventTable.Columns.EffectiveReadOrder} from {Name} where {EventTable.Columns.EffectiveReadOrder} < @AfterReadOrder and ({EventTable.Columns.InsertBefore} is null or {EventTable.Columns.InsertBefore} != @{EventTable.Columns.InsertBefore}) order by {EventTable.Columns.EffectiveReadOrder} DESC
 		   if(@BeforeReadOrder is null or @BeforeReadOrder < 0)
-				set @BeforeReadOrder = cast(0 as decimal(38,19)) --We are inserting before the first event in the whole event store and possibly the original first event has been replace and thus has a negative {EventTable.Columns.EffectiveReadOrder}
+				set @BeforeReadOrder = cast(0 as {EventTable.ReadOrderType}) --We are inserting before the first event in the whole event store and possibly the original first event has been replaced and thus has a negative {EventTable.Columns.EffectiveReadOrder}
 
 		   set @AvailableSpaceBetwenReadOrders = @AfterReadOrder - @BeforeReadOrder
 		   set @Increment = @AvailableSpaceBetwenReadOrders / (@EventsToReorder + 1)

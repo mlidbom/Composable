a773517d5875ad0aea6619b3a54bee282c796de0
mlidbo <magnus.lidbom@gmail.com>
2015-11-09 03:00:43 +0100

    Small refactorings.

--- Composable.CQRS.Tests/CQRS/EventSourcing/Sql/ReadOrderImplementationExperimentTests.cs
+++ Composable.CQRS.Tests/CQRS/EventSourcing/Sql/ReadOrderImplementationExperimentTests.cs
@@ -24,7 +24,7 @@ namespace CQRS.Tests.CQRS.EventSourcing.Sql
         [Test]
         public void PrintCreateReadOrdersSql()
         {
-            Console.WriteLine(new EventTableSchemaManager().CreateManualOrderEntriesSql);
+            Console.WriteLine(new EventTableSchemaManager().UpdateManualReadOrderValuesSql);
         }
 
         [Test]

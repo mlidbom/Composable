2648c03cae380103cfc1ab3597bb1dd2fcb17d02
mlidbo <magnus.lidbom@gmail.com>
2015-11-12 11:03:23 +0100

    Checkpoint

--- Composable.CQRS/CQRS/EventSourcing/EventRefactoring/Migrations/IEventMigration.cs
+++ Composable.CQRS/CQRS/EventSourcing/EventRefactoring/Migrations/IEventMigration.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using Composable.System.Linq;
 
@@ -19,7 +20,6 @@ namespace Composable.CQRS.EventSourcing.EventRefactoring.Migrations
         void InspectEvent(IAggregateRootEvent @event, IEventModifier modifier);
     }
 
-
     public abstract class EventMigration : IEventMigration
     {
         public Guid Id { get; }
@@ -30,8 +30,67 @@ namespace Composable.CQRS.EventSourcing.EventRefactoring.Migrations
         public abstract void InspectEvent(IAggregateRootEvent @event, IEventModifier modifier);
     }
 
+    internal class EventModifier : IEventModifier
+    {
+        private IAggregateRootEvent _event;
+        private IEnumerable<IAggregateRootEvent> _replacementEvents;
+        private IEnumerable<IAggregateRootEvent> _insertedEvents;
+
+        public EventModifier(IAggregateRootEvent @event) { _event = @event; }
+
+        public void Replace(IEnumerable<IAggregateRootEvent> events)
+        {
+            Contract.Assert(_replacementEvents == null, "You can only call Replace once");
+
+            _replacementEvents = events;
+        }
+
+        public void InsertBefore(IEnumerable<IAggregateRootEvent> events)
+        {
+            Contract.Assert(_insertedEvents == null, "You can only call InsertBefore once");
+
+            _insertedEvents = events;
+        }
+
+        public IEnumerable<IAggregateRootEvent> GetMutated()
+        {
+            if(_insertedEvents != null)
+            {
+                foreach(var inserted in _insertedEvents)
+                {
+                    yield return inserted;
+                }
+            }
+
+            if(_replacementEvents != null)
+            {
+                foreach(var inserted in _replacementEvents)
+                {
+                    yield return inserted;
+                }
+            }
+            else
+            {
+                yield return _event;
+            }
+        }
+    }
+
     internal class EventStreamMutator
     {
-        public IEnumerable<IAggregateRootEvent> Mutate(IEnumerable<IAggregateRootEvent> @events) { return @events; } 
+        private readonly IEventMigration[] _eventMigrations;
+        public EventStreamMutator(params IEventMigration[] eventMigrations) { _eventMigrations = eventMigrations; }
+
+        private IEnumerable<IAggregateRootEvent> Mutate(IAggregateRootEvent @event, IEventMigration migration)
+        {
+            var modifier = new EventModifier(@event);
+            migration.InspectEvent(@event, modifier);
+            return modifier.GetMutated();
+        }
+
+        public IEnumerable<IAggregateRootEvent> Mutate(IAggregateRootEvent @event)
+        {
+            return Mutate(@event, _eventMigrations.Single());
+        }
     }
 }
--- Composable.CQRS/CQRS/EventSourcing/EventRefactoring/Migrations/_Interfaces.cs
+++ Composable.CQRS/CQRS/EventSourcing/EventRefactoring/Migrations/_Interfaces.cs
@@ -7,7 +7,6 @@ namespace Composable.CQRS.EventSourcing.EventRefactoring.Migrations
         void Replace(IEnumerable<IAggregateRootEvent> events);
         void InsertBefore(IEnumerable<IAggregateRootEvent> events);
         //void InsertAfter(IEnumerable<IAggregateRootEvent> events); //Will not support guaranteeing that the migration is stable(Does not recursively change the stream again and again.) and will therefore not be supported
-        void Ignore();
     }
 
     public interface IReplaceMyself<TEvent>

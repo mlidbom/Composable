ac0b602d1d01fa3ac852e24b1270614e7957614d
mlidbo <magnus.lidbom@gmail.com>
2015-11-12 16:48:52 +0100

    Write tests to expose bug with aggregate version.

---
+++ Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/BeforeEventType.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Composable.CQRS.EventSourcing;
+using Composable.CQRS.EventSourcing.EventRefactoring.Migrations;
+using Composable.System.Linq;
+
+namespace CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations
+{
+    public class BeforeEventType<TEvent> : EventMigration
+    {
+        private readonly IEnumerable<Type> _insert;
+        private readonly List<Type> _seenEventTypes = new List<Type>();
+
+        public static BeforeEventType<TEvent> Insert<T1>() => new BeforeEventType<TEvent>(Seq.OfTypes<T1>());
+        public static BeforeEventType<TEvent> Insert<T1, T2>() => new BeforeEventType<TEvent>(Seq.OfTypes<T1, T2>());
+        public static BeforeEventType<TEvent> Insert<T1, T2, T3>() => new BeforeEventType<TEvent>(Seq.OfTypes<T1, T2, T3>());
+
+        public BeforeEventType(IEnumerable<Type> insert) { _insert = insert; }
+
+        public override void InspectEvent(IAggregateRootEvent @event, IEventModifier modifier)
+        {
+            if (@event.GetType() == typeof(TEvent) && _seenEventTypes.Last() != _insert.Last())
+            {
+                modifier.InsertBefore(_insert.Select(Activator.CreateInstance).Cast<IAggregateRootEvent>().ToList());
+            }
+
+            _seenEventTypes.Add(@event.GetType());
+        }
+    }
+}
--- Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/ReplaceEventType.cs
+++ Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/ReplaceEventType.cs
@@ -25,27 +25,4 @@ namespace CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations
             }
         }
     }
-
-
-    public class BeforeEventType<TEvent> : EventMigration
-    {
-        private readonly IEnumerable<Type> _insert;
-        private List<Type> _seenEventType = new List<Type>();
-
-        public static BeforeEventType<TEvent> Insert<T1>() => new BeforeEventType<TEvent>(Seq.OfTypes<T1>());
-        public static BeforeEventType<TEvent> Insert<T1, T2>() => new BeforeEventType<TEvent>(Seq.OfTypes<T1, T2>());
-        public static BeforeEventType<TEvent> Insert<T1, T2, T3>() => new BeforeEventType<TEvent>(Seq.OfTypes<T1, T2, T3>());
-
-        public BeforeEventType(IEnumerable<Type> insert) { _insert = insert; }
-
-        public override void InspectEvent(IAggregateRootEvent @event, IEventModifier modifier)
-        {
-            if (@event.GetType() == typeof(TEvent) && _seenEventType.Last() != _insert.Last())
-            {
-                modifier.InsertBefore(_insert.Select(Activator.CreateInstance).Cast<IAggregateRootEvent>().ToList());
-            }
-
-            _seenEventType.Add(@event.GetType());
-        }
-    }
 }

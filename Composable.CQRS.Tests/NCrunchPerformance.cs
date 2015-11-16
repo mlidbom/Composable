032fff7c3d47602711a9a701072633acbab1d1af
mlidbo <magnus.lidbom@gmail.com>
2015-11-16 22:47:05 +0100

    Add adjustments in the tests for ncrunch performance slowdown.

---
+++ Composable.CQRS.Tests/NCrunchPerformance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Composable.System;
+
+namespace CQRS.Tests
+{
+    public static class NCrunchPerformance
+    {
+        private static bool IsRunningInNcrunch => NCrunch.Framework.NCrunchEnvironment.NCrunchIsResident();
+        private static double NCRunchSlowDownFactor = 5.0;
+
+        public static TimeSpan AdjustRuntime(TimeSpan original, double boost = 1.0)
+        {
+            if (IsRunningInNcrunch)
+            {
+                return ((int)(original.TotalMilliseconds * (NCRunchSlowDownFactor + boost))).Milliseconds();
+            }
+            return original;
+        }
+
+        public static int AdjustIterations(int original, double boost = 1.0)
+        {
+            if (IsRunningInNcrunch)
+            {
+                return (int)(original / (NCRunchSlowDownFactor + boost));
+            }
+            return original;
+        }
+    }
+}

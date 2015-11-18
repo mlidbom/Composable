e20d97c8e87a53b38bdd5e1f38c73a97826e36a9
mlidbo <magnus.lidbom@gmail.com>
2015-11-18 22:58:26 +0100

    Fix spelling.

--- Composable.CQRS/CQRS/Windsor/Testing/IConfigureWiringForTests.cs
+++ Composable.CQRS/CQRS/Windsor/Testing/IConfigureWiringForTests.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Composable.CQRS.Windsor.Testing
 {
-    [Obsolete("This inteface is obsolete and will soon be removed. Please use Composable.Windsor.Testing instead. Search and replac 'using Composable.CQRS.Windsor.Testing;' with 'using Composable.Windsor.Testing;'")]
+    [Obsolete("This inteface is obsolete and will soon be removed. Please use Composable.Windsor.Testing instead. Search and replace 'using Composable.CQRS.Windsor.Testing;' with 'using Composable.Windsor.Testing;'")]
     public interface IConfigureWiringForTests
     {
         void ConfigureWiringForTesting();

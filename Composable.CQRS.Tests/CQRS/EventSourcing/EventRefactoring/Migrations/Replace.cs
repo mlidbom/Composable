d5e7661410ac8cfb9ee880ee7eb6762da09a44ea
mlidbo <magnus.lidbom@gmail.com>
2015-11-19 17:39:42 +0100

    Use a singleton for the migrator when possible.

--- Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/Replace.cs
+++ Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/Replace.cs
@@ -10,7 +10,7 @@ namespace CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations
 {
     public class Replace<TEvent> : EventMigration<IRootEvent>
     {
-        private readonly IEnumerable<Type> _replaceWith;
+        private readonly Migrator _migratorSingleton;
 
         public static Replace<TEvent> With<T1>() => new Replace<TEvent>(Seq.OfTypes<T1>());
         public static Replace<TEvent> With<T1, T2>() => new Replace<TEvent>(Seq.OfTypes<T1, T2>());
@@ -18,10 +18,10 @@ namespace CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations
 
         private Replace(IEnumerable<Type> replaceWith) : base(Guid.Parse("9B51F7BC-D9B3-43C7-A183-76CA5E662091"), "Replace", "Long description of Replace")
         {
-            _replaceWith = replaceWith;
+            _migratorSingleton = new Migrator(replaceWith);
         }
 
-        public override ISingleAggregateInstanceHandlingEventMigrator CreateSingleAggregateInstanceHandlingMigrator() { return new Migrator(_replaceWith); }
+        public override ISingleAggregateInstanceHandlingEventMigrator CreateSingleAggregateInstanceHandlingMigrator() { return _migratorSingleton; }
 
         private class Migrator : ISingleAggregateInstanceHandlingEventMigrator
         {

e162fdbf2f6879a5e9e56521c57adc9d15923cea
mlidbo <magnus.lidbom@gmail.com>
2015-11-20 14:17:32 +0100

    Try to detect and fix type mapping problems in a way that allow for runtime assemblies that do not contain all of the event types in the event store.

--- Composable.CQRS/CQRS/EventSourcing/Refactoring/Naming/DefaultEventNameMapper.cs
+++ Composable.CQRS/CQRS/EventSourcing/Refactoring/Naming/DefaultEventNameMapper.cs
@@ -6,6 +6,16 @@ namespace Composable.CQRS.EventSourcing.Refactoring.Naming
     public class DefaultEventNameMapper : IEventNameMapper
     {
         public string GetName(Type eventType) => eventType.FullName;
-        public Type GetType(string eventTypeName) => eventTypeName.AsType();
+        public Type GetType(string eventTypeName)
+        {
+            try
+            {
+                return eventTypeName.AsType();
+            }
+            catch(System.Reflection.TypeExtensions.FailedToFindTypeException exception)
+            {
+                throw new CouldNotFindTypeBasedOnName(eventTypeName, exception);
+            }
+        }
     }
 }

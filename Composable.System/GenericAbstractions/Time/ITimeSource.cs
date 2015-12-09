3436f90f3f8a378afc1388f010b573091e6709b2
mlidbo <magnus.lidbom@gmail.com>
2015-12-09 17:08:06 +0100

    Get the wiring support in and obsolete ITimeSource.

--- Composable.System/GenericAbstractions/Time/ITimeSource.cs
+++ Composable.System/GenericAbstractions/Time/ITimeSource.cs
@@ -1,5 +1,8 @@
-﻿namespace Composable.GenericAbstractions.Time
+﻿using System;
+
+namespace Composable.GenericAbstractions.Time
 {
+    [Obsolete("Please switch to using IUtcTimeTimeSource")]
     ///<summary>Provides methods for determining the current time as local time or utc time.</summary>
     public interface ITimeSource : ILocalTimeTimeSource, IUtcTimeTimeSource
     {        

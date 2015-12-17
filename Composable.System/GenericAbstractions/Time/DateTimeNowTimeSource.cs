5978634e7e3b5aa2ec2a892d2129172469e61eb8
mlidbo <magnus.lidbom@gmail.com>
2015-12-17 04:12:23 +0100

    Get rid of the old time source interfaces.

--- Composable.System/GenericAbstractions/Time/DateTimeNowTimeSource.cs
+++ Composable.System/GenericAbstractions/Time/DateTimeNowTimeSource.cs
@@ -3,12 +3,9 @@
 namespace Composable.GenericAbstractions.Time
 {
     ///<summary>Simply returns DateTime.Now or DateTime.UtcNow</summary>
-    public class DateTimeNowTimeSource : ITimeSource
+    public class DateTimeNowTimeSource : IUtcTimeTimeSource
     {
         public static readonly DateTimeNowTimeSource Instance = new DateTimeNowTimeSource();      
-
-        ///<summary>Returns DateTime.Now</summary>
-        public DateTime LocalNow { get { return DateTime.Now; } }
 
         ///<summary>Returns DateTime.UtcNow</summary>
         public DateTime UtcNow { get { return DateTime.UtcNow; } }

1d9d0db41a828b08bbc8e39e82c80d2f9b3ba4ad
mlidbo <magnus.lidbom@gmail.com>
2015-12-18 19:19:43 +0100

    Pull in code from powermatch.

---
+++ Composable.CQRS/CQRS/EventSourcing/AggregateRoot.NestedEntity.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using Composable.CQRS.EventHandling;
+using Composable.GenericAbstractions.Time;
+
+namespace Composable.CQRS.EventSourcing
+{   
+    public abstract partial class AggregateRoot<TAggregateRoot, TAggregateRootBaseEventClass, TAggregateRootBaseEventInterface>
+    {
+        public abstract class NestedEntity<TComponent, TComponentBaseEventClass, TComponentBaseEventInterface, TComponentCreatedEventInterface>
+            where TComponentBaseEventInterface : TAggregateRootBaseEventInterface, IAggregateRootComponentEvent
+            where TComponentBaseEventClass : TAggregateRootBaseEventClass, TComponentBaseEventInterface
+            where TComponentCreatedEventInterface : TComponentBaseEventInterface, IAggregateRootComponentCreatedEvent
+            where TComponent : NestedEntity<TComponent, TComponentBaseEventClass, TComponentBaseEventInterface, TComponentCreatedEventInterface>
+        {
+            private readonly CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface> _eventAppliersEventDispatcher =
+                new CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface>();
+            private readonly CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface> _eventHandlersEventDispatcher =
+                new CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface>();
+
+            protected IUtcTimeTimeSource TimeSource => AggregateRoot.TimeSource;
+            protected TAggregateRoot AggregateRoot { get; private set; }
+
+            private void ApplyEvent(TComponentBaseEventInterface @event) { _eventAppliersEventDispatcher.Dispatch(@event); }
+
+            protected NestedEntity()
+            {
+                _eventHandlersEventDispatcher.Register()
+                                             .IgnoreUnhandled<TComponentBaseEventInterface>();
+            }
+
+            protected void RaiseEvent(TComponentBaseEventClass @event)
+            {
+                AggregateRoot.RaiseEvent(@event);
+                _eventHandlersEventDispatcher.Dispatch(@event);
+            }
+
+            protected CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface>.RegistrationBuilder RegisterEventAppliers()
+            {
+                return _eventAppliersEventDispatcher.RegisterHandlers();
+            }
+
+            protected CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface>.RegistrationBuilder RegisterEventHandlers()
+            {
+                return _eventHandlersEventDispatcher.RegisterHandlers();
+            }
+
+
+            public static Collection CreateSelfManagingCollection(TAggregateRoot aggregate) => new Collection(aggregate);
+
+            public class Collection : IReadOnlyAggregateRootComponentCollection<TComponent>
+            {
+                private readonly TAggregateRoot _aggregate;
+                public Collection(TAggregateRoot aggregate)
+                {
+                    _aggregate = aggregate;
+                    _aggregate.RegisterEventAppliers()
+                        .For<TComponentCreatedEventInterface>(
+                            e =>
+                            {
+                                var component = (TComponent)Activator.CreateInstance(typeof(TComponent), nonPublic: true);
+                                component.AggregateRoot = _aggregate;
+
+                                _components.Add(e.ComponentId, component);
+                                _componentsInCreationOrder.Add(component);
+                            })
+                        .For<TComponentBaseEventInterface>(e => _components[e.ComponentId].ApplyEvent(e));
+                }
+
+   
+
+                public TComponent Add<TCreationEvent>(TCreationEvent creationEvent) where TCreationEvent : TComponentBaseEventClass, TComponentCreatedEventInterface
+                {
+                    _aggregate.RaiseEvent(creationEvent);
+                    var result = _componentsInCreationOrder.Last();
+                    result._eventHandlersEventDispatcher.Dispatch(creationEvent);
+                    return result;
+                }
+
+                public IReadOnlyList<TComponent> InCreationOrder => _componentsInCreationOrder;
+
+                public bool TryGet(Guid id, out TComponent component) => _components.TryGetValue(id, out component);
+                [Pure]
+                public bool Exists(Guid id) => _components.ContainsKey(id);
+                public TComponent Get(Guid id) => _components[id];
+
+
+                private readonly Dictionary<Guid, TComponent> _components = new Dictionary<Guid, TComponent>();
+                private readonly List<TComponent> _componentsInCreationOrder = new List<TComponent>();
+            }
+        }
+
+    }
+}
---
+++ Composable.CQRS/CQRS/EventSourcing/SelfUpdatingSingleAggregateQueryModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Composable.CQRS.EventHandling;
+using Composable.System.Linq;
+
+namespace Composable.CQRS.EventSourcing
+{
+    public abstract class SelfUpdatingSingleAggregateQueryModel<TRootQueryModel, TAggregateRootBaseEventInterface>
+        where TRootQueryModel : SelfUpdatingSingleAggregateQueryModel<TRootQueryModel, TAggregateRootBaseEventInterface>
+    {
+        readonly CallMatchingHandlersInRegistrationOrderEventDispatcher<TAggregateRootBaseEventInterface> _eventAppliersEventDispatcher =
+            new CallMatchingHandlersInRegistrationOrderEventDispatcher<TAggregateRootBaseEventInterface>();
+
+
+        public Guid Id { get; private set; }
+
+        protected SelfUpdatingSingleAggregateQueryModel()
+        {
+            RegisterEventAppliers()
+                .ForGenericEvent<IAggregateRootCreatedEvent>(e => Id = e.AggregateRootId);
+        }
+
+        public void ApplyEvent(TAggregateRootBaseEventInterface @event) { _eventAppliersEventDispatcher.Dispatch(@event); }
+        public void ApplyEvents(IEnumerable<TAggregateRootBaseEventInterface> @event)
+        {
+            @event.ForEach(_eventAppliersEventDispatcher.Dispatch);
+        }
+
+        protected CallMatchingHandlersInRegistrationOrderEventDispatcher<TAggregateRootBaseEventInterface>.RegistrationBuilder RegisterEventAppliers()
+        {
+            return _eventAppliersEventDispatcher.RegisterHandlers();
+        }
+
+
+        public abstract class Component<TComponent, TComponentBaseEventInterface, TComponentCreatedEventInterface>
+            where TComponentBaseEventInterface : TAggregateRootBaseEventInterface, IAggregateRootComponentEvent
+            where TComponentCreatedEventInterface : TComponentBaseEventInterface, IAggregateRootComponentCreatedEvent
+            where TComponent : Component<TComponent, TComponentBaseEventInterface, TComponentCreatedEventInterface>
+        {
+            private readonly CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface> _eventAppliersEventDispatcher =
+                new CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface>();
+
+
+            protected TRootQueryModel RootQueryModel { get; private set; }
+
+            private void ApplyEvent(TComponentBaseEventInterface @event) { _eventAppliersEventDispatcher.Dispatch(@event); }
+
+            protected CallMatchingHandlersInRegistrationOrderEventDispatcher<TComponentBaseEventInterface>.RegistrationBuilder RegisterEventAppliers()
+            {
+                return _eventAppliersEventDispatcher.RegisterHandlers();
+            }
+
+            public static IQueryModelComponentCollection<TComponent> CreateSelfManagingCollection(TRootQueryModel rootQueryModel) => new Collection(rootQueryModel);
+
+            public class Collection : IQueryModelComponentCollection<TComponent>
+            {
+                private readonly TRootQueryModel _aggregate;
+                public Collection(TRootQueryModel aggregate)
+                {
+                    _aggregate = aggregate;
+                    _aggregate.RegisterEventAppliers()
+                         .For<TComponentCreatedEventInterface>(
+                            e =>
+                            {
+                                var component = (TComponent)Activator.CreateInstance(typeof(TComponent), nonPublic:true);
+                                component.RootQueryModel = _aggregate;
+
+                                _components.Add(e.ComponentId, component);
+                                _componentsInCreationOrder.Add(component);
+                            })
+                        .For<TComponentBaseEventInterface>(e => _components[e.ComponentId].ApplyEvent(e));
+                }
+
+
+                public IReadOnlyList<TComponent> InCreationOrder => _componentsInCreationOrder;
+
+                public bool TryGet(Guid id, out TComponent component) => _components.TryGetValue(id, out component);
+                public bool Exists(Guid id) => _components.ContainsKey(id);
+                public TComponent Get(Guid id) => _components[id];
+
+                private readonly Dictionary<Guid, TComponent> _components = new Dictionary<Guid, TComponent>();
+                private readonly List<TComponent> _componentsInCreationOrder = new List<TComponent>();
+            }
+        }
+    }
+}

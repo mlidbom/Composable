25cf1b1060f7baea4f3cb16568c171cc550eff05
mlidbo <magnus.lidbom@gmail.com>
2015-12-19 05:13:29 +0100

    Split classes for readability.

---
+++ Composable.CQRS/CQRS/EventSourcing/AggregateRoot.Component.NestedEntity.Removable.cs
@@ -0,0 +1,53 @@
+﻿namespace Composable.CQRS.EventSourcing
+{
+    public abstract partial class AggregateRoot<TAggregateRoot, TAggregateRootBaseEventClass, TAggregateRootBaseEventInterface>
+    {
+        public abstract partial class Component<TComponent, TComponentBaseEventClass, TComponentBaseEventInterface>
+            where TComponentBaseEventInterface : TAggregateRootBaseEventInterface
+            where TComponentBaseEventClass : TAggregateRootBaseEventClass, TComponentBaseEventInterface
+            where TComponent : Component<TComponent, TComponentBaseEventClass, TComponentBaseEventInterface>
+        {
+            public abstract class NestedEntity<TEntity,
+                                               TEntityId,
+                                               TEntityBaseEventClass,
+                                               TEntityBaseEventInterface,
+                                               TEntityCreatedEventInterface,
+                                               TEntityRemovedEventInterface,
+                                               TEventEntityIdSetterGetter> :
+                                                   NestedEntity
+                                                       <TEntity, TEntityId, TEntityBaseEventClass, TEntityBaseEventInterface, TEntityCreatedEventInterface,
+                                                       TEventEntityIdSetterGetter>
+                where TEntityBaseEventInterface : TComponentBaseEventInterface
+                where TEntityBaseEventClass : TComponentBaseEventClass, TEntityBaseEventInterface
+                where TEntityCreatedEventInterface : TEntityBaseEventInterface
+                where TEntityRemovedEventInterface : TEntityBaseEventInterface
+                where TEventEntityIdSetterGetter : IGetSetAggregateRootEntityEventEntityId<TEntityId, TEntityBaseEventClass, TEntityBaseEventInterface>, new()
+                where TEntity :
+                    NestedEntity<TEntity, TEntityId, TEntityBaseEventClass, TEntityBaseEventInterface, TEntityCreatedEventInterface, TEventEntityIdSetterGetter>
+            {
+                protected NestedEntity(TComponent component) : base(component) { }
+
+                public new static Collection CreateSelfManagingCollection(TComponent aggregate) => new Collection(aggregate);
+
+                public new class Collection : Component<TComponent, TComponentBaseEventClass, TComponentBaseEventInterface>
+                                                  .NestedEntity
+                                                  <TEntity, TEntityId, TEntityBaseEventClass, TEntityBaseEventInterface, TEntityCreatedEventInterface,
+                                                  TEventEntityIdSetterGetter>.Collection
+                {
+                    public Collection(TComponent component) : base(component)
+                    {
+                        component.RegisterEventAppliers()
+                                 .For<TEntityRemovedEventInterface>(
+                                     e =>
+                                     {
+                                         var id = IdGetterSetter.GetId(e);
+                                         var entity = this[id];
+                                         Entities.Remove(id);
+                                         EntitiesInCreationOrder.Remove(entity);
+                                     });
+                    }
+                }
+            }            
+        }
+    }
+}

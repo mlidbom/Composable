06152c40a09f57f55d9b505db5348949f2c29178
mlidbo <magnus.lidbom@gmail.com>
2015-12-20 15:18:06 +0100

    Use common assembly info for all Composable packages.

--- Composable.CQRS.ServiceBus.NServiceBus/Properties/AssemblyInfo.cs
+++ Composable.CQRS.ServiceBus.NServiceBus/Properties/AssemblyInfo.cs
@@ -2,5 +2,3 @@
 
 [assembly: AssemblyTitle("Composable.CQRS.ServiceBus.NServiceBus")]
 [assembly: AssemblyProduct("Composable.CQRS.ServiceBus.NServiceBus")]
-
-[assembly: AssemblyVersion("3.3.0.0")]

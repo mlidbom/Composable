be0bc3affa4a1c38daacab4e1a235157962f0f16
mlidbo <magnus.lidbom@gmail.com>
2015-12-21 02:59:26 +0100

    Grant a bit more time for test on teamcity.

--- Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/MigrationsPerformanceTest.SingleAggregateInstanceEventStreamMutatorPerformanceTest_Given_a_10000_events_large_aggregate.cs
+++ Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/MigrationsPerformanceTest.SingleAggregateInstanceEventStreamMutatorPerformanceTest_Given_a_10000_events_large_aggregate.cs
@@ -33,7 +33,7 @@ namespace CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations
         }
 
         [Test]
-        public void Aggregate_should_raise_100_000_events_in_less_than_150_milliseconds()
+        public void Aggregate_should_raise_100_000_events_in_less_than_180_milliseconds()
         {
             var history = Seq.OfTypes<Ec1>()
                                   .Concat(1.Through(10000).Select(_ => typeof(E1)))
@@ -49,7 +49,7 @@ namespace CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations
                              .ToEvents();
 
             TimeAsserter.Execute(
-                maxTotal: 150.Milliseconds().AdjustRuntimeForNCrunch(),
+                maxTotal: 180.Milliseconds().AdjustRuntimeForNCrunch(),
                 action: () => new TestAggregate2(history));
         }
 

b2636d6aa7118069ee26975245e2dc39b10a4733
mlidbo <magnus.lidbom@gmail.com>
2015-12-21 04:08:57 +0100

    Rename for greater clarity.

--- Composable.CQRS.Tests/CQRS/EventSourcing/AggregateRoot/NestedEntitiesTests/GuidId/Domain/Root.Component.NestedComponent.cs
+++ Composable.CQRS.Tests/CQRS/EventSourcing/AggregateRoot/NestedEntitiesTests/GuidId/Domain/Root.Component.NestedComponent.cs
@@ -4,7 +4,7 @@ namespace CQRS.Tests.CQRS.EventSourcing.AggregateRoot.NestedEntitiesTests.GuidId
 {
     public partial class Component
     {
-        public class NestedComponent : NestedComponent<NestedComponent, RootEvent.Component.NestedComponent.Implementation.Root, RootEvent.Component.NestedComponent.IRoot>
+        public class NestedComponent : Component.NestedComponent<NestedComponent, RootEvent.Component.NestedComponent.Implementation.Root, RootEvent.Component.NestedComponent.IRoot>
         {
             public string Name { get; private set; }
             public NestedComponent(Component parent) : base(parent)

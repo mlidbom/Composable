f040c6ce0a5f9c41c355cef1aafad7c584a49f47
mlidbo <magnus.lidbom@gmail.com>
2016-01-19 17:33:02 +0100

    Make sure that when replacing a documentdb or event store you also register them as IResetTestDatabases so that they will clean them selves up + Obsolete IResetTestDatabases and ResetTestDataBases()

--- Composable.Windsor/Testing/IResetTestDatabases.cs
+++ Composable.Windsor/Testing/IResetTestDatabases.cs
@@ -1,5 +1,8 @@
+using System;
+
 namespace Composable.Windsor.Testing
 {
+    [Obsolete("Relying on this method is fragile and apt to cause trouble. You should make sure to set up a new container for each test instead. That is far more safe and simple than using this")]
     public interface IResetTestDatabases
     {
         void ResetDatabase();

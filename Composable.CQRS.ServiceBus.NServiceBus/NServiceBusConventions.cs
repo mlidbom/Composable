52463e244717f99ac76d504b5933179010807679
mlidbo <magnus.lidbom@gmail.com>
2016-10-11 14:40:49 +0200

    Define conventions for events commands and messages in the endpoint configuration.

---
+++ Composable.CQRS.ServiceBus.NServiceBus/NServiceBusConventions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Composable.System.Reflection;
+using NServiceBus;
+
+namespace Composable.CQRS.ServiceBus.NServiceBus
+{
+    public static class NServiceBusConventions
+    {
+        public static bool IsMessageType(this Type type)
+        {
+            return type.Implements<global::NServiceBus.IMessage>()
+                   || type.Name.EndsWith("Message")
+                   || (type.Namespace != null && type.Namespace.EndsWith("Messages"));
+        }
+
+        public static bool IsCommandType(this Type type)
+        {
+            return type.Implements<global::NServiceBus.ICommand>()
+                   || type.Implements<Composable.CQRS.Command.ICommand>()
+                   || type.Name.EndsWith("Command")
+                   || (type.Namespace != null && type.Namespace.EndsWith("Commands"));
+        }
+
+        public static bool IsEventType(this Type type)
+        {
+            return type.Implements<global::NServiceBus.IEvent>()
+                   || type.Implements<Composable.DomainEvents.IDomainEvent>()
+                   || type.Name.EndsWith("Event")
+                   || (type.Namespace != null && type.Namespace.EndsWith("Events"));
+        }
+
+        public static Configure SetupConventions(Configure config)
+        {
+            return config.DefiningEventsAs(IsEventType)
+                         .DefiningCommandsAs(IsCommandType)
+                         .DefiningCommandsAs(IsMessageType);
+        }
+    }
+}

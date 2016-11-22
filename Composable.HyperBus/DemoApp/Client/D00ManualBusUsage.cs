e4da79aa5847bf2d647ce8da0186253eafdd4748
mlidbom <magnus.lidbom@gmail.com>
2016-11-23 00:20:36 +0100

    minor readability improvement.

--- Composable.HyperBus/DemoApp/Client/D00ManualBusUsage.cs
+++ Composable.HyperBus/DemoApp/Client/D00ManualBusUsage.cs
@@ -15,7 +15,8 @@ namespace Composable.HyperBus.DemoApp.Client
             var startPage = await startPageSomething.GetReturnValue();
             var accountsSomething = await Bus.GetAsync(startPage.Links.Accounts);
             var accounts = await accountsSomething.GetReturnValue();
-            var account = await Bus.ExecuteAsync(accounts.Commands.Register(email: "someone@somewhere.com", password: "secret"));
+            var registerAccountCommand = accounts.Commands.Register(email: "someone@somewhere.com", password: "secret");
+            var account = await Bus.ExecuteAsync(registerAccountCommand);
         }
     }
 }

3a8c6a25d7cdb474ce36d741ac98073484934b63
mlidbom <magnus.lidbom@gmail.com>
2016-11-23 00:40:13 +0100

    Get rid of some warnings.

--- Composable.HyperBus/APIDraft/IMessageHandlers.cs
+++ Composable.HyperBus/APIDraft/IMessageHandlers.cs
@@ -1,20 +1,26 @@
-﻿namespace Composable.HyperBus.APIDraft
+﻿using JetBrains.Annotations;
+
+namespace Composable.HyperBus.APIDraft
 {
+    [UsedImplicitly]
     public interface ICommandHandler<in TCommand, out TReturnValue> where TCommand : ICommand<TReturnValue>
     {
         TReturnValue Handle(TCommand command);
     }
 
+    [UsedImplicitly]
     public interface ICommandHandler<in TCommand> where TCommand : ICommand
     {
         void Handle(TCommand command);
     }
 
+    [UsedImplicitly]
     public interface IQueryHandler<in TQuery, out TReturnValue> where TQuery : IQuery<TReturnValue>
     {
         TReturnValue Handle(TQuery command);
     }
 
+    [UsedImplicitly]
     public interface IEventHandler<in TEvent> where TEvent : IEvent
     {
         void Handle(TEvent @event);

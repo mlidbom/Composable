dc58542b5c4e33ebc79558af342108b45621b53e
mlidbom <magnus.lidbom@gmail.com>
2016-12-17 16:52:31 +0100

    Improve build and publish script.

--- Documentation/Website/buildAndPublish.ps1
+++ Documentation/Website/buildAndPublish.ps1
@@ -2,11 +2,14 @@ $ghPagesCheckoutFolder = '../../../Composable-gh-pages'
 
 if(!(Test-Path $ghPagesCheckoutFolder))
 {
+    Push-Location '../../../'
     Write-Host "Missing gh-pages checkout"
+    git clone --single-branch --branch gh-pages git@github.com:mlidbom/Composable.git Composable-gh-pages
+    Pop-Location
     return
 }
 
-$targetFolder = (Get-Item ).FullName
+$targetFolder = (Get-Item $ghPagesCheckoutFolder).FullName
 Push-Location $targetFolder
 git checkout -f 
 git clean -fd

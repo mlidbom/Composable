05d8a40498a4cf5f763d800265111494f207f499
mlidbo <magnus.lidbom@gmail.com>
2017-01-29 14:37:58 +0100

    Write failing test for sub-par performance.

--- Composable.CQRS.Tests/SqlServerDatabasePoolTests/PerformanceTests.cs
+++ Composable.CQRS.Tests/SqlServerDatabasePoolTests/PerformanceTests.cs
@@ -20,7 +20,7 @@ namespace CQRS.Tests.SqlServerDatabasePoolTests
         [Test]
         public void Single_thread_can_reserve_and_release_100_identically_named_databases_in_5_seconds()
         {
-            var dbName = "EB82270F-E0BA-49F7-BC09-79AE95BA109F";
+            var dbName = "74EA37DF-03CE-49C4-BDEC-EAD40FAFB3A1";
 
             TimeAsserter.Execute(
                 action:
@@ -54,6 +54,22 @@ namespace CQRS.Tests.SqlServerDatabasePoolTests
                 timeIndividualExecutions: true,
                 maxTotal: 2.Seconds(),
                 maxTries:3);
+        }
+
+        [Test]
+        public void Repeated_fetching_of_same_connection_runs_100_times_in_one_second()
+        {
+            var dbName = "4669B59A-E0AC-4E76-891C-7A2369AE0F2F";
+            using (var manager = new SqlServerDatabasePool(_masterConnectionString))
+            {
+                var connection1 = manager.ConnectionStringFor(dbName);
+
+                TimeAsserter.Execute(
+                    action: () => manager.ConnectionStringFor(dbName),
+                    iterations: 100,
+                    maxTotal: 1.Seconds()
+                    );
+            }
         }
     }
 }

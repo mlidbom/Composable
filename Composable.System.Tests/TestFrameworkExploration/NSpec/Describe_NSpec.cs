43db550916aa12aeb263b9b959a338fd5ab67959
mlidbo <magnus.lidbom@gmail.com>
2017-01-30 11:44:03 +0100

    Improve naming for readability.

--- Composable.System.Tests/TestFrameworkExploration/NSpec/Describe_NSpec.cs
+++ Composable.System.Tests/TestFrameworkExploration/NSpec/Describe_NSpec.cs
@@ -12,55 +12,55 @@ namespace Composable.Tests.TestFrameworkExploration.NSpec
 {
     [UsedImplicitly] public class Describe_NSpec : nspec
     {
-        readonly CallTracker Calls = new CallTracker();
-        public void before_all() => Calls.CurrentIs(null)
-                                         .Push(Class_context.before_all);
+        readonly CallTracker Current = new CallTracker();
+        public void before_all() => Current.Is(null)
+                                           .Push(Class_context.before_all);
 
-        public void before_each() => Calls.CurrentIs("")
-                                          .Push(Class_context.before_each);
+        public void before_each() => Current.Is("")
+                                            .Push(Class_context.before_each);
 
-        public void after_each() => Calls.CurrentIs("")
-                                         .Push(Class_context.after_each);
+        public void after_each() => Current.Is("")
+                                           .Push(Class_context.after_each);
 
         public void outer_context()
         {
-            beforeAll = () => Calls.CurrentIs(Class_context.before_all)
-                                   .Push(Outer_context.beforeAll);
+            beforeAll = () => Current.Is(Class_context.before_all)
+                                     .Push(Outer_context.beforeAll);
 
-            before = () => Calls.CurrentIs(Class_context.before_each)
-                                .Push(Outer_context.before);
+            before = () => Current.Is(Class_context.before_each)
+                                  .Push(Outer_context.before);
 
-            after = () => Calls.CurrentIs("")
-                               .Push(Outer_context.after);
+            after = () => Current.Is("")
+                                 .Push(Outer_context.after);
 
-            afterAll = () => Calls.CurrentIs(Inner_context.afterAll)
-                                  .Push(Outer_context.afterAll);
+            afterAll = () => Current.Is(Inner_context.afterAll)
+                                    .Push(Outer_context.afterAll);
 
-            it[Outer_context.It1] = () => Calls.CurrentIs(Outer_context.before)
-                                               .Push(Outer_context.It1);
+            it[Outer_context.It1] = () => Current.Is(Outer_context.before)
+                                                 .Push(Outer_context.It1);
 
-            it[Outer_context.It2] = () => Calls.CurrentIs(Outer_context.before)
-                                               .Push(Outer_context.It2);
+            it[Outer_context.It2] = () => Current.Is(Outer_context.before)
+                                                 .Push(Outer_context.It2);
 
             context[Inner_context.Name] = () =>
                                           {
-                                              beforeAll = () => Calls.CurrentIs("")
-                                                                     .Push(Inner_context.beforeAll);
+                                              beforeAll = () => Current.Is("")
+                                                                       .Push(Inner_context.beforeAll);
 
-                                              before = () => Calls.CurrentIs(Outer_context.before)
-                                                                  .Push(Inner_context.before);
+                                              before = () => Current.Is(Outer_context.before)
+                                                                    .Push(Inner_context.before);
 
-                                              after = () => Calls.CurrentIs(Inner_context.It1, Inner_context.It2)
-                                                                 .Push(Inner_context.after);
+                                              after = () => Current.Is(Inner_context.It1, Inner_context.It2)
+                                                                   .Push(Inner_context.after);
 
-                                              afterAll = () => Calls.CurrentIs("")
-                                                                    .Push(Inner_context.afterAll);
+                                              afterAll = () => Current.Is("")
+                                                                      .Push(Inner_context.afterAll);
 
-                                              it[Inner_context.It1] = () => Calls.CurrentIs(Inner_context.before)
-                                                                                 .Push(Inner_context.It1);
+                                              it[Inner_context.It1] = () => Current.Is(Inner_context.before)
+                                                                                   .Push(Inner_context.It1);
 
-                                              it[Inner_context.It1] = () => Calls.CurrentIs(Inner_context.before)
-                                                                                 .Push(Inner_context.It2);
+                                              it[Inner_context.It1] = () => Current.Is(Inner_context.before)
+                                                                                   .Push(Inner_context.It2);
                                           };
         }
 
@@ -68,7 +68,7 @@ namespace Composable.Tests.TestFrameworkExploration.NSpec
         {
             Stack<string> calls;
 
-            public CallTracker CurrentIs(params string[] current)
+            public CallTracker Is(params string[] current)
             {
                 if(current == null)
                 {

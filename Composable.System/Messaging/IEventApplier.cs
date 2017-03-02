2ade4799d7328af9ba5a6f388cf3470a13ded858
Magnus Lidbom <magnus.lidbom@raysearchlabs.com>
2017-03-02 10:38:45 +0100

    Doc comment update.

--- Composable.System/Messaging/IEventApplier.cs
+++ Composable.System/Messaging/IEventApplier.cs
@@ -6,6 +6,10 @@ using Composable.CQRS.EventSourcing;
 
 namespace Composable.Messaging
 {
+    ///<summary>
+    /// <para>An <see cref="IEventSubscriber{TEvent}" /> that promises not to change any domain state. </para>
+    /// <para></para>
+    /// </summary>
     public interface IEventApplier<in TEvent> : IEventSubscriber<TEvent> where TEvent:IEvent
     {
     }
--- Composable.System/Messaging/IEventHandler.cs
+++ Composable.System/Messaging/IEventHandler.cs
@@ -1,6 +1,10 @@
 ﻿namespace Composable.Messaging
 {
-  using Composable.CQRS.EventSourcing;
-    public interface IEventHandler<in TEvent> : IEventSubscriber<TEvent> where TEvent : IEvent
-    {}
+    using Composable.CQRS.EventSourcing;
+
+    ///<summary>An <see cref="IEventSubscriber{TEvent}"/> for the purpose of executing logic in some domain.
+    /// <para>Should only be called when events are published. Not when they are replayed.</para>
+    /// </summary>
+    public interface IEventHandler<in TEvent> : IEventSubscriber<TEvent>
+        where TEvent : IEvent {}
 }

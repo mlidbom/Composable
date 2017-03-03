4222865836f19cee711be77a3919abd96ce57461
mlidbom <magnus.lidbom@gmail.com>
2017-03-03 08:29:32 +0100

    Start adding design for managing scopes for lifestyles for message handler. Got stuck on the realization that I likely must have some sort of access to a container here in order to manage it. And that refactoring the code & test likewise seems to become an unreasonable burden without being able to extract wiring concerns out of the tests. So time to start work on some sort of abstraction for usage of containers....

--- Composable.System/Messaging/Buses/_Composable.Messaging.Buses.Interfaces.cs
+++ Composable.System/Messaging/Buses/_Composable.Messaging.Buses.Interfaces.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using Composable.Messaging.Events;
+using Composable.System.Transactions;
 
 namespace Composable.Messaging.Buses
 {
+    ///<summary>Dispatches messages within a process.</summary>
     public interface IInProcessServiceBus
     {
         void Publish(IEvent anEvent);
@@ -12,10 +14,15 @@ namespace Composable.Messaging.Buses
         bool Handles(object aMessage);
     }
 
-    public interface IServiceBus : IInProcessServiceBus
+    ///<summary>Dispatches messages between processes.</summary>
+    public interface IInterProcessServiceBus
     {
         void SendAtTime(DateTime sendAt, ICommand message);
     }
+
+    public interface IServiceBus : IInProcessServiceBus, IInterProcessServiceBus
+    {        
+    }    
 
     public interface IMessageSpy
     {
@@ -38,5 +45,18 @@ namespace Composable.Messaging.Buses
         IMessageHandlerRegistrar ForEvent<TEvent>(Action<TEvent> handler) where TEvent : IEvent;
         IMessageHandlerRegistrar ForCommand<TCommand>(Action<TCommand> handler) where TCommand : ICommand;
         IMessageHandlerRegistrar ForQuery<TQuery, TResult>(Func<TQuery, TResult> handler) where TQuery : IQuery<TResult> where TResult : IQueryResult;
-    }    
+    }
+
+
+    public interface IMessageHandlerScope : IDisposable
+    {
+        
+    }
+
+    public interface IMessageHandlerScopeManager
+    {
+        ITransactionalScope GetOrCreateQueryHandlerScope();
+        ITransactionalScope GetOrCreateCommandHandlerScope();
+        ITransactionalScope GetOrCreateEventSubscriberScope();
+    }
 }

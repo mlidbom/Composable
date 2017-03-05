9daee4741dff7731c428c8039ce286fe68eb0bb6
mlidbom <magnus.lidbom@gmail.com>
2017-03-05 16:43:10 +0100

    Play a little with request/reply

---
+++ NetMqProcess1/__Mine/_001_request_reply_api_exploration.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NetMQ;
+using NetMQ.Sockets;
+using NUnit.Framework;
+
+namespace NetMqProcess01.__Mine
+{
+    [TestFixture]
+    public class _001_request_reply_api_exploration
+    {
+        static readonly string ResponseSocketAddress = "inproc://request-socket";
+        ResponseSocket _responseSocket;
+        RequestSocket _requestSocket;
+
+        [SetUp] public void SetupTask() {
+
+            _responseSocket = new ResponseSocket(ResponseSocketAddress);
+            _requestSocket = new RequestSocket(ResponseSocketAddress);
+        }
+
+        [TearDown] public void TearDownTask() {
+            _requestSocket.Dispose();
+            _requestSocket.Dispose();
+        }
+
+        [Test]
+        public void Basic_send_from_request_and_receive_at_reply()
+        {
+            _requestSocket.SendFrame("TEST", more: false);
+
+            var request = _responseSocket.ReceiveFrameString();
+            request.Should()
+                   .Be("TEST");
+        }
+
+        [Test] public void Sending_twice_from_request_throws_anException() {
+            _requestSocket.SendFrame("1");
+            _requestSocket.Invoking( socket => socket.SendFrame("2"))
+                .ShouldThrow<NetMQException>()
+                .And.
+                ErrorCode.Should().Be(ErrorCode.FiniteStateMachine);
+        }
+    }
+}
--- NetMqProcess1/__Mine/_01_ReqRouterDealerRep.cs
+++ NetMqProcess1/__Mine/_01_ReqRouterDealerRep.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Composable.System.Linq;
 using NetMQ;
+using NetMQ.Monitoring;
 using NetMQ.Sockets;
 
 namespace NetMqProcess01.__Mine
@@ -12,47 +13,71 @@ namespace NetMqProcess01.__Mine
         const bool UseInProcess = true;
         static string RouterSocket = UseInProcess ? "inproc://router-socket" : "tcp://127.0.0.1:5559";
         static string DealerSocket = UseInProcess ? "inproc://dealer-socket" : "tcp://127.0.0.1:5560";
-        static readonly int NumberOfServers = 10;
-        static readonly int NumberOfClient = 100;
+        static readonly int NumberOfServers = 100;
+        static readonly int NumberOfClient = 10;
 
         public static void Run()
         {
-            StartBroker();
+            using(var serverPoller = new NetMQPoller())
+            {
+                StartBroker();
 
-            Thread.Sleep(1000);
-            StartServers();
-            StartClients();
-            Console.ReadLine();
+                Thread.Sleep(1000);
+                StartServers(serverPoller);
+                StartClients();
+
+                serverPoller.RunAsync();
+                Console.ReadLine();
+            }
         }
 
-        static void StartServers() =>
+        static void StartServers(NetMQPoller serverPoller)
+        {
+            var threadServerSocket = new ThreadLocal<ResponseSocket>(trackAllValues:true);
             1.Through(NumberOfServers)
              .ForEach(serverId =>
                       {
-                          new Thread(() =>
+                          Task.Run(() =>
                                    {
-                                       using(var responseSocket = new ResponseSocket())
-                                       {
-                                           responseSocket.Connect(DealerSocket);
-                                           //Console.WriteLine($"Server: {serverId} started");
-                                           while(true)
+                                       ResponseSocket responseSocket = null;
+
+                                       EventHandler<NetMQSocketEventArgs> handleRequest = 
+                                       (sender, socketEventArgs) =>
                                            {
-                                               var request = responseSocket.ReceiveMultipartMessage();
-                                               var clientId = request.First.ConvertToInt32();
+                                               NetMQMessage request = null;
+                                               if(responseSocket.TryReceiveMultipartMessage(ref request))
+                                               {
+                                                   var clientId = request.First.ConvertToInt32();
 
-                                               // Console.WriteLine($"Server {serverId} got request from client: {clientId}");
-                                               var response = new NetMQMessage(new[]
-                                                                               {
-                                                                                   new NetMQFrame($"Server:{serverId}, Client:{clientId}")
-                                                                               }
-                                                                              );
+                                                   //Console.WriteLine($"Server {serverId} got request from client: {clientId}");
+                                                   var response = new NetMQMessage(new[]
+                                                                                   {
+                                                                                       new NetMQFrame($"Server:{serverId}, Client:{clientId}")
+                                                                                   }
+                                                                                  );
 
-                                               // Console.WriteLine($"Server {serverId} responding to client: {clientId}");
-                                               responseSocket.SendMultipartMessage(response);
-                                           }
+                                                   //Console.WriteLine($"Server {serverId} responding to client: {clientId}");
+                                                   responseSocket.SendMultipartMessage(response);
+                                               }
+                                           };
+
+                                       if(threadServerSocket.IsValueCreated)
+                                       {
+                                           responseSocket = threadServerSocket.Value;
+                                       } else
+                                       {
+                                           threadServerSocket.Value = responseSocket = new ResponseSocket();
+                                           responseSocket.Connect(DealerSocket);
+                                           responseSocket.ReceiveReady += handleRequest;
+                                           serverPoller.Add(responseSocket);
+
+                                           Console.WriteLine($"Added thread lockal socket nr: {threadServerSocket.Values.Count}");
                                        }
-                                   }).Start();
+
+                                       Console.WriteLine($"Server: {serverId} started");
+                                   });
                       });
+        }
 
         static long TotalRequests = 0;
 
@@ -72,50 +97,52 @@ namespace NetMqProcess01.__Mine
             1.Through(NumberOfClient)
              .ForEach(clientId =>
                       {
-                          new Thread(() =>
-                                   {
-                                       using(var requestSocket = new RequestSocket())
-                                       {
-                                           requestSocket.Connect(RouterSocket);
-                                           //Console.WriteLine($"Client: {clientId} started");
-                                           long requests = 0;
-                                           while(true)
-                                               try
-                                               {
-                                                   requests++;
+                          Task.Run(() =>
+                                     {
+                                         using(var requestSocket = new RequestSocket())
+                                         {
+                                             requestSocket.Connect(RouterSocket);
+                                             Console.WriteLine($"Client: {clientId} started");
+                                             long requests = 0;
+                                             while(true)
+                                                 try
+                                                 {
+                                                     requests++;
 
-                                                   var request = new NetMQMessage();
-                                                   request.Append(clientId);
+                                                     var request = new NetMQMessage();
+                                                     request.Append(clientId);
 
-                                                   // Console.WriteLine($"Client:{clientId} sending: {request.First.ConvertToString()}");
-                                                   requestSocket.SendMultipartMessage(request);
-                                                   var response = requestSocket.ReceiveMultipartMessage()
-                                                                               .First.ConvertToString();
+                                                     //Console.WriteLine($"Client:{clientId} sending: {clientId}");
+                                                     requestSocket.SendMultipartMessage(request);
+                                                     var response = requestSocket.ReceiveMultipartMessage()
+                                                                                 .First.ConvertToString();
 
-                                                   if(!response.Contains($"Client:{clientId}"))
-                                                   {
-                                                       throw new Exception($"Client:{clientId} got response indended for another client: {response}");
-                                                   }
+                                                     //Console.WriteLine($"Client:{clientId} got: {response}");
 
-                                                   //if(requests % 1000 == 0)
-                                                   //{
-                                                   //    Console.WriteLine($"Client:{clientId} has made {requests} requests.");
-                                                   //}
+                                                     if (!response.Contains($"Client:{clientId}"))
+                                                     {
+                                                         throw new Exception($"Client:{clientId} got response indended for another client: {response}");
+                                                     }
 
-                                                   var currentTotalRequests = Interlocked.Increment(ref TotalRequests);
-                                                   if(currentTotalRequests % 10000 == 0)
-                                                   {
-                                                       Console.WriteLine($"Total requests: {currentTotalRequests:N}");
-                                                   }
+                                                     //if(requests % 1000 == 0)
+                                                     //{
+                                                     //    Console.WriteLine($"Client:{clientId} has made {requests} requests.");
+                                                     //}
 
-                                                   // Console.WriteLine($"Client:{clientId} got: {response}");
-                                               }
-                                               catch(Exception e)
-                                               {
-                                                   Console.WriteLine(e);
-                                               }
-                                       }
-                                   }).Start();
+                                                     var currentTotalRequests = Interlocked.Increment(ref TotalRequests);
+                                                     if(currentTotalRequests % 10000 == 0)
+                                                     {
+                                                         Console.WriteLine($"Total requests: {currentTotalRequests:N}");
+                                                     }
+
+
+                                                 }
+                                                 catch(Exception e)
+                                                 {
+                                                     Console.WriteLine(e);
+                                                 }
+                                         }
+                                     });
                       });
     }
 }

375520b8dd499808923e09ec5adcf5a0e4b1c838
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-03-21 11:40:47 +0100

    Extract test attributes to a Composable.Testing project.

---
+++ Composable.Testing/TestCategories.cs
@@ -0,0 +1,15 @@
+﻿namespace Composable.Testing
+{
+    class PerformanceAttribute : NCrunch.Framework.ExclusivelyUsesAttribute,
+                                 NUnit.Framework.Interfaces.IApplyToTest,
+                                 NUnit.Framework.Interfaces.IApplyToContext
+    {
+        public PerformanceAttribute() : base("Performance") {}
+
+        public void ApplyToTest(NUnit.Framework.Internal.Test test)
+            => test.Properties.Add("Category", "Performance");
+
+        public void ApplyToContext(NUnit.Framework.Internal.TestExecutionContext context)
+            => context.ParallelScope = NUnit.Framework.ParallelScope.None;
+    }
+}

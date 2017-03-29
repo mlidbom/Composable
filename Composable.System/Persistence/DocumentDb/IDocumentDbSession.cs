2651eac7993b1405d4d94c8183ff00ddb0e6194f
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-03-29 23:01:43 +0200

    Fix warning.

--- Composable.System/Persistence/DocumentDb/IDocumentDbSession.cs
+++ Composable.System/Persistence/DocumentDb/IDocumentDbSession.cs
@@ -1,6 +1,6 @@
 ﻿namespace Composable.Persistence.DocumentDb
 {
     //todo: break up and probably remove this monolithic interface.
-    internal interface IDocumentDbSession : IDocumentDbBulkReader, IDocumentDbUpdater
+    interface IDocumentDbSession : IDocumentDbBulkReader, IDocumentDbUpdater
     {}
 }

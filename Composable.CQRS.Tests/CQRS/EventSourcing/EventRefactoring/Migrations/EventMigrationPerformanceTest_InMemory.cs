0999c2980f881baa52ceb593128b0e651525fce7
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-04-18 02:51:06 +0200

    Add missing testfixture attribtutes.

--- Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/EventMigrationPerformanceTest_InMemory.cs
+++ Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/EventMigrationPerformanceTest_InMemory.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 
 namespace Composable.CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations
 {
-    [Ignore("todo: Funnily the in-memory event store is to slow for the cached tests :)")]
+    [TestFixture, Ignore("todo: Funnily the in-memory event store is to slow for the cached tests :)")]
     public class EventMigrationPerformanceTest_InMemory : EventMigrationPerformanceTest
     {
         public EventMigrationPerformanceTest_InMemory() : base(typeof(InMemoryEventStore)) {}
--- Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/EventMigrationPerformanceTest_SqlServer.cs
+++ Composable.CQRS.Tests/CQRS/EventSourcing/EventRefactoring/Migrations/EventMigrationPerformanceTest_SqlServer.cs
@@ -1,8 +1,11 @@
 using Composable.Persistence.EventStore;
+using NUnit.Framework;
 
-namespace Composable.CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations {
+namespace Composable.CQRS.Tests.CQRS.EventSourcing.EventRefactoring.Migrations
+{
+    [TestFixture]
     public class EventMigrationPerformanceTest_SqlServer : EventMigrationPerformanceTest
     {
-        public EventMigrationPerformanceTest_SqlServer() : base(typeof(EventStore)) { }
+        public EventMigrationPerformanceTest_SqlServer() : base(typeof(EventStore)) {}
     }
 }

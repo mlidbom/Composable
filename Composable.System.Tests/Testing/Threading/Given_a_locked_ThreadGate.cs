f325a832d7c8fc586be87988d8884cc4aa2da765
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-08-27 13:16:10 +0200

    Checkpoint.

---
+++ Composable.System.Tests/Testing/Threading/Given_a_locked_ThreadGate.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using System.Threading;
+using Composable.Testing.Threading;
+using FluentAssertions;
+
+namespace Composable.Tests.Testing.Threading
+{
+    [TestFixture()]
+    public class Given_a_locked_ThreadGate
+    {
+        AutoResetEvent _autoResetEventBeforeGate;
+        [SetUp] public void SetupTask()
+        {
+            _autoResetEventBeforeGate = new AutoResetEvent(initialState:false);
+        }
+
+        [Test] public void Code_after_the_gate_is_not_executed_until_the_gate_is_opened()
+        {
+            Assert.Inconclusive();
+        }
+    }
+
+
+    [TestFixture] public class Given_a_TestingAutoResetEvent
+    {
+        TestingAutoResetEvent _sut = new TestingAutoResetEvent("MyTestEvent", startLocked: true, timeout: 5.Seconds());
+
+        [SetUp] public void That_is_locked_()
+        {
+            _sut.Set();
+        }
+
+        [Test] public void Expectation()
+        {
+            
+            Assert.Inconclusive();
+        }
+
+        [TestFixture] public class Scenario
+        {
+            [Test] public void Expectation()
+            {
+                
+                Assert.Inconclusive();
+            }
+        }
+    }
+}
---
+++ Composable.System/Testing/Threading/SynchronizationHelpers.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+
+namespace Composable.Testing.Threading
+{
+
+
+    ///<summary>A block of code with <see cref="ThreadGate"/>s for <see cref="EntryGate"/> and <see cref="ExitGate"/>. Useful for controlling multithreaded code for testing purposes.</summary>
+    class GatedCodeBlock
+    {
+        public ThreadGate EntryGate { get; }
+        public ThreadGate ExitGate { get; }
+
+
+        readonly Action _codeBlock;
+
+        GatedCodeBlock(Action codeBlock, ThreadGate entryGate, ThreadGate exitGate)
+        {
+            EntryGate = entryGate;
+            ExitGate = exitGate;
+            _codeBlock = codeBlock;
+        }
+    }
+
+
+
+    enum ThreadGateOptions
+    {
+        LockOnNextPass,
+        LockOnEveryPass
+    }
+
+    ///<summary>Blocks threads calling <see cref="BlockUntilOpen"/> until the the gate is opened.</summary>
+    class ThreadGate
+    {
+        ///<summary>Opens the gate</summary>
+        public void Open() {}
+        public void Close() {}
+        public void BlockUntilOpen(TimeSpan timeout) {}
+        public int Passed { get; }
+        public string Name { get; }
+        public ThreadGateOptions Options { get; }
+
+        ThreadGate(ThreadGateOptions options)
+        {
+            Options = options;
+        }
+    }
+
+    class TestingManualResetEvent
+    {
+        readonly ManualResetEventSlim _event = new ManualResetEventSlim(false);
+        readonly TimeSpan _timeout;
+        readonly string _name;
+
+        public TestingManualResetEvent(string name, TimeSpan? timeout = null)
+        {
+            _timeout = timeout ?? TimeSpan.FromSeconds(1);
+            _name = name;
+        }
+
+        public void Wait()
+        {
+            if(!_event.Wait(_timeout))
+            {
+                throw new Exception($"Timed out waiting for lock: {_name}");
+            }
+        }
+
+        public void Set() => _event.Set();
+        public void Reset() => _event.Reset();
+    }
+
+    class TestingAutoResetEvent
+    {
+        readonly AutoResetEvent _event;
+        readonly TimeSpan _timeout;
+        readonly string _name;
+
+        public TestingAutoResetEvent(string name, bool startLocked, TimeSpan? timeout = null)
+        {
+            _timeout = timeout ?? TimeSpan.FromSeconds(1);
+            _event = new AutoResetEvent(!startLocked);
+            _name = name;
+        }
+
+        public void Wait()
+        {
+            if (!_event.WaitOne(_timeout))
+            {
+                throw new Exception($"Timed out waiting for lock: {_name}");
+            }
+        }
+
+        public void Set() => _event.Set();
+        public void Reset() => _event.Reset();
+    }
+}

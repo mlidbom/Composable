a3a2a5fb3bfce0f170bd3e78179041184f48a28e
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-08-29 13:58:48 +0200

    Move threading code between files for easier reading.

--- Composable.System/Testing/Threading/GatedCodeSection.cs
+++ Composable.System/Testing/Threading/GatedCodeSection.cs
@@ -1,68 +1,9 @@
 ﻿using System;
-using Composable.Contracts;
 using Composable.System;
-using Composable.System.Threading;
 using Composable.System.Threading.ResourceAccess;
 
 namespace Composable.Testing.Threading
 {
-    interface IGatedCodeSection
-    {
-        IThreadGate EntranceGate { get; }
-        IThreadGate ExitGate { get; }
-        IGatedCodeSection WithExclusiveLock(Action action);
-        IDisposable Enter();
-    }
-
-    static class GatedCodeSectionExtensions
-    {
-        public static IGatedCodeSection Open(this IGatedCodeSection @this)
-            => @this.WithExclusiveLock(
-                () =>
-                {
-                    @this.EntranceGate.Open();
-                    @this.ExitGate.Open();
-                });
-
-        public static IGatedCodeSection LetOneThreadEnter(this IGatedCodeSection @this)
-            => @this.WithExclusiveLock(
-                () =>
-                {
-                    @this.AssertIsEmpty();
-                    @this.EntranceGate.LetOneThreadPass();
-                });
-
-        public static IGatedCodeSection LetOneThreadEnterAndReachExit(this IGatedCodeSection @this)
-            => @this.WithExclusiveLock(
-                () =>
-                {
-                    @this.LetOneThreadEnter();
-                    @this.ExitGate.AwaitQueueLength(1);
-                });
-
-        public static IGatedCodeSection LetOneThreadPass(this IGatedCodeSection @this)
-            => @this.WithExclusiveLock(
-                () =>
-                {
-                    @this.LetOneThreadEnterAndReachExit();
-                    @this.ExitGate.LetOneThreadPass();
-                });
-
-        public static bool IsEmpty(this IGatedCodeSection @this)
-            => @this.WithExclusiveLock(() => @this.EntranceGate.Passed == @this.ExitGate.Passed);
-
-        public static IGatedCodeSection AssertIsEmpty(this IGatedCodeSection @this)
-            => @this.WithExclusiveLock(() => Contract.Assert.That(@this.IsEmpty(), "Code section should be empty"));
-
-        public static TResult WithExclusiveLock<TResult>(this IGatedCodeSection @this, Func<TResult> function)
-        {
-            var result = default(TResult);
-            @this.WithExclusiveLock(() => result = function());
-            return result;
-        }
-    }
-
-    ///<summary>A block of code with <see cref="ThreadGate"/>s for <see cref="EntranceGate"/> and <see cref="ExitGate"/>. Useful for controlling multithreaded code for testing purposes.</summary>
     class GatedCodeSection : IGatedCodeSection
     {
         readonly IExclusiveResourceLockManager _lock;
@@ -82,7 +23,7 @@ namespace Composable.Testing.Threading
         {
             using(_lock.AwaitExclusiveLock())
             {
-                //The reason for taking the lock is to inspect/modify both gates. So Take the locks right away and ensure consistency throughout the action
+                //The reason for taking the lock is to inspect/modify both gates. So take the locks right away and ensure consistency throughout the action
                 EntranceGate.WithExclusiveLock(() => ExitGate.WithExclusiveLock(action));
             }
             return this;
--- Composable.System/Testing/Threading/ThreadGate.cs
+++ Composable.System/Testing/Threading/ThreadGate.cs
@@ -8,36 +8,7 @@ using Composable.System.Threading.ResourceAccess;
 
 namespace Composable.Testing.Threading
 {
-    interface IThreadGateVisitor
-    {
-        void Pass();
-        void Pass(TimeSpan timeout);
-    }
 
-    interface IThreadGate : IThreadGateVisitor
-    {
-        ///<summary>Opens the gate and lets all threads through.</summary>
-        IThreadGate Open();
-
-        ///<summary>Lets a single thread pass.</summary>
-        IThreadGate LetOneThreadPass();
-
-        ///<summary>Blocks all threads from passing.</summary>
-        IThreadGate Close();
-
-        ///<summary>Blocks until the gate is in a state which satisfies <see cref="condition"/> and then while owning the lock executes <see cref="action"/></summary>
-        IThreadGate ExecuteLockedOnce(TimeSpan timeout, Predicate<IThreadGate> condition, Action<IThreadGate, IExclusiveResourceLock> action);
-
-        bool IsOpen { get; }
-        long Queued { get; }
-        long Requested { get; }
-        long Passed { get; }
-        TimeSpan DefaultTimeout { get; }
-
-        IReadOnlyList<Thread> RequestedThreads { get; }
-        IReadOnlyList<Thread> QueuedThreads { get; }
-        IReadOnlyList<Thread> PassedThreads { get; }
-    }
 
     static class ThreadGateExtensions
     {

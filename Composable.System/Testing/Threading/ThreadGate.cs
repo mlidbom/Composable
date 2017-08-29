1dc4bb2917ea3e0f8874e8f59bef86e07ec5addd
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-08-29 14:03:40 +0200

    Moving code around.

--- Composable.System/Testing/Threading/ThreadGate.cs
+++ Composable.System/Testing/Threading/ThreadGate.cs
@@ -7,16 +7,6 @@ using Composable.System.Threading.ResourceAccess;
 
 namespace Composable.Testing.Threading
 {
-    static class ThreadGateExtensions
-    {
-        public static IThreadGate Await(this IThreadGate @this, Predicate<IThreadGate> condition) => @this.Await(@this.DefaultTimeout, condition);
-        public static IThreadGate Await(this IThreadGate @this, TimeSpan timeout, Predicate<IThreadGate> condition) => @this.ExecuteLockedOnce(timeout, condition, (gate, owner) => {});
-        public static IThreadGate AwaitClosed(this IThreadGate @this) => @this.Await(_ => !@this.IsOpen);
-        public static IThreadGate AwaitQueueLength(this IThreadGate @this, int length) => @this.Await(me => me.Queued == length);
-        public static IThreadGate AwaitEmptyQueue(this IThreadGate @this) => @this.Await(me => me.Queued == 0);
-        public static IThreadGate WithExclusiveLock(this IThreadGate @this, Action action) => @this.ExecuteLockedOnce(@this.DefaultTimeout, _ => true, (gate, owner) => action());
-    }
-
     class ThreadGate : IThreadGate
     {
         public static IThreadGate WithTimeout(TimeSpan timeout) => new ThreadGate(timeout);

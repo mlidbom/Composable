e501bf2c221c6a5d79429f5dd612dc21c01779b6
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-08-29 08:13:20 +0200

    Flesh out GatedCodeSection a bit.

--- Composable.System/Testing/Threading/GatedCodeSection.cs
+++ Composable.System/Testing/Threading/GatedCodeSection.cs
@@ -1,19 +1,56 @@
 ﻿using System;
+using Composable.System;
 
 namespace Composable.Testing.Threading
 {
-    ///<summary>A block of code with <see cref="ThreadGate"/>s for <see cref="EntranceGate"/> and <see cref="ExitGate"/>. Useful for controlling multithreaded code for testing purposes.</summary>
-    class GatedCodeSection
+    interface IGatedCodeSection
     {
-        public ThreadGate EntranceGate { get; }
-        public ThreadGate ExitGate { get; }
+        IThreadGate EntranceGate { get; }
+        IThreadGate ExitGate { get; }
+        IDisposable Enter();
+    }
 
-        readonly Action _codeBlock;
-
-        GatedCodeSection(ThreadGate entranceGate, ThreadGate exitGate)
+    static class GatedCodeSectionExtensions
+    {
+        public static IGatedCodeSection LetOneThreadEnter(this IGatedCodeSection @this)
         {
-            EntranceGate = entranceGate;
-            ExitGate = exitGate;
+            @this.EntranceGate.LetOneThreadPass();
+            return @this;
+        }
+
+        public static IGatedCodeSection Open(this IGatedCodeSection @this)
+        {
+            @this.EntranceGate.Open();
+            @this.ExitGate.Open();
+            return @this;
+        }
+
+        public static IGatedCodeSection LetOneThreadPass(this IGatedCodeSection @this)
+        {
+            @this.EntranceGate.LetOneThreadPass();
+            @this.ExitGate.LetOneThreadPass();
+            return @this;
+        }
+    }
+
+    ///<summary>A block of code with <see cref="ThreadGate"/>s for <see cref="EntranceGate"/> and <see cref="ExitGate"/>. Useful for controlling multithreaded code for testing purposes.</summary>
+    class GatedCodeSection : IGatedCodeSection
+    {
+        public IThreadGate EntranceGate { get; }
+        public IThreadGate ExitGate { get; }
+
+        public static IGatedCodeSection WithTimeout(TimeSpan timeout) => new GatedCodeSection(timeout);
+
+        GatedCodeSection(TimeSpan timeout)
+        {
+            EntranceGate = ThreadGate.WithTimeout(timeout);
+            ExitGate = ThreadGate.WithTimeout(timeout);
+        }
+
+        public IDisposable Enter()
+        {
+            EntranceGate.Pass();
+            return Disposable.Create(() => ExitGate.Pass());
         }
     }
 }
--- Composable.System/Testing/Threading/ThreadGate.cs
+++ Composable.System/Testing/Threading/ThreadGate.cs
@@ -43,6 +43,7 @@ namespace Composable.Testing.Threading
         public static IThreadGate Await(this IThreadGate @this, Predicate<IThreadGate> condition) => @this.Await(@this.DefaultTimeout, condition);
         public static IThreadGate Await(this IThreadGate @this, TimeSpan timeout, Predicate<IThreadGate> condition) => @this.ExecuteLockedOnce(timeout, condition, (gate, owner) => {});
         public static IThreadGate AwaitClosed(this IThreadGate @this) => @this.Await(_ => !@this.IsOpen);
+        public static IThreadGate AwaitQueueLength(this IThreadGate @this, int length) => @this.Await(me => me.Queued == length);
         public static IThreadGate AwaitEmptyQueue(this IThreadGate @this) => @this.Await(me => me.Queued == 0);
     }
 

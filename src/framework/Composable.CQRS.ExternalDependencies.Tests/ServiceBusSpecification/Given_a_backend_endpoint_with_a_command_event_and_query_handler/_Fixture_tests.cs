005c26e5b1b00527f400bccd0dbac0a3f41d0898
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-10-01 12:46:53 +0200

    reformat.

--- src/framework/Composable.CQRS.ExternalDependencies.Tests/ServiceBusSpecification/Given_a_backend_endpoint_with_a_command_event_and_query_handler/_Fixture_tests.cs
+++ src/framework/Composable.CQRS.ExternalDependencies.Tests/ServiceBusSpecification/Given_a_backend_endpoint_with_a_command_event_and_query_handler/_Fixture_tests.cs
@@ -9,61 +9,59 @@ namespace Composable.CQRS.Tests.ServiceBusSpecification.Given_a_backend_endpoint
 {
     public class Failure_tests : Fixture
     {
-        IntentionalException thrownException = new IntentionalException();
-
         [Fact] public void If_command_handler_throws_disposing_host_throws_AggregateException_containing_a_single_exception_that_is_the_thrown_exception()
         {
-            CommandHandlerThreadGate.ThrowOnPassThrough(thrownException);
+            CommandHandlerThreadGate.ThrowOnPassThrough(_thrownException);
             Host.ClientBus.Send(new MyCommand());
             AssertThrowsAggregateExceptionWithSingleInnerExceptionThatIsThrownException(Host.Dispose);
         }
 
         [Fact] public async Task If_command_handler_with_result_throws_disposing_host_throws_AggregateException_containing_a_single_exception_that_is_the_thrown_exception()
         {
-            CommandHandlerWithResultThreadGate.ThrowOnPassThrough(thrownException);
+            CommandHandlerWithResultThreadGate.ThrowOnPassThrough(_thrownException);
             var exceptionResult = Host.ClientBus.SendAsync(new MyCommandWithResult());
 
             AssertThrowsAggregateExceptionWithSingleInnerExceptionThatIsThrownException(Host.Dispose);
 
-            (await Assert.ThrowsAsync<IntentionalException>(async () => await exceptionResult)).Should().Be(thrownException);
+            (await Assert.ThrowsAsync<IntentionalException>(async () => await exceptionResult)).Should().Be(_thrownException);
         }
 
         [Fact] public void If_event_handler_throws_disposing_host_throws_AggregateException_containing_a_single_exception_that_is_the_thrown_exception()
         {
-            EventHandlerThreadGate.ThrowOnPassThrough(thrownException);
+            EventHandlerThreadGate.ThrowOnPassThrough(_thrownException);
             Host.ClientBus.Publish(new MyEvent());
             AssertThrowsAggregateExceptionWithSingleInnerExceptionThatIsThrownException(TestDispose);
         }
 
         [Fact] public async Task If_query_handler_throws_disposing_host_throws_AggregateException_containing_a_single_exception_that_is_the_thrown_exception()
         {
-            QueryHandlerThreadGate.ThrowOnPassThrough(thrownException);
+            QueryHandlerThreadGate.ThrowOnPassThrough(_thrownException);
             var queryResult = Host.ClientBus.QueryAsync(new MyQuery());
 
             AssertThrowsAggregateExceptionWithSingleInnerExceptionThatIsThrownException(TestDispose);
 
-            (await Assert.ThrowsAsync<IntentionalException>(async () => await queryResult)).Should().Be(thrownException);
+            (await Assert.ThrowsAsync<IntentionalException>(async () => await queryResult)).Should().Be(_thrownException);
         }
 
         [Fact] public void If_query_handler_throws_Query_and_disposing_host_throws_AggregateException_containing_a_single_exception_that_is_the_thrown_exception()
         {
-            QueryHandlerThreadGate.ThrowOnPassThrough(thrownException);
+            QueryHandlerThreadGate.ThrowOnPassThrough(_thrownException);
 
             AssertThrowsAggregateExceptionWithSingleInnerExceptionThatIsThrownException(
                 () => Host.ClientBus.Query(new MyQuery()),
                 TestDispose);
         }
 
-
         void AssertThrowsAggregateExceptionWithSingleInnerExceptionThatIsThrownException(params Action[] actions)
         {
             foreach(var action in actions)
             {
                 Assert.Throws<AggregateException>(action)
-                      .InnerExceptions.Single().Should().Be(thrownException);
+                      .InnerExceptions.Single().Should().Be(_thrownException);
             }
         }
 
+        readonly IntentionalException _thrownException = new IntentionalException();
         class IntentionalException : Exception {}
     }
 }

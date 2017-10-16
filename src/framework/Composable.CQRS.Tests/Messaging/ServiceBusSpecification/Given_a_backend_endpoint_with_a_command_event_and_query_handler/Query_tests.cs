74ef98425a7a8b50c5408bc2fd53a377b73f4d64
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-10-16 09:51:20 +0200

    Write failing test for reusing queries.

---
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Given_a_backend_endpoint_with_a_command_event_and_query_handler/Query_tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Composable.Testing.Threading;
+using Xunit;
+
+namespace Composable.Tests.Messaging.ServiceBusSpecification.Given_a_backend_endpoint_with_a_command_event_and_query_handler
+{
+    public class Query_tests : Fixture
+    {
+        [Fact] public void The_same_query_can_be_reused_in_parallel_without_issues()
+        {
+            var test = new MyQuery();
+
+            QueryHandlerThreadGate.Close();
+
+            var result1 = Host.ClientBus.QueryAsync(test);
+            var result2 = Host.ClientBus.QueryAsync(test);
+
+            QueryHandlerThreadGate.AwaitQueueLengthEqualTo(2);
+            QueryHandlerThreadGate.Open();
+
+            Task.WaitAll(result1, result2);
+        }
+    }
+}

a334ad3ed45030c4d43f6cc83bbf4629b65b7923
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-12-16 07:23:14 +0100

    Spelling fix and a little bit of reformatting.

--- src/Samples/AccountManagement/AccountManagement.Domain/Account.cs
+++ src/Samples/AccountManagement/AccountManagement.Domain/Account.cs
@@ -15,7 +15,7 @@ namespace AccountManagement.Domain
 
         //No public constructors please. Aggregates are created through domain verbs.
         //Expose named factory methods that ensure the instance is valid instead. See register method below.
-        Account():base(new DateTimeNowTimeSource())
+        Account() : base(new DateTimeNowTimeSource())
         {
             //Maintain correct state as events are raised or read from the store.
             //Use property updated events whenever possible. Changes to public state should be represented by property updated events.
@@ -25,24 +25,16 @@ namespace AccountManagement.Domain
         }
 
         //Ensure that the state of the instance is sane. If not throw an exception.
-        //Called after every call to RaiseEvent.
-        protected override void AssertInvariantsAreMet()
-        {
-            OldContract.Invariant(() => Email, () => Password, () => Id).NotNullOrDefault();
-        }
+        //Called after every call to Publish.
+        protected override void AssertInvariantsAreMet() => OldContract.Invariant(() => Email, () => Password, () => Id).NotNullOrDefault();
 
         /// <summary><para>Used when a user manually creates an account themselves.</para>
         /// <para>Note how this design with a named static creation method: </para>
-        /// <para> * makes it clearear what the caller intends.</para>
+        /// <para> * makes it clearer what the caller intends.</para>
         /// <para> * makes it impossible to use the class incorrectly, such as forgetting to check for duplicates or save the new instance in the repository.</para>
         /// <para> * reduces code duplication since multiple callers are not burdened with saving the instance, checking for duplicates etc.</para>
         /// </summary>
-        public static Account Register(
-            Email email,
-            Password password,
-            Guid accountId,
-            IAccountRepository repository,
-            IDuplicateAccountChecker duplicateAccountChecker)
+        public static Account Register(Email email, Password password, Guid accountId, IAccountRepository repository, IDuplicateAccountChecker duplicateAccountChecker)
         {
             //Ensure that it is impossible to call with invalid arguments.
             //Since all domain types should ensure that it is impossible to create a non-default value that is invalid we only have to disallow default values.

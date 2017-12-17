7ba56cc44c14ecfee2efde56c05178aec8d1d413
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-12-17 20:48:22 +0100

    Cleanup bootstrapper a bit more.

--- src/Samples/AccountManagement/AccountManagement.Server/AccountManagementServerDomainBootstrapper.cs
+++ src/Samples/AccountManagement/AccountManagement.Server/AccountManagementServerDomainBootstrapper.cs
@@ -13,42 +13,26 @@ namespace AccountManagement
 {
     public static class AccountManagementServerDomainBootstrapper
     {
-        internal const string ConnectionStringName = "AccountManagement";
+        const string ConnectionStringName = "AccountManagement";
 
         public static IEndpoint RegisterWith(IEndpointHost host)
         {
             return host.RegisterAndStartEndpoint("UserManagement.Domain",
                                                  builder =>
                                                  {
-                                                     SetupContainer(builder.Container);
+                                                     RegisterDomainComponents(builder.Container);
+                                                     RegisterUserInterfaceComponents(builder.Container);
+
                                                      RegisterHandlers(builder.RegisterHandlers);
                                                  });
         }
-
-        static void SetupContainer(IDependencyInjectionContainer container)
-        {
-            RegisterDomainComponents(container);
-
-            container.RegisterSqlServerDocumentDb<IAccountManagementUiDocumentDbUpdater, IAccountManagementUiDocumentDbReader, IAccountManagementUiDocumentDbBulkReader>(ConnectionStringName);
-
-            container.Register(
-                Component.For<AccountManagementQueryModelReader>()
-                         .UsingFactoryMethod((IAccountManagementUiDocumentDbReader documentDbQueryModels, AccountQueryModel.Generator accountQueryModelGenerator, ISingleContextUseGuard usageGuard) =>
-                                                 new AccountManagementQueryModelReader(documentDbQueryModels, accountQueryModelGenerator, usageGuard))
-                         .LifestyleScoped()
-            );
-
-            container.Register(Component.For<AccountQueryModel.Generator>()
-                                        .UsingFactoryMethod((IAccountManagementEventStoreReader session) => new AccountQueryModel.Generator(session))
-                                        .LifestyleScoped());
-        }
-
 
         static void RegisterDomainComponents(IDependencyInjectionContainer container)
         {
             container.RegisterSqlServerEventStore<IAccountManagementEventStoreUpdater, IAccountManagementEventStoreReader>(ConnectionStringName);
 
-            container.RegisterSqlServerDocumentDb<IAccountManagementDomainDocumentDbUpdater, IAccountManagementDomainDocumentDbReader, IAccountManagementDomainDocumentDbBulkReader>(ConnectionStringName);
+            container.RegisterSqlServerDocumentDb<IAccountManagementDomainDocumentDbUpdater, IAccountManagementDomainDocumentDbReader, IAccountManagementDomainDocumentDbBulkReader>(
+                ConnectionStringName);
 
             container.Register(
                 Component.For<IAccountRepository>()
@@ -56,8 +40,22 @@ namespace AccountManagement
                          .LifestyleScoped(),
                 Component.For<IDuplicateAccountChecker>()
                          .UsingFactoryMethod((IAccountManagementDomainDocumentDbReader queryModels) => new DuplicateAccountChecker(queryModels))
-                         .LifestyleScoped()
-            );
+                         .LifestyleScoped());
+        }
+
+        static void RegisterUserInterfaceComponents(IDependencyInjectionContainer container)
+        {
+            container.RegisterSqlServerDocumentDb<IAccountManagementUiDocumentDbUpdater, IAccountManagementUiDocumentDbReader, IAccountManagementUiDocumentDbBulkReader>(ConnectionStringName);
+
+            container.Register(
+                Component.For<AccountManagementQueryModelReader>()
+                         .UsingFactoryMethod((IAccountManagementUiDocumentDbReader documentDbQueryModels, AccountQueryModel.Generator accountQueryModelGenerator, ISingleContextUseGuard usageGuard) =>
+                                                 new AccountManagementQueryModelReader(documentDbQueryModels, accountQueryModelGenerator, usageGuard))
+                         .LifestyleScoped());
+
+            container.Register(Component.For<AccountQueryModel.Generator>()
+                                        .UsingFactoryMethod((IAccountManagementEventStoreReader session) => new AccountQueryModel.Generator(session))
+                                        .LifestyleScoped());
         }
 
         static void RegisterHandlers(MessageHandlerRegistrarWithDependencyInjectionSupport registrar)

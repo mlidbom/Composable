474aafad0ae2f4e388ef67602bbe284203824c6e
Magnus Lidbom <magnus.lidbom@gmail.com>
2017-12-19 13:58:07 +0100

    Use NUnit for class where there was no need for XUnit.

--- src/Samples/AccountManagement/AccountManagement.Tests/Domain/PasswordSpecification.cs
+++ src/Samples/AccountManagement/AccountManagement.Tests/Domain/PasswordSpecification.cs
@@ -1,50 +1,48 @@
 ﻿using AccountManagement.Domain;
 using FluentAssertions;
-using Xunit;
-// ReSharper disable InconsistentNaming
-// ReSharper disable ClassNeverInstantiated.Global
+using NUnit.Framework;
 
 namespace AccountManagement.Tests.Domain
 {
-    public class PasswordSpecification
+    [TestFixture] public class PasswordSpecification
     {
-        public class When_creating_a_password
+        [TestFixture] public class When_creating_a_password
         {
-            public class From_the_string_Pass
+            [TestFixture] public class From_the_string_Pass
             {
                 readonly Password _password;
                 public From_the_string_Pass() => _password = new Password("Pass");
 
-                [Fact] void HashedPassword_is_not_null() => _password.HashedPassword.Should().NotBeNull();
-                [Fact] void HashedPassword_is_not_an_empty_array() => _password.HashedPassword.Should().NotBeEmpty();
-                [Fact] void Salt_is_not_null() => _password.Salt.Should().NotBeNull();
+                [Test] public void HashedPassword_is_not_null() => _password.HashedPassword.Should().NotBeNull();
+                [Test] public void HashedPassword_is_not_an_empty_array() => _password.HashedPassword.Should().NotBeEmpty();
+                [Test] public void Salt_is_not_null() => _password.Salt.Should().NotBeNull();
 
-                public class IsCorrectPassword_is_ : From_the_string_Pass
+                [TestFixture] public class IsCorrectPassword_is_ : From_the_string_Pass
                 {
-                    [Fact] void true_if_string_is_Pass() => _password.IsCorrectPassword("Pass").Should().BeTrue();
+                    [Test] public void true_if_string_is_Pass() => _password.IsCorrectPassword("Pass").Should().BeTrue();
 
                     public class false_if : IsCorrectPassword_is_
                     {
-                        [Fact] void _case_changes()
+                        [Test] public void _case_changes()
                         {
                             _password.IsCorrectPassword("pass").Should().BeFalse();
                             _password.IsCorrectPassword("PasS").Should().BeFalse();
                         }
 
-                        [Fact] void space_is_prepended() => _password.IsCorrectPassword(" Pass").Should().BeFalse();
-                        [Fact] void space_is_appended() => _password.IsCorrectPassword("Pass ").Should().BeFalse();
+                        [Test] public void space_is_prepended() => _password.IsCorrectPassword(" Pass").Should().BeFalse();
+                        [Test] public void space_is_appended() => _password.IsCorrectPassword("Pass ").Should().BeFalse();
                     }
                 }
             }
 
-            public class a_PasswordDoesNotMatchPolicyException_with_matching_failure_is_thrown_when_password_
+            [TestFixture] public class a_PasswordDoesNotMatchPolicyException_with_matching_failure_is_thrown_when_password_
             {
-                [Fact] void is_null() => AssertCreatingPasswordThrowsExceptionContainingFailure(null, Password.Policy.Failures.Null);
-                [Fact] void is_shorter_than_four_characters() => AssertCreatingPasswordThrowsExceptionContainingFailure("abc", Password.Policy.Failures.ShorterThanFourCharacters);
-                [Fact] void starts_with_whitespace() => AssertCreatingPasswordThrowsExceptionContainingFailure(" Pass", Password.Policy.Failures.BorderedByWhitespace);
-                [Fact] void ends_with_whitespace() => AssertCreatingPasswordThrowsExceptionContainingFailure("Pass ", Password.Policy.Failures.BorderedByWhitespace);
-                [Fact] void contains_only_lowercase_characters() => AssertCreatingPasswordThrowsExceptionContainingFailure("pass", Password.Policy.Failures.MissingUppercaseCharacter);
-                [Fact] void contains_only_uppercase_characters() => AssertCreatingPasswordThrowsExceptionContainingFailure("PASS", Password.Policy.Failures.MissingLowerCaseCharacter);
+                [Test] public void is_null() => AssertCreatingPasswordThrowsExceptionContainingFailure(null, Password.Policy.Failures.Null);
+                [Test] public void is_shorter_than_four_characters() => AssertCreatingPasswordThrowsExceptionContainingFailure("abc", Password.Policy.Failures.ShorterThanFourCharacters);
+                [Test] public void starts_with_whitespace() => AssertCreatingPasswordThrowsExceptionContainingFailure(" Pass", Password.Policy.Failures.BorderedByWhitespace);
+                [Test] public void ends_with_whitespace() => AssertCreatingPasswordThrowsExceptionContainingFailure("Pass ", Password.Policy.Failures.BorderedByWhitespace);
+                [Test] public void contains_only_lowercase_characters() => AssertCreatingPasswordThrowsExceptionContainingFailure("pass", Password.Policy.Failures.MissingUppercaseCharacter);
+                [Test] public void contains_only_uppercase_characters() => AssertCreatingPasswordThrowsExceptionContainingFailure("PASS", Password.Policy.Failures.MissingLowerCaseCharacter);
 
                 static void AssertCreatingPasswordThrowsExceptionContainingFailure(string password, Password.Policy.Failures expectedFailure)
                     => Assert.Throws<PasswordDoesNotMatchPolicyException>(() => new Password(password)).Failures.Should().Contain(expectedFailure);

a8c7f76b5b5dfd9c182aa39846db0263aeb4e7d5
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-02 19:07:53 +0100

    Skip being asynchronous for now so that the tests pass.

--- src/framework/Composable.CQRS/Messaging/Buses/Implementation/InterprocessTransport.MessageStorage.cs
+++ src/framework/Composable.CQRS/Messaging/Buses/Implementation/InterprocessTransport.MessageStorage.cs
@@ -18,19 +18,19 @@ namespace Composable.Messaging.Buses.Implementation
             {
                 try
                 {
-                    await _connectionFactory.UseCommandAsync(
-                        async command =>
-                            await command
-                                  .SetCommandText(
-                                      $@"
+                    _connectionFactory.UseCommand(
+                        command =>
+                            command
+                                .SetCommandText(
+                                    $@"
 INSERT {MessageDispatching.TableName} 
             ({MessageDispatching.MessageId},  {MessageDispatching.EndpointId},  {MessageDispatching.IsReceived}) 
     VALUES (@{MessageDispatching.MessageId}, @{MessageDispatching.EndpointId}, @{MessageDispatching.IsReceived})
 ")
-                                  .AddParameter(MessageDispatching.MessageId, outGoingMessage.MessageId)
-                                  .AddParameter(MessageDispatching.EndpointId, endpointId.GuidValue)
-                                  .AddParameter(MessageDispatching.IsReceived, 0)
-                                  .ExecuteNonQueryAsync());
+                                .AddParameter(MessageDispatching.MessageId, outGoingMessage.MessageId)
+                                .AddParameter(MessageDispatching.EndpointId, endpointId.GuidValue)
+                                .AddParameter(MessageDispatching.IsReceived, 0)
+                                .ExecuteNonQuery());
                 }
                 catch(Exception)
                 {
@@ -43,22 +43,22 @@ INSERT {MessageDispatching.TableName}
             {
                 try
                 {
-                    await _connectionFactory.UseCommandAsync(
-                        async command =>
+                    _connectionFactory.UseCommand(
+                        command =>
                         {
-                            var affectedRows = await command
-                                         .SetCommandText(
-                                             $@"
+                            var affectedRows = command
+                                               .SetCommandText(
+                                                   $@"
 UPDATE {MessageDispatching.TableName} 
     SET {MessageDispatching.IsReceived} = 1
 WHERE {MessageDispatching.MessageId} = @{MessageDispatching.MessageId}
     AND {MessageDispatching.EndpointId} = @{MessageDispatching.EndpointId}
     AND {MessageDispatching.IsReceived} = 0
 ")
-                                         .AddParameter(MessageDispatching.MessageId, response.RespondingToMessageId)
-                                         .AddParameter(MessageDispatching.EndpointId, endpointId.GuidValue)
-                                         .AddParameter(MessageDispatching.IsReceived, 1)
-                                         .ExecuteNonQueryAsync();
+                                               .AddParameter(MessageDispatching.MessageId, response.RespondingToMessageId)
+                                               .AddParameter(MessageDispatching.EndpointId, endpointId.GuidValue)
+                                               .AddParameter(MessageDispatching.IsReceived, 1)
+                                               .ExecuteNonQuery();
 
                             Contract.Result.Assert(affectedRows == 1);
                             return affectedRows;
@@ -75,19 +75,19 @@ WHERE {MessageDispatching.MessageId} = @{MessageDispatching.MessageId}
             {
                 try
                 {
-                    await _connectionFactory.UseCommandAsync(
-                        async command =>
-                            await command
-                                  .SetCommandText(
-                                      $@"
+                    _connectionFactory.UseCommand(
+                        command =>
+                            command
+                                .SetCommandText(
+                                    $@"
 INSERT {OutboxMessages.TableName} 
             ({OutboxMessages.MessageId},  {OutboxMessages.TypeId},  {OutboxMessages.Body}) 
     VALUES (@{OutboxMessages.MessageId}, @{OutboxMessages.TypeId}, @{OutboxMessages.Body})
 ")
-                                  .AddParameter(OutboxMessages.MessageId, message.MessageId)
-                                  .AddParameter(OutboxMessages.TypeId, TypeId.FromType(message.GetType()).GuidValue)
-                                  .AddNVarcharMaxParameter(OutboxMessages.Body, JsonConvert.SerializeObject(message))
-                                  .ExecuteNonQueryAsync());
+                                .AddParameter(OutboxMessages.MessageId, message.MessageId)
+                                .AddParameter(OutboxMessages.TypeId, TypeId.FromType(message.GetType()).GuidValue)
+                                .AddNVarcharMaxParameter(OutboxMessages.Body, JsonConvert.SerializeObject(message))
+                                .ExecuteNonQuery());
                 }
                 catch(Exception)
                 {

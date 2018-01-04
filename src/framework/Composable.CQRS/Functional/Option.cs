dd56439ab4c1ba69b28669596356ba5768891b5a
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-05 00:56:53 +0100

    Small additions to option class.

--- src/framework/Composable.CQRS/Functional/Option.cs
+++ src/framework/Composable.CQRS/Functional/Option.cs
@@ -5,11 +5,14 @@ namespace Composable.Functional
 {
     public static class Option
     {
+        public static Option<T> NoneIfNull<T>(T value) where T: class => value == null ? None<T>() : Some(value);
+        public static Option<T> NoneIfDefault<T>(T value) where T: struct => Equals(value, default(T)) ? None<T>() : Some(value);
+
         public static Option<T> Some<T>(T value) => new Some<T>(value);
         public static  Option<T> None<T>() => Functional.None<T>.Instance;
     }
 
-    // ReSharper disable once UnusedTypeParameter. It is used to maintain type safety.
+    // ReSharper disable once UnusedTypeParameter It is used to maintain type safety in pattern matching. We need to know what the type of the Some value vill be even if the value is not included in this class.
     public abstract class Option<T>
     {
         [Obsolete("For the love of sanity do NOT add another inheritor to this class. It will break the whole abstraction!")]

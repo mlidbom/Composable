971df3011c35675d0c10a29b7b7f0797e2f36517
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-21 15:05:29 +0100

    Adjust timings.

--- src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
@@ -8,24 +8,24 @@ namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
 {
     [TestFixture] public class Local_Query_performance_tests : PerformanceTestBase
     {
-        [Test] public void Given_30_client_threads_Runs_1000_local_queries_in_5_milliSecond()
+        [Test] public void Given_30_client_threads_Runs_100_local_queries_in_1_milliSecond()
         {
             var navigationSpecification = NavigationSpecification.Get(new MyQuery());
 
             //Warmup
             StopwatchExtensions.TimeExecutionThreaded(action: () => ServerBus.Execute(navigationSpecification), iterations: 10, maxDegreeOfParallelism: 30);
 
-            TimeAsserter.ExecuteThreaded(action: () => ServerBus.Execute(navigationSpecification), iterations: 1000, maxTotal: 5.Milliseconds(), maxDegreeOfParallelism: 30);
+            TimeAsserter.ExecuteThreaded(action: () => ServerBus.Execute(navigationSpecification), iterations: 100, maxTotal: 1.Milliseconds(), maxDegreeOfParallelism: 30);
         }
 
-        [Test] public void Given_1_client_thread_Runs_1000_local_queries_in_5_milliseconds()
+        [Test] public void Given_1_client_thread_Runs_100_local_queries_in_1_milliseconds()
         {
             var navigationSpecification = NavigationSpecification.Get(new MyQuery());
 
             //Warmup
             StopwatchExtensions.TimeExecutionThreaded(action: () => ServerBus.Execute(navigationSpecification), iterations: 10, maxDegreeOfParallelism: 30);
 
-            TimeAsserter.Execute(action: () => ServerBus.Execute(navigationSpecification), iterations: 1000, maxTotal: 5.Milliseconds());
+            TimeAsserter.Execute(action: () => ServerBus.Execute(navigationSpecification), iterations: 100, maxTotal: 1.Milliseconds());
         }
     }
 }

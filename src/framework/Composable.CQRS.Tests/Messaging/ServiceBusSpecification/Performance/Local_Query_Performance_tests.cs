96a2b47f8f89618f03b58fc85fca9ca3f1262f41
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-21 15:01:54 +0100

    Improve performance tests a bit.

---
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
@@ -0,0 +1,31 @@
+﻿using Composable.Messaging;
+using Composable.System.Diagnostics;
+using Composable.Testing.Performance;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
+{
+    [TestFixture] public class Local_Query_performance_tests : PerformanceTestBase
+    {
+        [Test] public void Given_30_client_threads_Runs_1000_local_queries_in_5_milliSecond()
+        {
+            var navigationSpecification = NavigationSpecification.Get(new MyQuery());
+
+            //Warmup
+            StopwatchExtensions.TimeExecutionThreaded(action: () => ServerBus.Execute(navigationSpecification), iterations: 10, maxDegreeOfParallelism: 30);
+
+            TimeAsserter.ExecuteThreaded(action: () => ServerBus.Execute(navigationSpecification), iterations: 1000, maxTotal: 5.Milliseconds(), maxDegreeOfParallelism: 30);
+        }
+
+        [Test] public void Given_1_client_thread_Runs_1000_local_queries_in_5_milliseconds()
+        {
+            var navigationSpecification = NavigationSpecification.Get(new MyQuery());
+
+            //Warmup
+            StopwatchExtensions.TimeExecutionThreaded(action: () => ServerBus.Execute(navigationSpecification), iterations: 10, maxDegreeOfParallelism: 30);
+
+            TimeAsserter.Execute(action: () => ServerBus.Execute(navigationSpecification), iterations: 1000, maxTotal: 5.Milliseconds());
+        }
+    }
+}
---
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Remote_Query_Performance_tests.cs
@@ -0,0 +1,31 @@
+﻿using Composable.Messaging;
+using Composable.System.Diagnostics;
+using Composable.Testing.Performance;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
+{
+    [TestFixture] public class RemoteQueryPerformanceTests : PerformanceTestBase
+    {
+        [Test] public void Given_30_client_threads_Runs_100_remote_queries_in_30_milliSecond()
+        {
+            var navigationSpecification = NavigationSpecification.GetRemote(new MyQuery());
+
+            //Warmup
+            StopwatchExtensions.TimeExecutionThreaded(action: () => ClientBus.Execute(navigationSpecification), iterations: 10, maxDegreeOfParallelism: 30);
+
+            TimeAsserter.ExecuteThreaded(action: () => ClientBus.Execute(navigationSpecification), iterations: 100, maxTotal: 30.Milliseconds(), maxDegreeOfParallelism: 30);
+        }
+
+        [Test] public void Given_1_client_thread_Runs_100_remote_queries_in_100_milliseconds()
+        {
+            var navigationSpecification = NavigationSpecification.GetRemote(new MyQuery());
+
+            //Warmup
+            StopwatchExtensions.TimeExecutionThreaded(action: () => ClientBus.Execute(navigationSpecification), iterations: 10, maxDegreeOfParallelism: 30);
+
+            TimeAsserter.Execute(action: () => ClientBus.Execute(navigationSpecification), iterations: 100, maxTotal: 100.Milliseconds());
+        }
+    }
+}

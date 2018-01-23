dc151303f7caaa8ab13473194c73ca3550246306
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-23 17:32:16 +0100

    Move MessageInspector to where it belongs.

---
+++ src/framework/Composable.CQRS/Messaging/MessageInspector.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
+using Composable.System.Reflection;
+
+namespace Composable.Messaging {
+    static class MessageInspector
+    {
+        static readonly HashSet<Type> SuccessfullyInspectedTypes = new HashSet<Type>();
+
+        internal static void AssertValid(IReadOnlyList<Type> eventTypesToInspect)
+        {
+            foreach(var type in eventTypesToInspect)
+            {
+                AssertTypeIsValid(type);
+            }
+        }
+
+        public static void AssertValidToSend(IMessage message)
+        {
+            if(message is IRequiresTransactionalSendOperationMessage && Transaction.Current == null)
+            {
+                throw new Exception($"{message.GetType().FullName} is {nameof(IRequiresTransactionalSendOperationMessage)} but there is no transaction.");
+            }
+
+            if(message is IForbidTransactionalSendOperationMessage && Transaction.Current != null)
+            {
+                throw new Exception($"{message.GetType().FullName} is {nameof(IForbidTransactionalSendOperationMessage)} but there is a transaction.");
+            }
+
+            AssertTypeIsValid(message.GetType());
+        }
+
+        static void AssertTypeIsValid(Type type)
+        {
+            lock(SuccessfullyInspectedTypes)
+            {
+                if(SuccessfullyInspectedTypes.Contains(type)) return;
+
+                if(type.Implements<ICommand>() && type.Implements<IEvent>())
+                {
+                    throw new Exception($"{type.FullName} implements both {typeof(ICommand)} and {typeof(IEvent)}.");
+                }
+
+                if(type.Implements<ICommand>() && type.Implements<IQuery>())
+                {
+                    throw new Exception($"{type.FullName} implements both {typeof(ICommand)} and {typeof(IQuery)}.");
+                }
+
+                if(type.Implements<IEvent>() && type.Implements<IQuery>())
+                {
+                    throw new Exception($"{type.FullName} implements both {typeof(IEvent)} and {typeof(IQuery)}.");
+                }
+
+                if(type.Implements<IRemoteMessage>() && type.Implements<ILocalMessage>())
+                {
+                    throw new Exception($"{type.FullName} implements both {typeof(IRemoteMessage)} and {typeof(ILocalMessage)}.");
+                }
+
+                if(type.Implements<IRequiresTransactionalSendOperationMessage>() && type.Implements<IForbidTransactionalSendOperationMessage>())
+                {
+                    throw new Exception($"{type.FullName} implements both {typeof(IRequiresTransactionalSendOperationMessage)} and {typeof(IForbidTransactionalSendOperationMessage)}.");
+                }
+                SuccessfullyInspectedTypes.Add(type);
+            }
+        }
+    }
+}
--- src/framework/Composable.CQRS/Persistence/EventStore/Aggregates/AggregateTypeValidator.cs
+++ src/framework/Composable.CQRS/Persistence/EventStore/Aggregates/AggregateTypeValidator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Transactions;
 using Composable.Messaging;
 using Composable.Refactoring.Naming;
 using Composable.System;
@@ -109,68 +108,6 @@ List of problem members:{Environment.NewLine}{brokenMembers}{Environment.NewLine
             static IReadOnlyList<Type> GetAllInheritingClassesOrInterfaces(Type type) => type.Assembly.GetTypes()
                                                                                              .Where(type.IsAssignableFrom)
                                                                                              .ToList();
-        }
-    }
-
-    static class MessageInspector
-    {
-        static readonly HashSet<Type> SuccessfullyInspectedTypes = new HashSet<Type>();
-
-        internal static void AssertValid(IReadOnlyList<Type> eventTypesToInspect)
-        {
-            foreach(var type in eventTypesToInspect)
-            {
-                AssertTypeIsValid(type);
-            }
-        }
-
-        public static void AssertValidToSend(IMessage message)
-        {
-            if(message is IRequiresTransactionalSendOperationMessage && Transaction.Current == null)
-            {
-                throw new Exception($"{message.GetType().FullName} is {nameof(IRequiresTransactionalSendOperationMessage)} but there is no transaction.");
-            }
-
-            if(message is IForbidTransactionalSendOperationMessage && Transaction.Current != null)
-            {
-                throw new Exception($"{message.GetType().FullName} is {nameof(IForbidTransactionalSendOperationMessage)} but there is a transaction.");
-            }
-
-            AssertTypeIsValid(message.GetType());
-        }
-
-        static void AssertTypeIsValid(Type type)
-        {
-            lock(SuccessfullyInspectedTypes)
-            {
-                if(SuccessfullyInspectedTypes.Contains(type)) return;
-
-                if(type.Implements<ICommand>() && type.Implements<IEvent>())
-                {
-                    throw new Exception($"{type.FullName} implements both {typeof(ICommand)} and {typeof(IEvent)}.");
-                }
-
-                if(type.Implements<ICommand>() && type.Implements<IQuery>())
-                {
-                    throw new Exception($"{type.FullName} implements both {typeof(ICommand)} and {typeof(IQuery)}.");
-                }
-
-                if(type.Implements<IEvent>() && type.Implements<IQuery>())
-                {
-                    throw new Exception($"{type.FullName} implements both {typeof(IEvent)} and {typeof(IQuery)}.");
-                }
-
-                if(type.Implements<IRemoteMessage>() && type.Implements<ILocalMessage>())
-                {
-                    throw new Exception($"{type.FullName} implements both {typeof(IRemoteMessage)} and {typeof(ILocalMessage)}.");
-                }
-
-                if(type.Implements<IRequiresTransactionalSendOperationMessage>() && type.Implements<IForbidTransactionalSendOperationMessage>())
-                {
-                    throw new Exception($"{type.FullName} implements both {typeof(IRequiresTransactionalSendOperationMessage)} and {typeof(IForbidTransactionalSendOperationMessage)}.");
-                }
-                SuccessfullyInspectedTypes.Add(type);
-            }
         }
     }
 }

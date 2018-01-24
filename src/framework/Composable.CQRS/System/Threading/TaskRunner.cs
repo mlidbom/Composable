780ebd33580d0e1ea0a5252e33709b3b8b867b61
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-24 23:25:11 +0100

    Remove cruft.

--- src/framework/Composable.CQRS/System/Threading/TaskRunner.cs
+++ src/framework/Composable.CQRS/System/Threading/TaskRunner.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Composable.System.Linq;
-using Composable.System.Threading.ResourceAccess;
 
 namespace Composable.System.Threading
 {

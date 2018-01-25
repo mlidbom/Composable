cf5881a6edc7f6c7f3a9a93d31d916e07aad9d8c
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-25 07:35:45 +0100

    Formatting.

--- src/Samples/AccountManagement/AccountManagement.Server/UI/Account.UIAdapter.cs
+++ src/Samples/AccountManagement/AccountManagement.Server/UI/Account.UIAdapter.cs
@@ -11,34 +11,34 @@ namespace AccountManagement.UI
     static class AccountUIAdapter
     {
         public static void Login(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForCommandWithResult(
-                (AccountResource.Commands.LogIn.UI logIn, ILocalServiceBusSession bus) =>
-                    Account.Login(Email.Parse(logIn.Email), logIn.Password, bus));
+            (AccountResource.Commands.LogIn.UI logIn, ILocalServiceBusSession bus) =>
+                Account.Login(Email.Parse(logIn.Email), logIn.Password, bus));
 
-            internal static void ChangePassword(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForCommand(
-                (AccountResource.Commands.ChangePassword command, ILocalServiceBusSession busSession) =>
-                    busSession.Get(PrivateAccountApi.Queries.ById(command.AccountId)).ChangePassword(command.OldPassword, new Password(command.NewPassword)));
+        internal static void ChangePassword(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForCommand(
+            (AccountResource.Commands.ChangePassword command, ILocalServiceBusSession busSession) =>
+                busSession.Get(PrivateAccountApi.Queries.ById(command.AccountId)).ChangePassword(command.OldPassword, new Password(command.NewPassword)));
 
-            internal static void ChangeEmail(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForCommand(
-                (AccountResource.Commands.ChangeEmail command, ILocalServiceBusSession bus) =>
-                    bus.Get(PrivateAccountApi.Queries.ById(command.AccountId)).ChangeEmail(Email.Parse(command.Email)));
+        internal static void ChangeEmail(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForCommand(
+            (AccountResource.Commands.ChangeEmail command, ILocalServiceBusSession bus) =>
+                bus.Get(PrivateAccountApi.Queries.ById(command.AccountId)).ChangeEmail(Email.Parse(command.Email)));
 
-            internal static void Register(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForCommandWithResult(
-                (AccountResource.Commands.Register command, ILocalServiceBusSession bus) =>
+        internal static void Register(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForCommandWithResult(
+            (AccountResource.Commands.Register command, ILocalServiceBusSession bus) =>
+            {
+                var (status, account) = Account.Register(command.AccountId, Email.Parse(command.Email), new Password(command.Password), bus);
+                switch(status)
                 {
-                    var (status, account) = Account.Register(command.AccountId, Email.Parse(command.Email), new Password(command.Password), bus);
-                    switch(status)
-                    {
-                        case RegistrationAttemptStatus.Successful:
-                            return new AccountResource.Commands.Register.RegistrationAttemptResult(status, new AccountResource(account));
-                        case RegistrationAttemptStatus.EmailAlreadyRegistered:
-                            return new AccountResource.Commands.Register.RegistrationAttemptResult(status, null);
-                        default:
-                            throw new ArgumentOutOfRangeException();
-                    }
-                });
+                    case RegistrationAttemptStatus.Successful:
+                        return new AccountResource.Commands.Register.RegistrationAttemptResult(status, new AccountResource(account));
+                    case RegistrationAttemptStatus.EmailAlreadyRegistered:
+                        return new AccountResource.Commands.Register.RegistrationAttemptResult(status, null);
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            });
 
-            internal static void GetById(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForQuery(
-                (EntityByIdQuery<AccountResource> accountQuery, ILocalServiceBusSession bus)
-                    => new AccountResource(bus.Get(PrivateAccountApi.Queries.ReadOnlyCopy(accountQuery.Id))));
+        internal static void GetById(MessageHandlerRegistrarWithDependencyInjectionSupport registrar) => registrar.ForQuery(
+            (EntityByIdQuery<AccountResource> accountQuery, ILocalServiceBusSession bus)
+                => new AccountResource(bus.Get(PrivateAccountApi.Queries.ReadOnlyCopy(accountQuery.Id))));
     }
 }

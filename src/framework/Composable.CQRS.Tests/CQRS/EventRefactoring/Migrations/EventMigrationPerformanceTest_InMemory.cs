7102cefad314d0ca5f712435ed37f800a3a1b014
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-30 15:20:36 +0100

    unignore tests.

--- src/framework/Composable.CQRS.Tests/CQRS/EventRefactoring/Migrations/EventMigrationPerformanceTest_InMemory.cs
+++ src/framework/Composable.CQRS.Tests/CQRS/EventRefactoring/Migrations/EventMigrationPerformanceTest_InMemory.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 
 namespace Composable.Tests.CQRS.EventRefactoring.Migrations
 {
-    [TestFixture, Ignore("todo: Funnily the in-memory event store is to slow for the cached tests :)")]
+    [TestFixture]
     public class EventMigrationPerformanceTest_InMemory : EventMigrationPerformanceTest
     {
         public EventMigrationPerformanceTest_InMemory() : base(typeof(InMemoryEventStore)) {}

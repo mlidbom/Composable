db31c16c81e3f55f87adc8fd3c84af1daf679e7a
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-30 14:55:07 +0100

    Checkpoint.

--- src/framework/Composable.CQRS/Messaging/Buses/EndpointBuilder.cs
+++ src/framework/Composable.CQRS/Messaging/Buses/EndpointBuilder.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using Composable.DependencyInjection;
 using Composable.GenericAbstractions.Time;
@@ -15,35 +14,6 @@ using Composable.SystemExtensions.Threading;
 
 namespace Composable.Messaging.Buses
 {
-    static class EndpointBuilderTypeMapperHelper
-    {
-        static string WithPostFix(this string guidTemplate, char postfix) => guidTemplate.Substring(0, guidTemplate.Length - 1) + postfix;
-
-        static class Postfix
-        {
-            internal const char TypeItself = '1';
-            internal const char Array = '2';
-            internal const char List = '3';
-            internal const char StringDictionary = '4';
-        }
-
-        public static ITypeMappingRegistar MapTypeAndStandardCollectionTypes<TType>(this ITypeMappingRegistar @this, string guidTemplate)
-        {
-            @this.Map<TType>(guidTemplate.WithPostFix(Postfix.TypeItself));
-
-            @this.MapStandardCollectionTypes<TType>(guidTemplate);
-            return @this;
-        }
-
-        public static ITypeMappingRegistar MapStandardCollectionTypes<TType>(this ITypeMappingRegistar @this, string guidTemplate)
-        {
-            @this.Map<TType[]>(guidTemplate.WithPostFix(Postfix.Array));
-            @this.Map<List<TType>>(guidTemplate.WithPostFix(Postfix.List));
-            @this.Map<Dictionary<string, TType>>(guidTemplate.WithPostFix(Postfix.StringDictionary));
-            return @this;
-        }
-    }
-
     class EndpointBuilder : IEndpointBuilder
     {
         static readonly ISqlConnection MasterDbConnection = new AppConfigSqlConnectionProvider().GetConnectionProvider(parameterName: "MasterDB");

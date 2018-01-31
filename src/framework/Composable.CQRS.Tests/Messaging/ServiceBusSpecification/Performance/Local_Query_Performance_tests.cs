cd551905d3d26ccc9c9fda134712b573e471aca1
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-31 09:42:16 +0100

    Adjust test expectation.

--- src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
@@ -8,12 +8,12 @@ namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
 {
     [TestFixture] public class Local_Query_performance_tests : PerformanceTestBase
     {
-        [Test] public void Runs_1000_MultiThreaded_local_queries_in_20_milliSecond()
+        [Test] public void Runs_10000_MultiThreaded_local_queries_in_20_milliSecond()
         {
             //Warmup
             StopwatchExtensions.TimeExecutionThreaded(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: 10);
 
-            TimeAsserter.ExecuteThreaded(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: 1000, maxTotal: 20.Milliseconds());
+            TimeAsserter.ExecuteThreaded(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: 10000, maxTotal: 20.Milliseconds(), maxTries: 1);
         }
 
         [Test] public void Runs_1000_SingleThreaded_local_queries_in_20_milliseconds()

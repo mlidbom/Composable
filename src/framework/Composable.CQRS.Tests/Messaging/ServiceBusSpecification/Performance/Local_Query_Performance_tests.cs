d80231d47cceb91000306a5b69257932d240dece
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-01-31 11:40:48 +0100

    Be stricter on expected single threaded performance.

--- src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
@@ -16,12 +16,12 @@ namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
             TimeAsserter.ExecuteThreaded(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: 10000, maxTotal: 30.Milliseconds(), maxTries: 1);
         }
 
-        [Test] public void Runs_1000_SingleThreaded_local_queries_in_20_milliseconds()
+        [Test] public void Runs_10000_SingleThreaded_local_queries_in_30_milliseconds()
         {
             //Warmup
             StopwatchExtensions.TimeExecutionThreaded(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: 10);
 
-            TimeAsserter.Execute(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: 1000, maxTotal: 20.Milliseconds());
+            TimeAsserter.Execute(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: 10000, maxTotal: 30.Milliseconds());
         }
     }
 }

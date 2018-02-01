694610693b5fc6dd77128a7001195a817dd5d3f9
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-01 12:18:20 +0100

    More performance tests.

--- src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Remote_Query_Performance_tests.cs
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Remote_Query_Performance_tests.cs
@@ -17,6 +17,12 @@ namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
         [Test] public void SingleThreaded_Runs_100_local_requests_making_one_remote_query_each_in_50_milliSeconds() =>
             RunScenario(threaded: false, requests: 100, queriesPerRequest: 1, maxTotal: 50.Milliseconds().InstrumentationSlowdown(1.3));
 
+        [Test] public void MultiThreaded_Runs_10_local_requests_making_10_remote_queries_each_in_12_milliSeconds() =>
+            RunScenario(threaded: true, requests: 10, queriesPerRequest: 10, maxTotal: 12.Milliseconds().InstrumentationSlowdown(2.0));
+
+        [Test] public void SingleThreaded_Runs_10_local_requests_making_10_remote_queries_each_in_50_milliSeconds() =>
+            RunScenario(threaded: false, requests: 10, queriesPerRequest: 10, maxTotal: 50.Milliseconds().InstrumentationSlowdown(1.3));
+
         void RunScenario(bool threaded, int requests, int queriesPerRequest, TimeSpan maxTotal)
         {
             var navigationSpecification = NavigationSpecification.Get(new MyRemoteQuery());

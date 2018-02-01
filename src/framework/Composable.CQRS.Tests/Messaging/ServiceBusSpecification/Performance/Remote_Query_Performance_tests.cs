b5e5effe68ddcd7c01c8fad3b6292e808718058f
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-01 13:00:45 +0100

    Implement async timings better.

--- src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Remote_Query_Performance_tests.cs
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Remote_Query_Performance_tests.cs
@@ -15,31 +15,25 @@ namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
     [TestFixture, Performance, Serial] public class RemoteQueryPerformanceTests : PerformanceTestBase
     {
         [Test] public void MultiThreaded_Runs_100_local_requests_making_one_remote_query_each_in_12_milliSeconds() =>
-            RunScenario(threaded: true, async: false, requests: 100, queriesPerRequest: 1, maxTotal: 12.Milliseconds().InstrumentationSlowdown(2.0));
+            RunScenario(threaded: true, requests: 100, queriesPerRequest: 1, maxTotal: 12.Milliseconds().InstrumentationSlowdown(2.0));
 
         [Test] public void SingleThreaded_Runs_100_local_requests_making_one_remote_query_each_in_50_milliSeconds() =>
-            RunScenario(threaded: false, async: false, requests: 100, queriesPerRequest: 1, maxTotal: 50.Milliseconds().InstrumentationSlowdown(1.3));
+            RunScenario(threaded: false, requests: 100, queriesPerRequest: 1, maxTotal: 50.Milliseconds().InstrumentationSlowdown(1.3));
 
         [Test] public void MultiThreaded_Runs_10_local_requests_making_10_remote_queries_each_in_12_milliSeconds() =>
-            RunScenario(threaded: true, async: false, requests: 10, queriesPerRequest: 10, maxTotal: 12.Milliseconds().InstrumentationSlowdown(2.3));
+            RunScenario(threaded: true, requests: 10, queriesPerRequest: 10, maxTotal: 12.Milliseconds().InstrumentationSlowdown(2.3));
 
         [Test] public void SingleThreaded_Runs_10_local_requests_making_10_remote_queries_each_in_50_milliSeconds() =>
-            RunScenario(threaded: false, async: false, requests: 10, queriesPerRequest: 10, maxTotal: 50.Milliseconds().InstrumentationSlowdown(1.3));
+            RunScenario(threaded: false, requests: 10, queriesPerRequest: 10, maxTotal: 50.Milliseconds().InstrumentationSlowdown(1.3));
 
-        [Test] public void MultiThreaded_Runs_100_local_requests_making_one_async_remote_query_each_in_12_milliSeconds() =>
-            RunScenario(threaded: true, async: true, requests: 100, queriesPerRequest: 1, maxTotal: 12.Milliseconds().InstrumentationSlowdown(2.0));
+        [Test] public void Async_Runs_100_local_requests_making_one_async_remote_query_each_in_12_milliSeconds() =>
+            RunAsyncScenario(requests: 100, queriesPerRequest: 1, maxTotal: 12.Milliseconds().InstrumentationSlowdown(2.0));
 
-        [Test] public void SingleThreaded_Runs_100_local_requests_making_one_async_remote_query_each_in_50_milliSeconds() =>
-            RunScenario(threaded: false, async: true, requests: 100, queriesPerRequest: 1, maxTotal: 50.Milliseconds().InstrumentationSlowdown(1.3));
-
-        [Test] public void MultiThreaded_Runs_10_local_requests_making_10_async_remote_queries_each_in_8_milliSeconds() =>
-            RunScenario(threaded: true, async: true, requests: 10, queriesPerRequest: 10, maxTotal: 8.Milliseconds().InstrumentationSlowdown(3.0));
-
-        [Test] public void SingleThreaded_Runs_10_local_requests_making_10_async_remote_queries_each_in_12_milliSeconds() =>
-            RunScenario(threaded: false, async: true, requests: 10, queriesPerRequest: 10, maxTotal: 12.Milliseconds().InstrumentationSlowdown(2.0));
+        [Test] public void Async_Runs_10_local_requests_making_10_async_remote_queries_each_in_8_milliSeconds() =>
+            RunAsyncScenario(requests: 10, queriesPerRequest: 10, maxTotal: 8.Milliseconds().InstrumentationSlowdown(3.0));
 
 
-        void RunScenario(bool threaded, bool async, int requests, int queriesPerRequest, TimeSpan maxTotal)
+        void RunScenario(bool threaded, int requests, int queriesPerRequest, TimeSpan maxTotal)
         {
             var navigationSpecification = NavigationSpecification.Get(new MyRemoteQuery());
 
@@ -55,28 +49,36 @@ namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
                 });
             }
 
-            void RunRequestAsync()
-            {
-                ClientEndpoint.ServiceLocator.ExecuteInIsolatedScope(() =>
-                {
-                    var tasks = 1.Through(queriesPerRequest).Select(index => RemoteNavigator.NavigateAsync(navigationSpecification)).Cast<Task>().ToArray();
-                    Task.WaitAll(tasks);
-                });
-            }
-
-            var runScenario = async ? (Action)RunRequestAsync : RunRequest;
-
             //ncrunch: no coverage end
 
             if(threaded)
             {
-                StopwatchExtensions.TimeExecutionThreaded(runScenario, iterations: requests);
-                TimeAsserter.ExecuteThreaded(runScenario, iterations: requests, maxTotal: maxTotal);
+                StopwatchExtensions.TimeExecutionThreaded(RunRequest, iterations: requests);
+                TimeAsserter.ExecuteThreaded(RunRequest, iterations: requests, maxTotal: maxTotal);
             } else
             {
-                StopwatchExtensions.TimeExecution(runScenario, iterations: requests);
-                TimeAsserter.Execute(runScenario, iterations: requests, maxTotal: maxTotal);
+                StopwatchExtensions.TimeExecution(RunRequest, iterations: requests);
+                TimeAsserter.Execute(RunRequest, iterations: requests, maxTotal: maxTotal);
             }
+        }
+
+        void RunAsyncScenario(int requests, int queriesPerRequest, TimeSpan maxTotal)
+        {
+            var navigationSpecification = NavigationSpecification.Get(new MyRemoteQuery());
+
+            async Task RunRequestAsync()
+            {
+                await ClientEndpoint.ServiceLocator.ExecuteInIsolatedScopeAsync(
+                    async () => await Task.WhenAll(1.Through(queriesPerRequest)
+                                                    .Select(index => RemoteNavigator.NavigateAsync(navigationSpecification))
+                                                    .ToArray()));
+            }
+
+            async Task RunScenario() => await Task.WhenAll(1.Through(requests).Select(_ => RunRequestAsync()).ToArray());
+
+            RunScenario().Wait();
+
+            TimeAsserter.Execute(() => RunScenario().Wait(), maxTotal: maxTotal);
         }
     }
 }

972c3c9f65c7389b97afcc99301e1bc8a159f5d7
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-01 10:58:08 +0100

    Checkpoint.

--- src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
@@ -1,4 +1,5 @@
-﻿using Composable.DependencyInjection;
+﻿using System;
+using Composable.DependencyInjection;
 using Composable.System.Diagnostics;
 using Composable.Testing.Performance;
 using FluentAssertions;
@@ -7,24 +8,43 @@ using NUnit.Framework;
 
 namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
 {
-    [TestFixture, Performance, Isolated, Serial] public class Local_Query_performance_tests : PerformanceTestBase
+    [TestFixture, Performance, Isolated, Serial]
+    public class Local_Query_performance_tests : PerformanceTestBase
     {
-        [Test, Isolated, Serial] public void Runs_100_000_MultiThreaded_local_queries_in_FIXME_milliSeconds()
+        [Test, Isolated, Serial] public void Runs_100_000__MultiThreaded_local_requests_making_1_local_query_each_in_100_milliseconds() =>
+            RunScenario(threaded: true, requests: 100_000, queriesPerRequest: 1, maxTotal: 100.Milliseconds());
+
+        [Test, Isolated, Serial] public void Runs_100_000_SingleThreaded_local_requests_making_a_single_local_query_in_260_milliseconds() =>
+            RunScenario(threaded: false, requests: 100_000, queriesPerRequest: 1, maxTotal: 260.Milliseconds());
+
+        [Test, Isolated, Serial] public void Runs_10_000__MultiThreaded_local_requests_making_10_local_queríes_each_in_60_milliseconds() =>
+            RunScenario(threaded: true, requests: 10_000, queriesPerRequest: 10, maxTotal: 60.Milliseconds());
+
+        [Test, Isolated, Serial] public void Runs_10_000__SingleThreaded_local_requests_making_10_local_queríes_each_in_100_milliseconds() =>
+            RunScenario(threaded: false, requests: 10_000, queriesPerRequest: 10, maxTotal: 100.Milliseconds());
+
+        void RunScenario(bool threaded, int requests, int queriesPerRequest, TimeSpan maxTotal)
         {
-            const int iterations = 100_000;
-            //Warmup
-            StopwatchExtensions.TimeExecutionThreaded(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: iterations);
+            void RunRequest()
+            {
+                ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() =>
+                {
+                    for(int i = 0; i < queriesPerRequest; i++)
+                    {
+                        ServerBusSession.Execute(new MyLocalQuery());
+                    }
+                });
+            }
 
-            TimeAsserter.ExecuteThreaded(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: iterations, maxTotal: 85.Milliseconds());
-        }
-
-        [Test, Isolated, Serial] public void Runs_100_000_SingleThreaded_local_queries_in_FIXME_milliseconds()
-        {
-            const int iterations = 100_000;
-            //Warmup
-            StopwatchExtensions.TimeExecutionThreaded(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: iterations);
-
-            TimeAsserter.Execute(action: () => ServerEndpoint.ServiceLocator.ExecuteInIsolatedScope(() => ServerBusSession.Execute(new MyLocalQuery())), iterations: iterations, maxTotal: 212.Milliseconds());
+            if(threaded)
+            {
+                StopwatchExtensions.TimeExecutionThreaded(RunRequest, iterations: requests);
+                TimeAsserter.ExecuteThreaded(RunRequest, iterations: requests, maxTotal: maxTotal);
+            } else
+            {
+                StopwatchExtensions.TimeExecution(RunRequest, iterations: requests);
+                TimeAsserter.Execute(RunRequest, iterations: requests, maxTotal: maxTotal);
+            }
         }
     }
 }

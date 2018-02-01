ba5bd1ffd4f39e6142f40bf765242696ed620fd6
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-01 11:05:52 +0100

    Checkpoint.

--- src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
+++ src/framework/Composable.CQRS.Tests/Messaging/ServiceBusSpecification/Performance/Local_Query_Performance_tests.cs
@@ -8,19 +8,18 @@ using NUnit.Framework;
 
 namespace Composable.Tests.Messaging.ServiceBusSpecification.Performance
 {
-    [TestFixture, Performance, Isolated, Serial]
-    public class Local_Query_performance_tests : PerformanceTestBase
+    [TestFixture, Performance, Serial] public class Local_Query_performance_tests : PerformanceTestBase
     {
-        [Test, Isolated, Serial] public void Runs_10_000__MultiThreaded_local_requests_making_1_local_query_each_in_10_milliseconds() =>
+        [Test] public void Runs_10_000__MultiThreaded_local_requests_making_1_local_query_each_in_10_milliseconds() =>
             RunScenario(threaded: true, requests: 10_000, queriesPerRequest: 1, maxTotal: 10.Milliseconds());
 
-        [Test, Isolated, Serial] public void Runs_10_000_SingleThreaded_local_requests_making_a_single_local_query_in_26_milliseconds() =>
+        [Test] public void Runs_10_000_SingleThreaded_local_requests_making_a_single_local_query_in_26_milliseconds() =>
             RunScenario(threaded: false, requests: 10_000, queriesPerRequest: 1, maxTotal: 26.Milliseconds());
 
-        [Test, Isolated, Serial] public void Runs_1_000__MultiThreaded_local_requests_making_10_local_queríes_each_in_7_milliseconds() =>
+        [Test] public void Runs_1_000__MultiThreaded_local_requests_making_10_local_queríes_each_in_7_milliseconds() =>
             RunScenario(threaded: true, requests: 1_000, queriesPerRequest: 10, maxTotal: 7.Milliseconds());
 
-        [Test, Isolated, Serial] public void Runs_1_000__SingleThreaded_local_requests_making_10_local_queríes_each_in_10_milliseconds() =>
+        [Test] public void Runs_1_000__SingleThreaded_local_requests_making_10_local_queríes_each_in_10_milliseconds() =>
             RunScenario(threaded: false, requests: 1_000, queriesPerRequest: 10, maxTotal: 10.Milliseconds());
 
         void RunScenario(bool threaded, int requests, int queriesPerRequest, TimeSpan maxTotal)

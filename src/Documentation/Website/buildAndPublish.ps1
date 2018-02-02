15fd2ae223cad1b4570eccb407c87fe488b11505
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-02 13:40:34 +0100

    Get buildAndPublish.ps1 working correctly again.

--- src/Documentation/Website/buildAndPublish.ps1
+++ src/Documentation/Website/buildAndPublish.ps1
@@ -1,28 +1,38 @@
-$ghPagesCheckoutFolder = '../../../Composable-gh-pages'
+Push-Location $PSScriptRoot #knowing which folder we are in is good :)
+
+$buildFolder = "$PSScriptRoot\_site"
+$ghPagesCheckoutFolder = "$PSScriptRoot/../../../../Composable-gh-pages"
 
 if(!(Test-Path $ghPagesCheckoutFolder))
 {
-    Push-Location '../../../'
-    Write-Host "Missing gh-pages checkout"
-    git clone --single-branch --branch gh-pages git@github.com:mlidbom/Composable.git Composable-gh-pages
+    Write-Host "Missing gh-pages checkout. Cloning"
+    Push-Location "$ghPagesCheckoutFolder/.."
+    git clone --quiet --single-branch --branch gh-pages 'https://github.com/mlidbom/Composable.git' Composable-gh-pages
     Pop-Location
-    return
 }
 
-$targetFolder = (Get-Item $ghPagesCheckoutFolder).FullName
-Push-Location $targetFolder
+
+$ghPagesCheckoutFolder = (Get-Item $ghPagesCheckoutFolder).FullName
+Push-Location $ghPagesCheckoutFolder
+git config core.autocrlf true
+git config --global core.safecrlf false
+
 git checkout -f 
 git clean -fd
 git pull
 
 Pop-Location
 
-bundle.bat exec jekyll build --destination $targetFolder
+pwd
+.\build.bat
 
-Push-Location $targetFolder
+robocopy.exe /MIR "$buildFolder" "$ghPagesCheckoutFolder" /XD ".git" "apidocs" /XF ".gitignore" "CNAME"
+
+Push-Location $ghPagesCheckoutFolder
 
 git add .
-git commit -a -m "Automatic commit created by automated build and publish script."
+git commit --quiet -a -m "Automatic commit created by automated build and publish script."
 git push
 
+Pop-Location
 Pop-Location

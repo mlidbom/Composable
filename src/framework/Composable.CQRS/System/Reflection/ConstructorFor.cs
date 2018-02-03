2946b1505052397eb66c5db9fd235c83a7bd5592
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-03 20:36:27 +0100

    Replace one nasty usage of Activator.CreateInstance

--- src/framework/Composable.CQRS/System/Reflection/ConstructorFor.cs
+++ src/framework/Composable.CQRS/System/Reflection/ConstructorFor.cs
@@ -12,32 +12,32 @@ namespace Composable.System.Reflection
         {
             internal static class DefaultConstructor
             {
-                internal static readonly Func<TInstance> Instance = (Func<TInstance>)CompileConstructorMethod(typeof(TInstance), typeof(Func< TInstance>));
+                internal static readonly Func<TInstance> Instance = (Func<TInstance>)CompileForSignature(typeof(Func< TInstance>));
             }
 
             internal static class WithArgument<TArgument1>
             {
-                internal static readonly Func<TArgument1, TInstance> CreateIntance = (Func<TArgument1, TInstance>)CompileConstructorMethod(typeof(TInstance), typeof(Func<TArgument1, TInstance>));
+                internal static readonly Func<TArgument1, TInstance> CreateIntance = (Func<TArgument1, TInstance>)CompileForSignature(typeof(Func<TArgument1, TInstance>));
             }
 
             internal static class WithArguments<TArgument1, TArgument2>
             {
-                internal static readonly Func<TArgument1, TArgument2, TInstance> Instance = (Func<TArgument1, TArgument2, TInstance>)CompileConstructorMethod(typeof(TInstance), typeof(Func<TArgument1, TArgument2, TInstance>));
+                internal static readonly Func<TArgument1, TArgument2, TInstance> Instance = (Func<TArgument1, TArgument2, TInstance>)CompileForSignature(typeof(Func<TArgument1, TArgument2, TInstance>));
             }
 
             internal static class WithArguments<TArgument1, TArgument2, TArgument3>
             {
-                internal static readonly Func<TArgument1, TArgument2, TArgument3, TInstance> Instance = (Func<TArgument1, TArgument2, TArgument3, TInstance>)CompileConstructorMethod(typeof(TInstance), typeof(Func<TArgument1, TArgument2, TArgument3, TInstance>));
+                internal static readonly Func<TArgument1, TArgument2, TArgument3, TInstance> Instance = (Func<TArgument1, TArgument2, TArgument3, TInstance>)CompileForSignature(typeof(Func<TArgument1, TArgument2, TArgument3, TInstance>));
             }
 
             internal static class WithArguments<TArgument1, TArgument2, TArgument3, TArgument4>
             {
-                internal static readonly Func<TArgument1, TArgument2, TArgument3, TArgument4, TInstance> Instance = (Func<TArgument1, TArgument2, TArgument3, TArgument4, TInstance>)CompileConstructorMethod(typeof(TInstance), typeof(Func<TArgument1, TArgument2, TArgument3, TArgument4, TInstance>));
+                internal static readonly Func<TArgument1, TArgument2, TArgument3, TArgument4, TInstance> Instance = (Func<TArgument1, TArgument2, TArgument3, TArgument4, TInstance>)CompileForSignature(typeof(Func<TArgument1, TArgument2, TArgument3, TArgument4, TInstance>));
             }
 
             internal static class WithArguments<TArgument1, TArgument2, TArgument3, TArgument4, TArgument5>
             {
-                internal static readonly Func<TArgument1, TArgument2, TArgument3, TArgument4, TArgument5, TInstance> Instance = (Func<TArgument1, TArgument2, TArgument3, TArgument4, TArgument5, TInstance>)CompileConstructorMethod(typeof(TInstance), typeof(Func<TArgument1, TArgument2, TArgument3, TArgument4, TArgument5, TInstance>));
+                internal static readonly Func<TArgument1, TArgument2, TArgument3, TArgument4, TArgument5, TInstance> Instance = (Func<TArgument1, TArgument2, TArgument3, TArgument4, TArgument5, TInstance>)CompileForSignature(typeof(Func<TArgument1, TArgument2, TArgument3, TArgument4, TArgument5, TInstance>));
             }
 
         }
@@ -55,29 +55,30 @@ namespace Composable.System.Reflection
             lock(_defaultConstructors)
             {
                 var newConstructors = new Dictionary<Type, Func<object>>(_defaultConstructors);
-                var newConstructor = (Func<object>)CompileConstructorMethod(type, typeof(Func<>).MakeGenericType(type));
+                var newConstructor = (Func<object>)CompileForSignature(typeof(Func<>).MakeGenericType(type));
                 newConstructors.Add(type, newConstructor);
                 _defaultConstructors = newConstructors;
                 return newConstructor;
             }
         }
 
-        static Delegate CompileConstructorMethod(Type type, Type delegateType)
+        internal static Delegate CompileForSignature(Type delegateType)
         {
             var delegateTypeGenericArgumentTypes = delegateType.GetGenericArguments();
+            var instanceType = delegateTypeGenericArgumentTypes[delegateTypeGenericArgumentTypes.Length -1];
             var constructorArgumentTypes = delegateTypeGenericArgumentTypes.Length > 1 ? delegateTypeGenericArgumentTypes.Take(delegateTypeGenericArgumentTypes.Length - 1).ToArray() : Type.EmptyTypes;
 
-            ConstructorInfo constructor = type.GetConstructor(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance, binder: null, types: constructorArgumentTypes, modifiers: null);
+            ConstructorInfo constructor = instanceType.GetConstructor(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance, binder: null, types: constructorArgumentTypes, modifiers: null);
             if (constructor == null)
             {
                 if (constructorArgumentTypes.Length == 0)
                 {
-                    throw new InvalidProgramException(string.Format("Type '{0}' doesn't have a parameterless constructor.", type.Name));
+                    throw new InvalidProgramException(string.Format("Type '{0}' doesn't have a parameterless constructor.", instanceType.Name));
                 }
-                throw new InvalidProgramException(string.Format("Type '{0}' doesn't have the requested constructor.", type.Name));
+                throw new InvalidProgramException(string.Format("Type '{0}' doesn't have the requested constructor.", instanceType.Name));
             }
 
-            var dynamicMethod = new DynamicMethod("DM$_" + type.Name, type, constructorArgumentTypes, type);
+            var dynamicMethod = new DynamicMethod("DM$_" + instanceType.Name, instanceType, constructorArgumentTypes, instanceType);
             var ilGen = dynamicMethod.GetILGenerator();
             for (var i = 0; i < constructorArgumentTypes.Length; i++)
             {

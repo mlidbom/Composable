ec154d0cb27b729686daca2552534efe07f622b8
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-03 09:10:04 +0100

    Add first simple test for BinarySerializedObject

---
+++ src/framework/Composable.CQRS.Tests/Serialization/BinarySerializedObjectTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Composable.Serialization;
+using Composable.System.Threading;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Composable.Tests.Serialization
+{
+    [TestFixture]public class BinarySerializedObjectTests
+    {
+        class SinglePropertyManualImplementation : IBinarySerializeMySelf
+        {
+            public string Name { get; set; } = "Default";
+            public void Deserialize(BinaryReader reader) { Name = reader.ReadString(); }
+            public void Serialize(BinaryWriter writer) { writer.Write(Name);}
+        }
+
+        class SingleStringProperty : BinarySerializedObject<SingleStringProperty>
+        {
+            static SingleStringProperty()
+            {
+                InitGetterSetters(GetterSetterFor.String(instance => instance.Name, (instance, value) => instance.Name = value));
+            }
+
+            public string Name { get; set; } = "Default";
+        }
+
+        [Test] public void Class_with_single_property_roundtrips_correctly()
+        {
+            var singleString = new SingleStringProperty() {Name = "NonDefault"};
+
+            var data = singleString.Serialize();
+            var roundTripped = SingleStringProperty.Deserialize(data);
+
+            roundTripped.ShouldBeEquivalentTo(singleString);
+        }
+    }
+}
--- src/framework/Composable.CQRS/Serialization/BinarySerializedObject.cs
+++ src/framework/Composable.CQRS/Serialization/BinarySerializedObject.cs
@@ -43,6 +43,30 @@ namespace Composable.Serialization
             }
         }
 
+        internal byte[] Serialize()
+        {
+            //Optimization to know the size of the buffer to use from the start so we don't need to reallocate or to use ToArray. We can just return the existing buffer we created.
+            using(var stream = new MemoryStream())
+            {
+                using(var binaryWriter = new BinaryWriter(stream))
+                {
+                    Serialize(binaryWriter);
+                }
+
+                return stream.ToArray();
+            }
+        }
+
+        internal static TInheritor Deserialize(byte[] data)
+        {
+            using(var reader = new BinaryReader(new MemoryStream(data)))
+            {
+                var instance = new TInheritor();
+                instance.Deserialize(reader);
+                return instance;
+            }
+        }
+
         protected abstract class MemberGetterSetter
         {
             internal abstract void Serialize(TInheritor inheritor, BinaryWriter writer);

9677a1aeeb064d81a8cbd2948c9bf0b35f621437
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-04 09:47:43 +0100

    Create playground project.

---
+++ src/ScratchPad/ApiDraft.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable CheckNamespace
+namespace ServiceBusApi
+{
+    interface ICentralServer{}
+
+
+    interface IServiceHost{}
+    interface IService{}
+    interface IDomain{}
+    interface IBoundedContext{}
+    interface IEndpoint{}
+    interface IConsistencyBoundary{}
+
+
+    interface IMessageHandler{}
+    interface IEventHandler{}
+    interface ICommandHandler{}
+}

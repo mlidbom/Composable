2d2a441edad8dde5480da5a9ab6102206a71330d
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-02-10 05:53:41 +0100

    Improve readability.

--- src/framework/Composable.CQRS/Testing/Databases/DatabaseHelpers.cs
+++ src/framework/Composable.CQRS/Testing/Databases/DatabaseHelpers.cs
@@ -7,26 +7,28 @@ namespace Composable.Testing.Databases
         static string DropAllObjectsStatement = @"
 
 DECLARE @statements nvarchar(max)
+
 select @statements = COALESCE(@statements + '
-', '') + stmt
-FROM (SELECT CASE WHEN type = 'AF'                           THEN 'DROP AGGREGATE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
-                                                              WHEN type IN('C', 'F', 'UQ')               THEN 'ALTER TABLE ' + QUOTENAME(object_schema_name(parent_object_id)) + '.' + QUOTENAME(object_name(parent_object_id)) + ' DROP CONSTRAINT ' + QUOTENAME(name)
-                                                              WHEN type IN('FN', 'FS', 'FT', 'IF', 'TF') THEN 'DROP FUNCTION ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
-                                                              WHEN type IN('P', 'PC')                    THEN 'DROP PROCEDURE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
-                                                              WHEN type = 'R'                            THEN 'DROP RULE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
-                                                              WHEN type = 'SN'                           THEN 'DROP SYNONYM ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
-                                                              WHEN type = 'U'                            THEN 'DROP TABLE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
-                                                              WHEN type IN('TA', 'TR')                   THEN 'DROP TRIGGER ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
-                                                              WHEN type = 'V'                            THEN 'DROP VIEW ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
-                                                         END stmt, type
-                                                    FROM sys.objects) x
-                                 WHERE stmt IS NOT NULL
-                                 ORDER BY CASE 
-											WHEN type IN('F') THEN 1
-											WHEN type IN('C', 'TA', 'TR') THEN 2 
-											WHEN type IN('UQ') THEN 3
-											ELSE 4
-										END
+', '') + statement
+FROM (SELECT CASE                                             
+        WHEN type = 'AF'                           THEN 'DROP AGGREGATE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
+        WHEN type = 'R'                            THEN 'DROP RULE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
+        WHEN type = 'SN'                           THEN 'DROP SYNONYM ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
+        WHEN type = 'U'                            THEN 'DROP TABLE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
+        WHEN type = 'V'                            THEN 'DROP VIEW ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
+        WHEN type IN('C', 'F', 'UQ')               THEN 'ALTER TABLE ' + QUOTENAME(object_schema_name(parent_object_id)) + '.' + QUOTENAME(object_name(parent_object_id)) + ' DROP CONSTRAINT ' + QUOTENAME(name)
+        WHEN type IN('FN', 'FS', 'FT', 'IF', 'TF') THEN 'DROP FUNCTION ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
+        WHEN type IN('P', 'PC')                    THEN 'DROP PROCEDURE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
+        WHEN type IN('TA', 'TR')                   THEN 'DROP TRIGGER ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name)
+    END statement,
+    type FROM sys.objects) as dropStatements
+    WHERE statement IS NOT NULL
+    ORDER BY CASE 
+			WHEN type IN('F') THEN 1
+			WHEN type IN('C', 'TA', 'TR') THEN 2 
+			WHEN type IN('UQ') THEN 3
+			ELSE 4
+		END
 
 execute sp_executesql @statements
 ";

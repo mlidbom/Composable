e3191b0d1825d195caa92bc231ddd4295990a3ed
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-09-17 11:58:10 +0200

    Doc adjustments.

---
+++ src/framework/Composable.CQRS.Tests/Samples/CompleteQuickStartSample.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Composable.Tests.Samples
+{
+    [TestFixture]
+    public class CompleteQuickStartSample
+    {
+        [Test] public void RunSomeScenario()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}

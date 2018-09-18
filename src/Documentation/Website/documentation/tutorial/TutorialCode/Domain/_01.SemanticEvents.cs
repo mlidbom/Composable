524b89d47e576e3231fc77827aac56b8d361ae48
Magnus Lidbom <magnus.lidbom@gmail.com>
2018-09-18 19:36:39 +0200

    Checkpoint.

--- src/Documentation/Website/documentation/tutorial/TutorialCode/Domain/_01.SemanticEvents.cs
+++ src/Documentation/Website/documentation/tutorial/TutorialCode/Domain/_01.SemanticEvents.cs
@@ -1,36 +1,48 @@
-﻿using Composable.Persistence.EventStore;
+﻿using System;
+using Composable.Persistence.EventStore;
 
-namespace TutorialCode
+// ReSharper disable InconsistentNaming
+
+namespace _01
 {
-    public static partial class AccountEvent
+    // tag::recruitment-birds-eye-view[]
+    interface IUserActionEvent {}
+    interface IJobSeekerActed : IUserActionEvent {} //<1>
+    interface IRecruiterActed : IUserActionEvent {}
+
+    interface IRecruitmentEvent : IAggregateEvent {} //<2>
+
+    interface IRecruitmentRegistered : IRecruitmentEvent,        //<3>
+                                       IRecruiterActed,          //<4>
+                                       IAggregateCreatedEvent {} //<5>
+
+    interface IApplicationEvent : IRecruitmentEvent {} //<6>
+    interface IApplicationCreated : IApplicationEvent {}
+
+    interface IJobSeekerApplied : IRecruitmentEvent,   // <7>
+                                  IApplicationCreated, // <8>
+                                  IJobSeekerActed {}   // <9>
+    // end::recruitment-birds-eye-view[]
+}
+
+namespace _02
+{
+    // tag::02[]
+    static partial class AccountEvent // <1>
     {
-        public interface Root : IAggregateEvent {}
-
-        public interface Registered : Root,
-                                      PropertyUpdated.Email,
-                                      PropertyUpdated.Password,
-                                      IAggregateCreatedEvent {}
-
-        public interface NewEmailEntered : Root
-        {
-            string NewEmail { get; }
-        }
-
-        public interface NewEmailValidated : PropertyUpdated.Email {}
-
-        public interface UserChangedPassword : PropertyUpdated.Password {}
-
-        public static class PropertyUpdated
-        {
-            public interface Password : Root
-            {
-                string Password { get; }
-            }
-
-            public interface Email : Root
-            {
-                string Email { get; }
-            }
-        }
+        interface RootEvent : IAggregateEvent {}                    //<2> <3>
+        interface Registered : RootEvent, IAggregateCreatedEvent {} // <4>
     }
+    // end::02[]
+}
+
+namespace _03
+{
+    // tag::03[]
+    static partial class AccountEvent // <1>
+    {
+        interface RootEvent : IAggregateEvent {}                    //<2> <3>
+        interface Registered : RootEvent, IAggregateCreatedEvent {} // <4>
+    }
+    // end::03[]
 }

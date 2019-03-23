c37d0f3deea001825edec40f7de8998eeb3555c0
Magnus Lidbom <magnus.lidbom@gmail.com>
2019-03-23 13:31:22 +0100

    Improve test coverage for MachineWideSharedObject

---
+++ src/framework/Composable.CQRS.Tests/System/Threading/MachineWideSharedObjectPerformanceTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Composable.System;
+using Composable.System.Threading;
+using Composable.Testing.Performance;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Composable.Tests.System.Threading
+{
+    [TestFixture] public class MachineWideSharedObjectPerformanceTests
+    {
+        [Test] public void Get_copy_runs_single_threaded_1000_times_in_60_milliseconds()
+        {
+            var name = Guid.NewGuid().ToString();
+            using(var shared = MachineWideSharedObject<SharedObject>.For(name))
+            using(var shared2 = MachineWideSharedObject<SharedObject>.For(name))
+            {
+                TimeAsserter.Execute(() => shared.GetCopy(), iterations: 1000, maxTotal: 60.Milliseconds());
+                TimeAsserter.Execute(() => shared2.GetCopy(), iterations: 1000, maxTotal: 60.Milliseconds());
+            }
+        }
+
+        [Test] public void Get_copy_runs_multi_threaded_1000_times_in_60_milliseconds()
+        {
+            var name = Guid.NewGuid().ToString();
+            using(var shared = MachineWideSharedObject<SharedObject>.For(name))
+            using(var shared2 = MachineWideSharedObject<SharedObject>.For(name))
+            {
+                TimeAsserter.ExecuteThreaded(() => shared.GetCopy(), iterations: 1000, maxTotal: 60.Milliseconds());
+                TimeAsserter.ExecuteThreaded(() => shared2.GetCopy(), iterations: 1000, maxTotal: 60.Milliseconds());
+            }
+        }
+
+        [Test] public void Update_runs_single_threaded_1000_times_in_80_milliseconds()
+        {
+            var name = Guid.NewGuid().ToString();
+            using(var shared = MachineWideSharedObject<SharedObject>.For(name))
+            using(var shared2 = MachineWideSharedObject<SharedObject>.For(name))
+            {
+                int counter = 0;
+                TimeAsserter.Execute(() => shared.Update(@this => @this.Name = (++counter).ToString()), iterations: 1000, maxTotal: 80.Milliseconds(), maxTries: 1);
+                shared.GetCopy().Name.Should().Be("1000");
+                TimeAsserter.Execute(() => shared2.Update(@this => @this.Name = (++counter).ToString()), iterations: 1000, maxTotal: 80.Milliseconds(), maxTries: 1);
+                shared2.GetCopy().Name.Should().Be("2000");
+            }
+        }
+
+        [Test] public void Update_runs_multi_threaded_1000_times_in_120_milliseconds()
+        {
+            var name = Guid.NewGuid().ToString();
+            using(var shared = MachineWideSharedObject<SharedObject>.For(name))
+            using(var shared2 = MachineWideSharedObject<SharedObject>.For(name))
+            {
+                int counter = 0;
+                TimeAsserter.ExecuteThreaded(() => shared.Update(@this => @this.Name = (++counter).ToString()), iterations: 1000, maxTotal: 120.Milliseconds(), maxTries: 1);
+                shared.GetCopy().Name.Should().Be("1000");
+                TimeAsserter.ExecuteThreaded(() => shared2.Update(@this => @this.Name = (++counter).ToString()), iterations: 1000, maxTotal: 120.Milliseconds(), maxTries: 1);
+                shared2.GetCopy().Name.Should().Be("2000");
+            }
+        }
+    }
+}
--- src/framework/Composable.CQRS.Tests/System/Threading/MachineWideSharedObjectTests.cs
+++ src/framework/Composable.CQRS.Tests/System/Threading/MachineWideSharedObjectTests.cs
@@ -1,7 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using Composable.Serialization;
+using Composable.System.Linq;
 using Composable.System.Threading;
+using Composable.Testing.Threading;
 using FluentAssertions;
+using FluentAssertions.Extensions;
 using NUnit.Framework;
 
 namespace Composable.Tests.System.Threading
@@ -12,12 +18,12 @@ namespace Composable.Tests.System.Threading
         protected override IEnumerable<MemberGetterSetter> CreateGetterSetters() => new[] {GetterSetter.ForString(@this => @this.Name, (@this, value) => @this.Name = value)};
     }
 
-    [TestFixture]
-    public class MachineWideSharedObjectTests
+    [TestFixture] public class MachineWideSharedObjectTests
     {
         [Test] public void Create()
         {
-            using (var shared = MachineWideSharedObject<SharedObject>.For("somethingPrettyUnique"))
+            var name = Guid.NewGuid().ToString();
+            using(var shared = MachineWideSharedObject<SharedObject>.For(name))
             {
                 var test = shared.GetCopy();
 
@@ -25,10 +31,10 @@ namespace Composable.Tests.System.Threading
             }
         }
 
-        [Test]
-        public void Create_update_and_get()
+        [Test] public void Create_update_and_get()
         {
-            using (var shared = MachineWideSharedObject<SharedObject>.For("somethingMoreUnique"))
+            var name = Guid.NewGuid().ToString();
+            using(var shared = MachineWideSharedObject<SharedObject>.For(name))
             {
                 var test = shared.GetCopy();
 
@@ -41,6 +47,92 @@ namespace Composable.Tests.System.Threading
                 test = shared.GetCopy();
 
                 test.Name.Should().Be("Updated");
+            }
+        }
+
+        [Test] public void Two_instances_with_same_name_share_data()
+        {
+            var name = Guid.NewGuid().ToString();
+            using(var shared1 = MachineWideSharedObject<SharedObject>.For(name))
+            using(var shared2 = MachineWideSharedObject<SharedObject>.For(name))
+            {
+                var test1 = shared1.GetCopy();
+                var test2 = shared2.GetCopy();
+
+                test1.Name.Should().Be("Default");
+                test2.Name.Should().Be("Default");
+
+                test1 = shared1.Update(@this => @this.Name = "Updated");
+                test2 = shared2.GetCopy();
+
+                test1.Name.Should().Be("Updated");
+                test2.Name.Should().Be("Updated");
+
+                test1 = shared1.GetCopy();
+                test1.Name.Should().Be("Updated");
+            }
+        }
+
+        [Test] public void Once_all_instance_are_disposed_data_is_gone()
+        {
+            var name = Guid.NewGuid().ToString();
+            MachineWideSharedObject<SharedObject> shared2;
+            using(var shared = MachineWideSharedObject<SharedObject>.For(name))
+            {
+                shared.Update(@this => @this.Name = "New").Name.Should().Be("New");
+                shared2 = MachineWideSharedObject<SharedObject>.For(name);
+                shared.GetCopy().Name.Should().Be("New");
+            }
+
+            shared2.GetCopy().Name.Should().Be("New");
+            shared2.Dispose();
+
+            using(var shared = MachineWideSharedObject<SharedObject>.For(name))
+            {
+                shared.GetCopy().Name.Should().Be("Default");
+            }
+        }
+
+        [Test] public void Update_blocks_GetCopy_and_Update_from_both_same_and_other_instances()
+        {
+            var updateGate = ThreadGate.CreateClosedWithTimeout(100.Milliseconds());
+            var conflictingUpdateSectionSameInstance = GatedCodeSection.WithTimeout(100.Milliseconds());
+            var conflictingUpdateSectionOtherInstance = GatedCodeSection.WithTimeout(100.Milliseconds());
+            var conflictingGetCopySectionSameInstance = GatedCodeSection.WithTimeout(100.Milliseconds());
+            var conflictingGetCopySectionOtherInstance = GatedCodeSection.WithTimeout(100.Milliseconds());
+
+            var conflictingGates = Seq.Create(conflictingUpdateSectionSameInstance,
+                                              conflictingUpdateSectionOtherInstance,
+                                              conflictingGetCopySectionSameInstance,
+                                              conflictingGetCopySectionOtherInstance).ToList();
+
+            var name = Guid.NewGuid().ToString();
+            using(var shared1 = MachineWideSharedObject<SharedObject>.For(name))
+            using(var shared2 = MachineWideSharedObject<SharedObject>.For(name))
+            using(var taskRunner = new TestingTaskRunner(100.Milliseconds()))
+            {
+                // ReSharper disable AccessToDisposedClosure
+                taskRunner.Start(() => shared1.Update(@this => { updateGate.AwaitPassthrough(); }));
+                taskRunner.Start(() => conflictingUpdateSectionSameInstance.Execute(() => shared1.Update(@me => {})));
+                taskRunner.Start(() => conflictingGetCopySectionSameInstance.Execute(() => shared1.GetCopy()));
+                taskRunner.Start(() => conflictingUpdateSectionOtherInstance.Execute(() => shared2.Update(@me => {})));
+                taskRunner.Start(() => conflictingGetCopySectionOtherInstance.Execute(() => shared2.GetCopy()));
+
+                updateGate.AwaitQueueLengthEqualTo(1);
+                conflictingGates.ForEach(@gate =>
+                {
+                    gate.EntranceGate.AwaitQueueLengthEqualTo(1);
+                    gate.Open();
+                });
+
+                Thread.Sleep(50.Milliseconds());
+
+                conflictingGates.ForEach(@gate => gate.ExitGate.PassedThrough.Count.Should().Be(0));
+                updateGate.Open();
+                conflictingGates.ForEach(@gate => gate.ExitGate.AwaitPassedThroughCountEqualTo(1));
+
+                taskRunner.WaitForTasksToComplete();
+                // ReSharper restore AccessToDisposedClosure
             }
         }
     }

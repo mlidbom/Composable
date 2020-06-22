8517439aa05393952448edf3ebdc57c28d48ec78
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-06-22 22:01:51 +0200

    Add method to specifically bootstrap the sql server persistence layer for the bus.

---
+++ src/framework/Composable.CQRS.ExternalDependencies/Messaging/Buses/SqlServerPersistenceLayerBootstrapper.cs
@@ -0,0 +1,47 @@
+﻿using Composable.DependencyInjection;
+using Composable.GenericAbstractions.Time;
+using Composable.Messaging.Buses.Implementation;
+using Composable.Refactoring.Naming;
+using Composable.Serialization;
+using Composable.System.Configuration;
+using Composable.System.Data.SqlClient;
+using Composable.System.Threading;
+
+namespace Composable.Messaging.Buses
+{
+    public static class SqlServerPersistenceLayerBootstrapper
+    {
+        static IDependencyInjectionContainer RegisterSqlServerPersistenceLayer(this IDependencyInjectionContainer @this, EndpointConfiguration endpointConfiguration)
+        {
+            var endpointSqlConnection = new LazySqlServerConnectionProvider(
+                () => @this.CreateServiceLocator()
+                           .Resolve<ISqlConnectionProviderSource>()
+                           .GetConnectionProvider(endpointConfiguration.ConnectionStringName).ConnectionString);
+
+            @this.Register(
+                Singleton.For<ISqlConnectionProviderSource>().CreatedBy(
+                    () => @this.RunMode.IsTesting
+                              ? (ISqlConnectionProviderSource)new SqlServerDatabasePoolSqlConnectionProviderSource(@this.CreateServiceLocator().Resolve<IConfigurationParameterProvider>())
+                              : new ConfigurationSqlConnectionProviderSource(@this.CreateServiceLocator().Resolve<IConfigurationParameterProvider>())).DelegateToParentServiceLocatorWhenCloning(),
+                Singleton.For<IInterprocessTransport>().CreatedBy(
+                    (
+                            IGlobalBusStateTracker globalStateTracker,
+                            ITypeMapper typeMapper,
+                            IUtcTimeTimeSource timeSource,
+                            RealEndpointConfiguration configuration,
+                            ITaskRunner taskRunner,
+                            IRemotableMessageSerializer serializer)
+                        => new InterprocessTransport(globalStateTracker,
+                                                     timeSource,
+                                                     new SqlServerInterProcessTransportMessageStorage(endpointSqlConnection, typeMapper, serializer),
+                                                     typeMapper,
+                                                     configuration,
+                                                     taskRunner,
+                                                     serializer)),
+                Singleton.For<Inbox.IMessageStorage>().CreatedBy(() => new SqlServerMessageStorage(endpointSqlConnection))
+            );
+
+            return @this;
+        }
+    }
+}

4a5af7d747fb8226b3e753c160bb5225d2629a0c
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-06-23 16:58:14 +0200

    Add an in-memory storage provider and ignore all tests from mysql and in-memory provider for now.

--- src/framework/CommonAssemblyInfo.cs
+++ src/framework/CommonAssemblyInfo.cs
@@ -32,6 +32,7 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Composable.CQRS.Tests")]
 [assembly: InternalsVisibleTo("Composable.CQRS.Tests.SQLServer")]
 [assembly: InternalsVisibleTo("Composable.CQRS.Tests.MySql")]
+[assembly: InternalsVisibleTo("Composable.CQRS.Tests.InMemory")]
 [assembly: InternalsVisibleTo("Composable.CQRS.ExternalDependencies.Tests")]
 
 [assembly: InternalsVisibleTo("ScratchPad")]

0632a5da34798480bd1e0ef138b23a19e8d60995
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-06-26 21:36:04 +0200

    Group class under Inbox.

---
+++ src/framework/Composable.CQRS/Messaging/Buses/Implementation/Inbox.MessageDispatchingRules.cs
@@ -0,0 +1,27 @@
+﻿using Composable.System.Linq;
+
+namespace Composable.Messaging.Buses.Implementation
+{
+    partial class Inbox
+    {
+        class QueriesExecuteAfterAllCommandsAndEventsAreDone : IMessageDispatchingRule
+        {
+            public bool CanBeDispatched(IExecutingMessagesSnapshot executing, TransportMessage.InComing candidateMessage)
+            {
+                if(candidateMessage.MessageTypeEnum != TransportMessage.TransportMessageType.NonTransactionalQuery) return true;
+
+                return executing.AtMostOnceCommands.None() && executing.ExactlyOnceCommands.None() && executing.ExactlyOnceEvents.None();
+            }
+        }
+
+        class CommandsAndEventHandlersDoNotRunInParallelWithEachOtherInTheSameEndpoint : IMessageDispatchingRule
+        {
+            public bool CanBeDispatched(IExecutingMessagesSnapshot executing, TransportMessage.InComing candidateMessage)
+            {
+                if(candidateMessage.MessageTypeEnum == TransportMessage.TransportMessageType.NonTransactionalQuery) return true;
+
+                return executing.AtMostOnceCommands.None() && executing.ExactlyOnceCommands.None() && executing.ExactlyOnceEvents.None();
+            }
+        }
+    }
+}

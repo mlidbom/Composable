caa8e8a97bbf9d3dac8d1cd3fd2ae9aa1757c80f
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-03 18:20:38 +0200

    All tests now pass. Still some transactionality work to be done.

--- src/framework/Composable.CQRS.ExternalDependencies/Persistence/InMemory/ServiceBus/InMemoryOutboxPersistenceLayer.cs
+++ src/framework/Composable.CQRS.ExternalDependencies/Persistence/InMemory/ServiceBus/InMemoryOutboxPersistenceLayer.cs
@@ -1,14 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Composable.Messaging.Buses.Implementation;
+using Composable.System.Collections.Collections;
+using Composable.System.Linq;
+using Composable.System.Threading.ResourceAccess;
+using Message = Composable.Messaging.Buses.Implementation.IServiceBusPersistenceLayer.OutboxMessageWithReceivers;
 
 namespace Composable.Persistence.InMemory.ServiceBus
 {
-    //urgent: Implement InMemoryOutboxPersistenceLayer
+    //urgent: Handle transactions correctly. Consider using SqlLite in-memory instead: https://www.sqlite.org/inmemorydb.html
     class InMemoryOutboxPersistenceLayer : IServiceBusPersistenceLayer.IOutboxPersistenceLayer
     {
-        public void SaveMessage(IServiceBusPersistenceLayer.OutboxMessageWithReceivers messageWithReceivers) { throw new NotImplementedException(); }
-        public int MarkAsReceived(Guid messageId, Guid endpointId) => throw new NotImplementedException();
-        public Task InitAsync() => throw new NotImplementedException();
+        readonly OptimizedThreadShared<Implementation> _implementation = new OptimizedThreadShared<Implementation>(new Implementation());
+
+        public void SaveMessage(Message messageWithReceivers) => _implementation.WithExclusiveAccess(@this => @this.SaveMessage(messageWithReceivers));
+        public int MarkAsReceived(Guid messageId, Guid endpointId) => _implementation.WithExclusiveAccess(@this => @this.MarkAsReceived(messageId, endpointId));
+        public Task InitAsync() => _implementation.WithExclusiveAccess(@this => @this.InitAsync());
+
+        class Implementation : IServiceBusPersistenceLayer.IOutboxPersistenceLayer
+        {
+            readonly List<Message> _messages = new List<Message>();
+            readonly Dictionary<Guid, Dictionary<Guid, bool>> _dispatchingStatus = new Dictionary<Guid, Dictionary<Guid, bool>>();
+
+            public void SaveMessage(Message messageWithReceivers)
+            {
+                _messages.Add(messageWithReceivers);
+                var dispatchingInfo =_dispatchingStatus.GetOrAdd(messageWithReceivers.MessageId, () => new Dictionary<Guid, bool>());
+                messageWithReceivers.ReceiverEndpointIds.ForEach(@this => dispatchingInfo[@this] = false);
+            }
+
+            public int MarkAsReceived(Guid messageId, Guid endpointId)
+            {
+                _dispatchingStatus[messageId][endpointId] = true;
+                return 1;
+            }
+
+            public Task InitAsync() => Task.CompletedTask;
+        }
     }
 }

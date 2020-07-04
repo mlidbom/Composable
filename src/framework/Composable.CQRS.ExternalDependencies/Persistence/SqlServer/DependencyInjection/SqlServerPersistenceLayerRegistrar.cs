86dc779e3434f4f74e70d877febda43a22478e5f
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-04 12:02:17 +0200

    Add todo comment about wiring issue.

--- src/framework/Composable.CQRS.ExternalDependencies/Persistence/SqlServer/DependencyInjection/SqlServerPersistenceLayerRegistrar.cs
+++ src/framework/Composable.CQRS.ExternalDependencies/Persistence/SqlServer/DependencyInjection/SqlServerPersistenceLayerRegistrar.cs
@@ -49,6 +49,7 @@ namespace Composable.Persistence.SqlServer.DependencyInjection
             );
 
             //Service bus
+            //Bug: Urgent: Registering these as Scoped does not cause a failure even though the endpoint builder wires singletons to use them.
             container.Register(
                 Singleton.For<IServiceBusPersistenceLayer.IOutboxPersistenceLayer>()
                          .CreatedBy((ISqlServerConnectionProvider endpointSqlConnection) => new SqlServerOutboxPersistenceLayer(endpointSqlConnection)),

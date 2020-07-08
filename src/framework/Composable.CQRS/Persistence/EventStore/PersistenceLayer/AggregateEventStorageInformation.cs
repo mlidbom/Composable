f838987318c11cb6930bf740af20a87d48aad3ca
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-08 13:42:44 +0200

    Remove obsolete todo comment.

--- src/framework/Composable.CQRS/Persistence/EventStore/PersistenceLayer/AggregateEventStorageInformation.cs
+++ src/framework/Composable.CQRS/Persistence/EventStore/PersistenceLayer/AggregateEventStorageInformation.cs
@@ -2,7 +2,6 @@
 
 namespace Composable.Persistence.EventStore.PersistenceLayer
 {
-    //Urgent: Refactor into enum Replace,InsertBefore,InsertAfter + RefactoredEventId
     //Urgent: make all properties non-nullable instead make the whole instance on the event nullable
     //Urgent:  move data that is on all events elsewhere
     //Urgent: split that between read and write so that ReadOrder is not nullable when reading and not present when writing.

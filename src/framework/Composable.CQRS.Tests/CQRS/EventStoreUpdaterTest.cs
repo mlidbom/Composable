3a6b9a8b794f2170915aa1970ddba9b572b9a648
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-09 16:45:15 +0200

    I spoke too soon. Some tests were not run and one fails.

--- src/framework/Composable.CQRS.Tests/CQRS/EventStoreUpdaterTest.cs
+++ src/framework/Composable.CQRS.Tests/CQRS/EventStoreUpdaterTest.cs
@@ -28,7 +28,7 @@ using NUnit.Framework;
 namespace Composable.Tests.CQRS
 {
     //urgent: Remove this attribute once whole assembly runs all persistence layers.
-    [DuplicateByDimensions(nameof(PersistenceLayer.MsSql), nameof(PersistenceLayer.InMemory), nameof(PersistenceLayer.MySql))]
+    [DuplicateByDimensions(nameof(PersistenceLayer.MsSql), nameof(PersistenceLayer.InMemory), nameof(PersistenceLayer.MySql), nameof(PersistenceLayer.PgSql))]
     [TestFixture]
     public class EventStoreUpdaterTest
     {

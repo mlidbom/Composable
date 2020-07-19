785ad3dc169c3651e7d34b71f506d0499764eeea
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-19 13:46:59 +0200

    Add todo comment.

--- src/framework/Composable.CQRS/Persistence/EventStore/AggregateEvent.cs
+++ src/framework/Composable.CQRS/Persistence/EventStore/AggregateEvent.cs
@@ -13,6 +13,10 @@ namespace Composable.Persistence.EventStore
 
         protected AggregateEvent(Guid aggregateId) : this() => AggregateId = aggregateId;
 
+        /*Refactor: Consider making these fields read-only and then generating accessor for them at runtime. This would make the special nature of changing them more explicit.
+         And it would remove the requirement that this class is used. Another class could be used and we would detect that and generate new setters for that class, requiring 
+        only that it had private setters (including the one generated for a readonly property by the runtime.).
+        */
         public Guid EventId { get; internal set; }
         public int AggregateVersion { get; internal set; }
 

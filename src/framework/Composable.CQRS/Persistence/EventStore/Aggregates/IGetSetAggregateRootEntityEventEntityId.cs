75c382cc0d9d36e3b740f584473af88c64a425ca
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-19 13:58:25 +0200

    Add todo comment.

--- src/framework/Composable.CQRS/Persistence/EventStore/Aggregates/IGetSetAggregateRootEntityEventEntityId.cs
+++ src/framework/Composable.CQRS/Persistence/EventStore/Aggregates/IGetSetAggregateRootEntityEventEntityId.cs
@@ -1,5 +1,6 @@
 namespace Composable.Persistence.EventStore.Aggregates
 {
+    //Refactor: Consider removing this interface and having the aggregate component|entity pass actions as a constructor arguments to its base class instead.
     public interface IGetSetAggregateEntityEventEntityId<TEntityId, in TEventImplementation, in TEvent> : IGetAggregateEntityEventEntityId<TEvent, TEntityId>
     {
         void SetEntityId(TEventImplementation @event, TEntityId id);

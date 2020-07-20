b26438239e170be858935d3ee836a8cb34ed2f67
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-20 15:42:17 +0200

    Split unit test project from AccountManagement.Tests so that we do not run the Unit tests once per PersistenceLayer.

---
+++ src/Samples/AccountManagement/AccountManagement.UnitTests/TestData.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+namespace AccountManagement
+{
+    static class TestData
+    {
+        internal static class Passwords
+        {
+            internal const string ValidPassword = "Pass";
+
+            internal static class Invalid
+            {
+                public const string Null = null;
+                public static readonly string EmptyString = string.Empty;
+                public static readonly string ShorterThanFourCharacters = ValidPassword[0..3];
+                public static readonly string BorderedByWhiteSpaceAtEnd = $"{ValidPassword} ";
+                public static readonly string BorderedByWhiteSpaceAtBeginning = $" {ValidPassword}";
+                public static readonly string MissingUpperCaseCharacter = ValidPassword.ToLowerInvariant();
+                public static readonly string MissingLowercaseCharacter = ValidPassword.ToUpperInvariant();
+
+                public static readonly string[] All =
+                {
+                    Null,
+                    EmptyString,
+                    ShorterThanFourCharacters,
+                    BorderedByWhiteSpaceAtBeginning,
+                    BorderedByWhiteSpaceAtEnd,
+                    MissingUpperCaseCharacter,
+                    MissingLowercaseCharacter
+                };
+            }
+
+            static int _passwordCount = 1;
+            internal static string CreateValidPasswordString() => $"{ValidPassword}{_passwordCount++}";
+        }
+
+        internal static class Emails
+        {
+            static int _registeredAccounts = 1;
+
+            internal static string CreateUnusedEmail() => $"test.test@test{Interlocked.Increment(ref _registeredAccounts)}.se";
+
+            internal static IEnumerable<string> InvalidEmails => InvalidEmailsTestData.Select(@this => @this.Data);
+
+            internal static IReadOnlyList<StringTestData> InvalidEmailsTestData =>
+                new List<StringTestData>
+                {
+                    new StringTestData(null, "Is null null"),
+                    new StringTestData(string.Empty, "Is empty"),
+                    new StringTestData("test.com", "Missing @ character"),
+                    new StringTestData("test@test.com ", "Missing domain"),
+                    new StringTestData("te st@test.com", "Contains space"),
+                    new StringTestData("test@test", "Missing domain"),
+                    new StringTestData("test@test..com", "Contains \"..\""),
+                    new StringTestData("test@.test.com", "Contains \"@.\""),
+                    new StringTestData("test.@test.com", "Contains \".@\"")
+                };
+
+
+            public class StringTestData : TestData<string>
+            {
+                public StringTestData(string data, string description) : base(data, description)
+                {
+                }
+            }
+
+            public class TestData<TData> : TestCaseData
+            where TData : class
+            {
+                public TData Data { get; }
+                public TestData(TData data, string description) : base(data)
+                {
+                    Data = data;
+                    SetName($"{description} ==  \"{data?.ToString() ?? "NULL"}\"");
+                }
+            }
+        }
+    }
+}

1bafff1b4c1760e495921e739d2abb8684be6df1
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-28 18:45:06 +0200

    Do some playing around with async to try and ensure that I understand the subtleties around SynchronizationContext correctly.

---
+++ src/ScratchPad/Async/SynchronizationContextAndAsyncExplorationTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Composable.SystemCE.ThreadingCE;
+using FluentAssertions;
+using Nito.AsyncEx;
+using NUnit.Framework;
+
+namespace ScratchPad.Async
+{
+    [TestFixture]
+    public class SynchronizationContextAndAsyncExplorationTests
+    {
+        [SetUp] public void SetupTask()
+        {
+        }
+
+        [Test] public void PrintContextOther()
+        {
+            AsyncContext.Run(async () =>
+            {
+
+                SynchronizationContext.Current.Should().NotBe(null);
+                //await Task.Delay(1).NoMarshalling();
+                //PrintContextInformation("After task delay");
+                PrintContextInformation("Before 1st top level async call");
+                Level1Async().Wait();
+                PrintContextInformation("After 1st top level async call");
+                //await Level1Async().ConfigureAwait(false);
+
+                PrintContextInformation("Before 2nd top level async call");
+                await Level1Async();
+                PrintContextInformation("After 2nd top level async call");
+
+                SynchronizationContext.Current.Should().NotBe(null);
+            });
+        }
+
+        static async Task Level1Async()
+        {
+            PrintContextInformation("Level1Async start");
+            await Level2Async().NoMarshalling();
+            PrintContextInformation("Level1Async end");
+        }
+
+        static async Task Level2Async()
+        {
+            PrintContextInformation("Level2Async start");
+            await Task.Delay(1).NoMarshalling();
+            PrintContextInformation("Level2Async end");
+        }
+
+        static void PrintContextInformation(string context) { Console.WriteLine($"{context + ":",-20} SynchronizationContext: {SynchronizationContext.Current?.ToString() ?? "null"} CurrentThread: {Thread.CurrentThread.ManagedThreadId}"); }
+    }
+}

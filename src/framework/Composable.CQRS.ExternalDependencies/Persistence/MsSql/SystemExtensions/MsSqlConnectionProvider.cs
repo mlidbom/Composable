d3ecec45abdeaea47b99bb8fcd7345549f8ec6b9
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-30 22:33:59 +0200

    Readability

--- src/framework/Composable.CQRS.ExternalDependencies/Persistence/MsSql/SystemExtensions/MsSqlConnectionProvider.cs
+++ src/framework/Composable.CQRS.ExternalDependencies/Persistence/MsSql/SystemExtensions/MsSqlConnectionProvider.cs
@@ -16,31 +16,24 @@ namespace Composable.Persistence.MsSql.SystemExtensions
 
         public MsSqlConnectionProvider(Func<string> connectionString) => _connectionString = new OptimizedLazy<string>(connectionString);
 
-        async Task<SqlConnection> OpenConnectionAsync(AsyncMode mode)
+        async Task<SqlConnection> OpenConnectionAsync(AsyncMode syncOrAsync)
         {
             using var escalationForbidden = TransactionCE.NoTransactionEscalationScope("Opening connection");
             var connectionString = GetConnectionString();
             var connection = new SqlConnection(connectionString);
 
-            if(mode == AsyncMode.Async)
-                await connection.OpenAsync().NoMarshalling();
-            else
-                connection.Open();
+            await syncOrAsync.Run(connection.Open, connection.OpenAsync).NoMarshalling();
 
             return connection;
         }
 
         public TResult UseConnection<TResult>(Func<SqlConnection, TResult> func)
         {
-            using var connection = OpenConnectionAsync(AsyncMode.Sync).GetAwaiter().GetResult();
+            using var connection = OpenConnectionAsync(AsyncMode.Sync).GetAwaiterResult();
             return func(connection);
         }
 
-        public void UseConnection(Action<SqlConnection> action) => UseConnection(connection =>
-        {
-            action(connection);
-            return 1;
-        });
+        public void UseConnection(Action<SqlConnection> action) => UseConnection(action.AsFunc());
 
         public async Task<TResult> UseConnectionAsync<TResult>(Func<SqlConnection, Task<TResult>> func)
         {
@@ -48,10 +41,6 @@ namespace Composable.Persistence.MsSql.SystemExtensions
             return await func(connection).NoMarshalling();
         }
 
-        public async Task UseConnectionAsync(Func<SqlConnection, Task> action)
-        {
-            await using var connection = await OpenConnectionAsync(AsyncMode.Async).NoMarshalling();
-            await action(connection).NoMarshalling();
-        }
+        public async Task UseConnectionAsync(Func<SqlConnection, Task> action) => await UseConnectionAsync(action.AsFunc()).NoMarshalling();
     }
 }

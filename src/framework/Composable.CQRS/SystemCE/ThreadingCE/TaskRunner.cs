469fbf9c92854bf7e57750c6b9fd1f3f9a88b9d4
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-07-30 16:32:48 +0200

    Add todo comment.

--- src/framework/Composable.CQRS/SystemCE/ThreadingCE/TaskRunner.cs
+++ src/framework/Composable.CQRS/SystemCE/ThreadingCE/TaskRunner.cs
@@ -11,6 +11,7 @@ namespace Composable.SystemCE.ThreadingCE
     interface ITaskRunner
     {
         //Urgent: We cannot just ignore exceptions because we log them. Maybe calling component should be notified about them somehow and get to decide what to do?. Maybe return a Task that calling code is responsible for checking the result of sooner or later?
+        //Urgent: Check out: TaskScheduler.UnobservedTaskException and if we should use it. Perhaps in EndpointHost.
         void RunAndSurfaceExceptions(string taskName, Action task);
     }
 

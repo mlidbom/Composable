134d09d1b51ea84f5a453ce76ac31ad2ac320724
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-08-08 12:07:48 +0200

    Use existing type.

--- src/framework/Composable.PerformanceTests/Messaging/Hypermedia/Remote_Query_Performance_tests.cs
+++ src/framework/Composable.PerformanceTests/Messaging/Hypermedia/Remote_Query_Performance_tests.cs
@@ -10,6 +10,7 @@ using Composable.SystemCE.LinqCE;
 using Composable.Testing;
 using Composable.Testing.Performance;
 using NUnit.Framework;
+using CreatesItsOwnResultQuery = Composable.Messaging.MessageTypes.Remotable.NonTransactional.Queries.NewableResultLink<Composable.Tests.Messaging.Hypermedia.PerformanceTestBase.MyQueryResult>;
 
 namespace Composable.Tests.Messaging.Hypermedia
 {
@@ -19,16 +20,16 @@ namespace Composable.Tests.Messaging.Hypermedia
             RunScenario(threaded: false, requests: 100.EnvDivide(instrumented:1.3), queriesPerRequest: 1, maxTotal: 60.Milliseconds(), query: new MyRemoteQuery());
 
         [Test] public void SingleThreaded_Runs_100_local_requests_making_one_ICreateMyOwnResult_query_each_in_2_milliseconds() =>
-            RunScenario(threaded: false, requests: 100.EnvDivide(instrumented:1.3), queriesPerRequest: 1, maxTotal: 2.Milliseconds(), query: new ResultSelfCreatingQuery());
+            RunScenario(threaded: false, requests: 100.EnvDivide(instrumented:1.3), queriesPerRequest: 1, maxTotal: 2.Milliseconds(), query: new CreatesItsOwnResultQuery());
 
         [Test] public void SingleThreaded_Runs_10_local_requests_making_10_remote_queries_each_in_60_milliseconds() =>
             RunScenario(threaded: false, requests: 10.EnvDivide(instrumented:1.3), queriesPerRequest: 10, maxTotal: 60.Milliseconds(), query: new MyRemoteQuery());
 
         [Test] public void SingleThreaded_Runs_10_local_requests_making_10_ICreateMyOwnResult_query_each_in_1_milliseconds() =>
-            RunScenario(threaded: false, requests: 10.EnvDivide(instrumented:1.3), queriesPerRequest: 10, maxTotal: 1.Milliseconds(), query: new ResultSelfCreatingQuery());
+            RunScenario(threaded: false, requests: 10.EnvDivide(instrumented:1.3), queriesPerRequest: 10, maxTotal: 1.Milliseconds(), query: new CreatesItsOwnResultQuery());
 
         [Test] public void SingleThreaded_Runs_1_local_request_making_200_ICreateMyOwnResult_query_each_in_1_milliseconds() =>
-            RunScenario(threaded: false, requests: 1, queriesPerRequest: 200.EnvDivide(instrumented:1.2), maxTotal: 1.Milliseconds(), query: new ResultSelfCreatingQuery());
+            RunScenario(threaded: false, requests: 1, queriesPerRequest: 200.EnvDivide(instrumented:1.2), maxTotal: 1.Milliseconds(), query: new CreatesItsOwnResultQuery());
 
         [Test] public void MultiThreaded_Runs_100_local_requests_making_one_remote_query_each_in_12_milliseconds() =>
             RunScenario(threaded: true, requests: 100.EnvDivide(instrumented:1.5), queriesPerRequest: 1, maxTotal: 12.Milliseconds(), query: new MyRemoteQuery());
@@ -37,7 +38,7 @@ namespace Composable.Tests.Messaging.Hypermedia
             RunScenario(threaded: true, requests: 10.EnvDivide(instrumented:1.5), queriesPerRequest: 10, maxTotal: 15.Milliseconds(), query: new MyRemoteQuery());
 
         [Test] public void MultiThreaded_Runs_10_local_request_making_200_ICreateMyOwnResult_query_each_in_3_milliseconds() =>
-            RunScenario(threaded: true, requests: 10, queriesPerRequest: 200.EnvDivide(instrumented: 1.2), maxTotal: 3.Milliseconds(), query: new ResultSelfCreatingQuery());
+            RunScenario(threaded: true, requests: 10, queriesPerRequest: 200.EnvDivide(instrumented: 1.2), maxTotal: 3.Milliseconds(), query: new CreatesItsOwnResultQuery());
 
         [Test] public async Task Async_Runs_100_local_requests_making_one_async_remote_query_each_in_10_milliseconds() =>
             await RunAsyncScenario(requests: 100.EnvDivide(instrumented:1.5), queriesPerRequest: 1, maxTotal: 10.Milliseconds(), query: new MyRemoteQuery());
@@ -46,7 +47,7 @@ namespace Composable.Tests.Messaging.Hypermedia
             await RunAsyncScenario(requests: 10.EnvDivide(instrumented:1.5), queriesPerRequest: 10, maxTotal: 7.Milliseconds(), query: new MyRemoteQuery());
 
         [Test] public async Task Async_Runs_10_local_request_making_200_ICreateMyOwnResult_query_each_in_9_milliseconds() =>
-            await RunAsyncScenario(requests: 10, queriesPerRequest: 200.EnvDivide(instrumented: 1.5), maxTotal: 9.Milliseconds(), query: new ResultSelfCreatingQuery());
+            await RunAsyncScenario(requests: 10, queriesPerRequest: 200.EnvDivide(instrumented: 1.5), maxTotal: 9.Milliseconds(), query: new CreatesItsOwnResultQuery());
 
 
         void RunScenario(bool threaded, int requests, int queriesPerRequest, TimeSpan maxTotal, MessageTypes.Remotable.NonTransactional.IQuery<MyQueryResult> query)
@@ -101,10 +102,5 @@ namespace Composable.Tests.Messaging.Hypermedia
         }
 
         public RemoteQueryPerformanceTests(string _) : base(_) {}
-    }
-
-    class ResultSelfCreatingQuery : MessageTypes.Remotable.NonTransactional.ICreateMyOwnResultQuery<PerformanceTestBase.MyQueryResult>
-    {
-        public PerformanceTestBase.MyQueryResult CreateResult() => new PerformanceTestBase.MyQueryResult();
     }
 }

5219c2d01171341836a1c4ce27912dc1915d467c
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-08-11 17:37:17 +0200

    Add todo comment.

--- src/framework/Composable.CQRS/SystemCE/IStaticInstancePropertySingleton.cs
+++ src/framework/Composable.CQRS/SystemCE/IStaticInstancePropertySingleton.cs
@@ -1,4 +1,6 @@
-﻿namespace Composable.SystemCE
+﻿
+namespace Composable.SystemCE
 {
+    //Performance: Serializer should take advantage of this. No serialized data. Delegating to the Instance property when deserializing. Many API start pages and navigation style pages could implement this and gain a nice performance boost.
     interface IStaticInstancePropertySingleton {}
 }

8dc06f73917f2fae6ffcf3191bf94edda7a91589
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-08-12 17:06:03 +0200

    Test that dispatching wrapped events works as expected.

--- src/framework/Composable.CQRS.UnitTests/CQRS/EventHandling/CallMatchingHandlersInRegistrationOrderEventDispatcher_WrappedEventsTests.cs
+++ src/framework/Composable.CQRS.UnitTests/CQRS/EventHandling/CallMatchingHandlersInRegistrationOrderEventDispatcher_WrappedEventsTests.cs
@@ -3,16 +3,33 @@ using Composable.Messaging.Events;
 using Composable.Persistence.EventStore;
 using NUnit.Framework;
 
-// ReSharper disable InconsistentNaming
-#pragma warning disable IDE0051 //Review OK: unused private members are intentional in this test.
-#pragma warning disable IDE1006 //Review OK: Test Naming Styles
-
 namespace Composable.Tests.CQRS.EventHandling
 {
-    public class CallMatchingHandlersInRegistrationOrderEventDispatcher_WrappedEventsTests
+    [TestFixture]public class CallMatchingHandlersInRegistrationOrderEventDispatcher_WrappedEventsTests
     {
-        CallMatchingHandlersInRegistrationOrderEventDispatcher<MessageTypes.IEvent> _dispatcher;
+        interface IUserWrapperEvent<out TEvent> : MessageTypes.IWrapperEvent<TEvent> where TEvent : IUserEvent {}
+        class UserWrapperEvent<TEvent> : MessageTypes.WrapperEvent<TEvent>, IUserWrapperEvent<TEvent> where TEvent : IUserEvent
+        {
+            public UserWrapperEvent(TEvent @event) : base(@event) {}
+        }
 
+        interface IUserEvent : IAggregateEvent {}
+        interface IUserCreatedEvent : IUserEvent {}
+        class UserCreatedEvent : AggregateEvent, IUserCreatedEvent {}
+
+
+        interface IAdminUserWrapperEvent<out TEvent> : IUserWrapperEvent<TEvent> where TEvent : IUserEvent {}
+        class AdminUserWrapperEvent<TEvent> : UserWrapperEvent<TEvent>, IAdminUserWrapperEvent<TEvent> where TEvent : IUserEvent
+        {
+            public AdminUserWrapperEvent(TEvent @event) : base(@event) {}
+        }
+
+        interface IAdminUserEvent : IUserEvent {}
+        interface IAdminUserCreatedEvent : IAdminUserEvent, IUserCreatedEvent {}
+        class AdminUserCreatedEvent : AggregateEvent, IAdminUserCreatedEvent {}
+
+
+        CallMatchingHandlersInRegistrationOrderEventDispatcher<MessageTypes.IEvent> _dispatcher;
         [SetUp] public void SetupTask() => _dispatcher = new CallMatchingHandlersInRegistrationOrderEventDispatcher<MessageTypes.IEvent>();
 
         public class Publishing_UserCreatedEvent : CallMatchingHandlersInRegistrationOrderEventDispatcher_WrappedEventsTests
@@ -21,14 +38,14 @@ namespace Composable.Tests.CQRS.EventHandling
 
             public class Dispatches_to_handler_for : Publishing_UserCreatedEvent
             {
-                [Test] public void _IWrapperEvent_of_IEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<MessageTypes.IEvent>>();
-                [Test] public void _IWrapperEvent_of_IUserEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserEvent>>();
-                [Test] public void _IWrapperEvent_of_IUserCreatedEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserCreatedEvent>>();
+                [Test] public void IWrapperEvent_of_IEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<MessageTypes.IEvent>>();
+                [Test] public void IWrapperEvent_of_IUserEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserEvent>>();
+                [Test] public void IWrapperEvent_of_IUserCreatedEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserCreatedEvent>>();
             }
 
             public class Does_not_dispatch_to_handler_for : Publishing_UserCreatedEvent
             {
-                [Test] public void _IUserWrapperEvent_of_IUserEvent() => AssertUserCreatedEvent().DoesNotDispatchToWrapped<IUserWrapperEvent<IUserEvent>>();
+                [Test] public void IUserWrapperEvent_of_IUserEvent() => AssertUserCreatedEvent().DoesNotDispatchToWrapped<IUserWrapperEvent<IUserEvent>>();
             }
         }
 
@@ -38,14 +55,14 @@ namespace Composable.Tests.CQRS.EventHandling
 
             public class Dispatches_to_handler_for : Publishing_WrapperEvent_of_UserCreatedEvent
             {
-                [Test] public void _IWrapperEvent_of_IEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<MessageTypes.IEvent>>();
-                [Test] public void _IWrapperEvent_of_IUserEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserEvent>>();
-                [Test] public void _IWrapperEvent_of_IUserCreatedEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserCreatedEvent>>();
+                [Test] public void IWrapperEvent_of_IEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<MessageTypes.IEvent>>();
+                [Test] public void IWrapperEvent_of_IUserEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserEvent>>();
+                [Test] public void IWrapperEvent_of_IUserCreatedEvent() => AssertUserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserCreatedEvent>>();
             }
 
             public class Does_not_dispatch_to_handler_for : Publishing_WrapperEvent_of_UserCreatedEvent
             {
-                [Test] public void _IUserWrapperEvent_of_IUserEvent() => AssertUserCreatedEvent().DoesNotDispatchToWrapped<IUserWrapperEvent<IUserEvent>>();
+                [Test] public void IUserWrapperEvent_of_IUserEvent() => AssertUserCreatedEvent().DoesNotDispatchToWrapped<IUserWrapperEvent<IUserEvent>>();
             }
         }
 
@@ -55,10 +72,11 @@ namespace Composable.Tests.CQRS.EventHandling
 
             public class Dispatches_to_handler_for : Publishing_UserWrapperEvent_of_UserCreatedEvent
             {
-                [Test] public void _IWrapperEvent_of_IEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<MessageTypes.IEvent>>();
-                [Test] public void _IWrapperEvent_of_IUserEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserEvent>>();
-                [Test] public void _IWrapperEvent_of_IUserCreatedEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserCreatedEvent>>();
-                [Test] public void _IUserWrapperEvent_of_IUserEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserEvent>>();
+                [Test] public void IWrapperEvent_of_IEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<MessageTypes.IEvent>>();
+                [Test] public void IWrapperEvent_of_IUserEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserEvent>>();
+                [Test] public void IWrapperEvent_of_IUserCreatedEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserCreatedEvent>>();
+                [Test] public void IUserWrapperEvent_of_IUserEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserEvent>>();
+                [Test] public void IUserWrapperEvent_of_IUserCreatedEvent() => AssertUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserCreatedEvent>>();
             }
 
             public class Does_not_dispatch_to_handler_for : Publishing_UserWrapperEvent_of_UserCreatedEvent
@@ -67,35 +85,38 @@ namespace Composable.Tests.CQRS.EventHandling
             }
         }
 
-        interface IUserWrapperEvent<out TEvent> : MessageTypes.IWrapperEvent<TEvent> where TEvent : IUserEvent {}
-        class UserWrapperEvent<TEvent> : MessageTypes.WrapperEvent<TEvent>, IUserWrapperEvent<TEvent> where TEvent : IUserEvent
+        public class Publishing_AdminUserWrapperEvent_of_UserCreatedEvent : CallMatchingHandlersInRegistrationOrderEventDispatcher_WrappedEventsTests
         {
-            public UserWrapperEvent(TEvent @event) : base(@event) {}
+            EventDispatcherAsserter.RouteAssertion<MessageTypes.IEvent> AssertAdminUserWrapperEvent_of_UserCreatedEvent() => _dispatcher.Assert().Event(new AdminUserWrapperEvent<UserCreatedEvent>(new UserCreatedEvent()));
+
+            public class Dispatches_to_handler_for : Publishing_AdminUserWrapperEvent_of_UserCreatedEvent
+            {
+                [Test] public void IWrapperEvent_of_IEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<MessageTypes.IEvent>>();
+                [Test] public void IWrapperEvent_of_IUserEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserEvent>>();
+                [Test] public void IWrapperEvent_of_IUserCreatedEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserCreatedEvent>>();
+                [Test] public void IUserWrapperEvent_of_IUserEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserEvent>>();
+                [Test] public void IUserWrapperEvent_of_IUserCreatedEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserCreatedEvent>>();
+                [Test] public void IAdminUserWrapperEvent_of_IUserEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IAdminUserWrapperEvent<IUserEvent>>();
+                [Test] public void IAdminUserWrapperEvent_of_IUserCreatedEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserCreatedEvent>>();
+            }
         }
 
-        interface IAdminUserWrapperEvent<out TEvent> : IUserWrapperEvent<TEvent> where TEvent : IAdminUserEvent {}
-
-        class AdminUserWrapperEvent<TEvent> : UserWrapperEvent<TEvent>, IAdminUserWrapperEvent<TEvent> where TEvent : IAdminUserEvent
+        public class Publishing_AdminUserWrapperEvent_of_AdminUserCreatedEvent : CallMatchingHandlersInRegistrationOrderEventDispatcher_WrappedEventsTests
         {
-            public AdminUserWrapperEvent(TEvent @event) : base(@event) {}
+            EventDispatcherAsserter.RouteAssertion<MessageTypes.IEvent> AssertAdminUserWrapperEvent_of_UserCreatedEvent() => _dispatcher.Assert().Event(new AdminUserWrapperEvent<AdminUserCreatedEvent>(new AdminUserCreatedEvent()));
+
+            public class Dispatches_to_handler_for : Publishing_AdminUserWrapperEvent_of_AdminUserCreatedEvent
+            {
+                [Test] public void IWrapperEvent_of_IEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<MessageTypes.IEvent>>();
+                [Test] public void IWrapperEvent_of_IUserEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserEvent>>();
+                [Test] public void IWrapperEvent_of_IUserCreatedEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<MessageTypes.IWrapperEvent<IUserCreatedEvent>>();
+                [Test] public void IUserWrapperEvent_of_IUserEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserEvent>>();
+                [Test] public void IUserWrapperEvent_of_IUserCreatedEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserCreatedEvent>>();
+                [Test] public void IAdminUserWrapperEvent_of_IUserEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IAdminUserWrapperEvent<IUserEvent>>();
+                [Test] public void IAdminUserWrapperEvent_of_IUserCreatedEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IUserCreatedEvent>>();
+                [Test] public void IAdminUserWrapperEvent_of_IAdminUserEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IAdminUserWrapperEvent<IAdminUserEvent>>();
+                [Test] public void IAdminUserWrapperEvent_of_IAdminUserCreatedEvent() => AssertAdminUserWrapperEvent_of_UserCreatedEvent().DispatchesToWrapped<IUserWrapperEvent<IAdminUserCreatedEvent>>();
+            }
         }
-
-        interface IUserEvent : IAggregateEvent {}
-        interface IUserCreatedEvent : IUserEvent {}
-        interface IUserRegistered : IUserCreatedEvent {}
-        interface IUserSkillsEvent : IUserEvent {}
-        interface IUserSkillsAdded : IUserSkillsEvent {}
-        interface IUserSkillsRemoved : IUserSkillsEvent {}
-        interface IIgnoredUserEvent : IUserEvent {}
-
-        class UnHandledUserEvent : AggregateEvent, IUserEvent {}
-
-        class IgnoredUserEvent : AggregateEvent, IIgnoredUserEvent {}
-
-        class UserCreatedEvent : AggregateEvent, IUserCreatedEvent {}
-
-        class UserRegistered : AggregateEvent, IUserRegistered {}
-
-        interface IAdminUserEvent : IUserEvent {}
     }
 }

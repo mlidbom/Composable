b0794a4f6e9e0bb93ff52be12aa1cceee3c1e056
Magnus Lidbom <magnus.lidbom@gmail.com>
2020-08-14 14:12:48 +0200

    Readability.

--- src/framework/Composable.CQRS/Persistence/EventStore/Aggregates/Aggregate..cs
+++ src/framework/Composable.CQRS/Persistence/EventStore/Aggregates/Aggregate..cs
@@ -5,6 +5,7 @@ using Composable.DDD;
 using Composable.GenericAbstractions.Time;
 using Composable.Messaging;
 using Composable.Messaging.Events;
+using Composable.SystemCE;
 using Composable.SystemCE.LinqCE;
 using Composable.SystemCE.ReactiveCE;
 using JetBrains.Annotations;
@@ -19,7 +20,8 @@ namespace Composable.Persistence.EventStore.Aggregates
         where TAggregateEvent : class, IAggregateEvent
         where TAggregateEventImplementation : AggregateEvent, TAggregateEvent
     {
-        [Obsolete("Only for infrastructure", true)] protected Aggregate():this(DateTimeNowTimeSource.Instance){ }
+        [Obsolete("Only for infrastructure", true)]
+        protected Aggregate() : this(DateTimeNowTimeSource.Instance) {}
         protected Aggregate([NotNull] IUtcTimeTimeSource timeSource) : base(timeSource) {}
     }
 
@@ -34,7 +36,8 @@ namespace Composable.Persistence.EventStore.Aggregates
 
         static Aggregate() => AggregateTypeValidator<TAggregate, TAggregateEventImplementation, TAggregateEvent>.AssertStaticStructureIsValid();
 
-        [Obsolete("Only for infrastructure", true)] protected Aggregate():this(DateTimeNowTimeSource.Instance){ }
+        [Obsolete("Only for infrastructure", true)]
+        protected Aggregate() : this(DateTimeNowTimeSource.Instance) {}
 
         //Yes empty. Id should be assigned by an action and it should be obvious that the aggregate in invalid until that happens
         protected Aggregate(IUtcTimeTimeSource timeSource) : base(Guid.Empty)
@@ -53,33 +56,22 @@ namespace Composable.Persistence.EventStore.Aggregates
         readonly List<TAggregateEventImplementation> _unpublishedEvents = new List<TAggregateEventImplementation>();
         bool _applyingEvents;
 
-        protected TEvent Publish<TEvent>(TEvent theEvent)
-        where TEvent : TAggregateEventImplementation
+        protected TEvent Publish<TEvent>(TEvent theEvent) where TEvent : TAggregateEventImplementation
         {
             Contract.Assert.That(!_applyingEvents, "You cannot raise events from within event appliers");
 
-            try
+            using(ScopedChange.Enter(() => _reentrancyLevel++, () => _reentrancyLevel--))
             {
-                _reentrancyLevel++;
                 theEvent.AggregateVersion = Version + 1;
                 theEvent.UtcTimeStamp = TimeSource.UtcNow;
                 if(Version == 0)
                 {
-                    if(!(theEvent is IAggregateCreatedEvent))
-                    {
-                        throw new Exception($"The first raised event type {theEvent.GetType()} did not inherit {nameof(IAggregateCreatedEvent)}");
-                    }
-                    if(theEvent.AggregateId == Guid.Empty)
-                    {
-                        throw new Exception($"{nameof(IAggregateDeletedEvent.AggregateId)} was empty in {nameof(IAggregateCreatedEvent)}");
-                    }
+                    if(!(theEvent is IAggregateCreatedEvent)) throw new Exception($"The first raised event type {theEvent.GetType()} did not inherit {nameof(IAggregateCreatedEvent)}");
+                    if(theEvent.AggregateId == Guid.Empty) throw new Exception($"{nameof(IAggregateDeletedEvent.AggregateId)} was empty in {nameof(IAggregateCreatedEvent)}");
                     theEvent.AggregateVersion = 1;
                 } else
                 {
-                    if(theEvent.AggregateId != Guid.Empty && theEvent.AggregateId != Id)
-                    {
-                        throw new ArgumentOutOfRangeException($"Tried to raise event for Aggregated: {theEvent.AggregateId} from Aggregate with Id: {Id}.");
-                    }
+                    if(theEvent.AggregateId != Guid.Empty && theEvent.AggregateId != Id) throw new ArgumentOutOfRangeException($"Tried to raise event for Aggregated: {theEvent.AggregateId} from Aggregate with Id: {Id}.");
                     theEvent.AggregateId = Id;
                 }
 
@@ -89,10 +81,6 @@ namespace Composable.Persistence.EventStore.Aggregates
                 _unpublishedEvents.Add(theEvent);
                 _eventHandlersEventDispatcher.Dispatch(theEvent);
             }
-            finally
-            {
-                _reentrancyLevel--;
-            }
 
             if(_reentrancyLevel == 0)
             {
@@ -100,6 +88,7 @@ namespace Composable.Persistence.EventStore.Aggregates
                 {
                     _eventStream.OnNext(@event);
                 }
+
                 _unpublishedEvents.Clear();
             }
 
@@ -113,27 +102,19 @@ namespace Composable.Persistence.EventStore.Aggregates
 
         void ApplyEvent(TAggregateEvent theEvent)
         {
-            try
+            using var scope = ScopedChange.Enter(() => _applyingEvents = true, () => _applyingEvents = false);
+            if(theEvent is IAggregateCreatedEvent)
             {
-                _applyingEvents = true;
-                if (theEvent is IAggregateCreatedEvent)
-                {
 #pragma warning disable 618 // Review OK: This is the one place where we are quite sure that calling this obsolete method is correct.
-                    SetIdBeVerySureYouKnowWhatYouAreDoing(theEvent.AggregateId);
+                SetIdBeVerySureYouKnowWhatYouAreDoing(theEvent.AggregateId);
 #pragma warning restore 618
-                }
-                Version = theEvent.AggregateVersion;
-                _eventDispatcher.Dispatch(theEvent);
             }
-            finally
-            {
-                _applyingEvents = false;
-            }
+
+            Version = theEvent.AggregateVersion;
+            _eventDispatcher.Dispatch(theEvent);
         }
 
-        protected virtual void AssertInvariantsAreMet()
-        {
-        }
+        protected virtual void AssertInvariantsAreMet() {}
 
         readonly SimpleObservable<TAggregateEventImplementation> _eventStream = new SimpleObservable<TAggregateEventImplementation>();
         IObservable<IAggregateEvent> IEventStored.EventStream => _eventStream;
@@ -144,10 +125,7 @@ namespace Composable.Persistence.EventStore.Aggregates
             _unCommittedEvents.Clear();
         }
 
-        void IEventStored.SetTimeSource(IUtcTimeTimeSource timeSource)
-        {
-            TimeSource = timeSource;
-        }
+        void IEventStored.SetTimeSource(IUtcTimeTimeSource timeSource) { TimeSource = timeSource; }
 
         void IEventStored.LoadFromHistory(IEnumerable<IAggregateEvent> history)
         {
---
+++ src/framework/Composable.CQRS/SystemCE/ScopedChange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Composable.SystemCE
+{
+    static class ScopedChange
+    {
+        ///<summary>Executes <param name="enterAction"> immediately and executes <param name="exitAction"> when disposed. A for more expressive and concise version of making a change and then using a finally block to ensure that the change is rolled back.</param></param></summary>
+        public static IDisposable Enter(Action enterAction, Action exitAction)
+        {
+            enterAction();
+            return DisposableCE.Create(exitAction);
+        }
+    }
+}
